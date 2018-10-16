79acd16154513c2cd9a3772831ef0648d08c7cc7
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-10-16 15:39:24 -0400

    a bit of bootstrapification of simple_form

---
+++ config/initializers/simple_form_bootstrap_customized.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Additional generator  we added locally to be after simple_form_bootstrap.rb alphabetically,
+# so we can re-define some things from that generated file.
+
+SimpleForm.setup do |config|
+  # Default class for buttons
+  config.button_class = 'btn btn-primary'
+end
