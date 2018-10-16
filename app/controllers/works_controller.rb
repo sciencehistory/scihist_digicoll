abd284490239feab7bcb726b1ea15ac79c1f1377
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-10-16 14:27:18 -0400

    basic rails-scaffold-generated controller for works

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,75 @@
+# Managers UI for creating and editing works.
+#
+# Started with generated code from Rails 5.2 scaffold.
+#
+# We'll probably handle `show` in a different controller, for now no show.
+class WorksController < ApplicationController
+  before_action :set_work, only: [:show, :edit, :update, :destroy]
+
+  # GET /works
+  # GET /works.json
+  def index
+    @works = Work.all
+  end
+
+
+  # GET /works/new
+  def new
+    @work = Work.new
+  end
+
+  # GET /works/1/edit
+  def edit
+  end
+
+  # POST /works
+  # POST /works.json
+  def create
+    @work = Work.new(work_params)
+
+    respond_to do |format|
+      if @work.save
+        format.html { redirect_to works_path, notice: 'Work was successfully created.' }
+        format.json { render :show, status: :created, location: @work }
+      else
+        format.html { render :new }
+        format.json { render json: @work.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /works/1
+  # PATCH/PUT /works/1.json
+  def update
+    respond_to do |format|
+      if @work.update(work_params)
+        format.html { redirect_to works_path, notice: 'Work was successfully updated.' }
+        format.json { render :show, status: :ok, location: @work }
+      else
+        format.html { render :edit }
+        format.json { render json: @work.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /works/1
+  # DELETE /works/1.json
+  def destroy
+    @work.destroy
+    respond_to do |format|
+      format.html { redirect_to works_url, notice: 'Work was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_work
+      @work = Work.find_by_friendlier_id!(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def work_params
+      params.require(:work).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # We'll handle show elsewhere
+  resources :works, except: [:show]
+
 end
