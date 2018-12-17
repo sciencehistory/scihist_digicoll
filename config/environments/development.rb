b6e6736ebd2c05da4c3a95e5893c6f9a32f16acd
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-12-17 15:27:38 -0500

    switch from sunsetted sass-rails to sassc-rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # sassc-rails
+  config.sass.inline_source_maps = true
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
