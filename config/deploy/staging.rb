daa63e67698069193ad18dd3be7b5a76c7ff7111
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-12-19 10:55:34 -0500

    new user for capistrano deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # using an ssh-config entry to set the user, key, and address of the machine
 set :stage, :staging
 set :rails_env, 'production'
-server 'kithe.sciencehistory.org', user: 'hydep', roles: [:web, :app, :db, :jobs, :solr, :cron]
+server 'kithe.sciencehistory.org', user: 'digcol', roles: [:web, :app, :db, :jobs, :solr, :cron]
 #server '34.227.26.131', user: 'hydep', roles: [:solr]
 #server '54.204.60.1', user: 'hydep', roles: [:jobs, :cron] # dzi bg job box
 
