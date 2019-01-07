158b279963ad21548eebafcdaf9c8a473a4bf72c
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-01-07 15:57:10 -0500

    add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :resque
   config.active_job.queue_name_prefix = "scihist_digicoll_#{Rails.env}"
 
+  # devise mailers require this set
+  config.action_mailer.default_url_options = { host: 'kithe.sciencehistory.org' }
+
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
