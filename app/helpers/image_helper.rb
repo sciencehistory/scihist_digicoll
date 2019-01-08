61e58c2abd7f013e1421c3da4dfe45e0aee4267f
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-01-08 17:13:52 -0500

    guard thumb_image_tag against unavailable derivatives

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -11,19 +11,26 @@ module ImageHelper
   # We may switch where the image URLs come from (a redirect instead of direct S3?)
   #
   # This maybe would be better in a presenter or something, but for now, rails helpers.
+  #
+  # TODO: fallbacks if asset is nil or derivatives not available?
   def thumb_image_tag(asset, size: :standard, **image_tag_options)
     thumb_size = size.to_s
     unless %w{mini large standard}.include?(thumb_size)
       raise ArgumentError, "thumb_size must be mini, large, or standard"
     end
 
-    return nil if asset.nil? # default image instead?
+    return nil if asset.nil?
+
 
     derivative_key = "thumb_#{size}".to_sym
+    derivative = asset.derivative_for(derivative_key)
     derivative_key_2x = "#{derivative_key.to_s}_2X".to_sym
+    derivative_2x = asset.derivative_for(derivative_key_2x)
 
-    image_tag asset.derivative_for(derivative_key).url,
-      srcset: "#{asset.derivative_for(derivative_key).url} 1x, #{asset.derivative_for(derivative_key_2x).url} 2x",
+    return nil if derivative.nil? || derivative_2x.nil?
+
+    image_tag derivative.url,
+      srcset: "#{derivative.url} 1x, #{derivative_2x.url} 2x",
       **image_tag_options
   end
 end
