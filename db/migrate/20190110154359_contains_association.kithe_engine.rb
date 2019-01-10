88194895c06666be14199019615f628b7c516b4c
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-01-10 10:58:31 -0500

    add kithe contains association

---
+++ db/migrate/20190110154359_contains_association.kithe_engine.rb
@@ -0,0 +1,9 @@
+# This migration comes from kithe_engine (originally 20190109192252)
+class ContainsAssociation < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kithe_model_contains, id: false do |t|
+      t.references :containee, type: :uuid, foreign_key: {to_table: :kithe_models}
+      t.references :container, type: :uuid, foreign_key: {to_table: :kithe_models}
+    end
+  end
+end
