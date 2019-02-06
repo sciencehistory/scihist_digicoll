0749b0e3390829c62886039686841910895cccf2
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-02-06 16:29:59 -0500

    add derivative for collection icon size

--- app/models/collection_thumb_asset.rb
+++ app/models/collection_thumb_asset.rb
@@ -1,3 +1,12 @@
 class CollectionThumbAsset < Asset
+  COLLECTION_PAGE_THUMB_SIZE = 266
+
+  define_derivative("thumb_collection_page", content_type: "image") do |original_file|
+    Kithe::VipsCliImageToJpeg.new(max_width: COLLECTION_PAGE_THUMB_SIZE, thumbnail_mode: true).call(original_file)
+  end
+
+  define_derivative("thumb_collection_page_2X", content_type: "image") do |original_file|
+    Kithe::VipsCliImageToJpeg.new(max_width: COLLECTION_PAGE_THUMB_SIZE * 2, thumbnail_mode: true).call(original_file)
+  end
 
 end
