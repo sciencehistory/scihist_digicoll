16fa2b233fa6f5e4c89e053f456ff8191cbb7c94
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-02-06 17:16:29 -0500

    collection thumbs, don't create derivatives we don't need

--- app/models/collection_thumb_asset.rb
+++ app/models/collection_thumb_asset.rb
@@ -1,6 +1,12 @@
 class CollectionThumbAsset < Asset
   COLLECTION_PAGE_THUMB_SIZE = 266
 
+  # remove inherited derivatives we don't need for collections. Kind of bad OO
+  # design maybe we should have an abstract asset superclass with ordinary and collection
+  # asset both as sub-classes we don't need to do this, but it works for now.
+  remove_derivative_definition!(*self.defined_derivative_keys.find_all { |key| key.start_with?("download_")})
+  remove_derivative_definition!(:thumb_large, :thumb_large_2X)
+
   define_derivative("thumb_collection_page", content_type: "image") do |original_file|
     Kithe::VipsCliImageToJpeg.new(max_width: COLLECTION_PAGE_THUMB_SIZE, thumbnail_mode: true).call(original_file)
   end
