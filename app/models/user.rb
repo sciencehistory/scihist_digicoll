c9658eb71b7cd844d3ba1c86fec679341da939ae
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-02-19 17:08:37 -0500

    allow user to be created with no password
    can't log in. We use for creating user records and letting people use reset password link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # commented out :registerable -jrochkind
+  #
+  # jrochkind did:
+  # * comment out registerable, we don't allow registrations
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+
+  # Only used by devise validatable, we want to allow user accounts
+  # to be saved with nil password, means they won't be able to log in
+  # with any password.
+  def password_required?
+    false
+  end
 end
