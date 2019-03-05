87d4150835c170ea33ea1b26a19c4e85e0612462
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-03-05 16:01:23 -0500

    badge for non-published things in management UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # What we show next to things that are not published, in management
+  # interface.
+  def publication_badge(kithe_model)
+    unless kithe_model.published?
+      '<span class="badge badge-warning">Private</span>'.html_safe
+    end
+  end
+
 end
