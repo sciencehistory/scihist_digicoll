bbf6ea6d1b229809d216201e11707e4a03881e64
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-03-07 10:35:40 -0500

    fix file_set_importer_spec to pass

--- spec/importers/file_set_importer_spec.rb
+++ spec/importers/file_set_importer_spec.rb
@@ -1,24 +1,6 @@
 require 'rails_helper'
 
-
-# this is not a good way to do this, need refactor.
-class Importers::FileSetImporter
-  # "populate()" attempts to actually download
-  # a file from Fedora and generate derivatives;
-  # this is out of scope for this test.
-  def populate()
-    super
-  end
-end
-
 RSpec.describe Importers::FileSetImporter do
-  class ProgressBarStub
-    def log()
-    end
-    def increment()
-    end
-  end
-
   # not giving it a file_url, so it won't try to import it from fedora, cause we're
   # not ready to test that here.
   let(:metadata) do
@@ -37,13 +19,15 @@ RSpec.describe Importers::FileSetImporter do
         "date_modified" => "2018-11-29T14:09:26+00:00",
         "access_control_id" => "4d4c2f66-78a3-4385-a835-765064056526",
         "file_url" => "http://www.example.com/fedora/rest/prod/n5/83/xw/08/n583xw08g/files/file_hash",
-        "sha_1" => "1fac2923901895582e0406bfa40779a662d1010e"
+        "sha_1" => "1fac2923901895582e0406bfa40779a662d1010e",
+        "title_for_export" => "b10371138_367.tif"
     }
   end
 
   context "Import fileset" do
-    context "simple fileset" do
-      let(:file_set_importer) { Importers::FileSetImporter.new(metadata, ProgressBarStub.new) }
+    # we're not testing bytestream import yet
+    context "simple fileset without bytestream" do
+      let(:file_set_importer) { Importers::FileSetImporter.new(metadata, disable_bytestream_import: true) }
 
       it "Imports properly" do
         file_set_importer.save_item()
