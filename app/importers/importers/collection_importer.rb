1a8c3bc8882b573d6d5644ed364e71d9c176264c
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-03-07 11:23:51 -0500

    proper indentation and decleration for module-namespaced classes

--- app/importers/importers/collection_importer.rb
+++ app/importers/importers/collection_importer.rb
@@ -1,66 +1,66 @@
 module Importers
-class CollectionImporter < Importers::Importer
+  class CollectionImporter < Importers::Importer
 
-  def populate()
-    target_item.description = @metadata['description'].first
+    def populate()
+      target_item.description = @metadata['description'].first
 
-    if metadata["related_url"].present?
-      target_item.related_url = metadata["related_url"]
-    end
-
-    if metadata['members'].present?
-      member_ids = Kithe::Model.where(friendlier_id: metadata['members']).pluck(:id)
-      if member_ids.count != metadata['members'].count
-        add_error("some members may be missing, expected #{metadata['members'].count}, only found #{member_ids.count} to associate")
+      if metadata["related_url"].present?
+        target_item.related_url = metadata["related_url"]
       end
-      target_item.contain_ids = member_ids
+
+      if metadata['members'].present?
+        member_ids = Kithe::Model.where(friendlier_id: metadata['members']).pluck(:id)
+        if member_ids.count != metadata['members'].count
+          add_error("some members may be missing, expected #{metadata['members'].count}, only found #{member_ids.count} to associate")
+        end
+        target_item.contain_ids = member_ids
+      end
+
+      create_or_update_thumbnail()
     end
 
-    create_or_update_thumbnail()
-  end
-
-  def self.exportee()
-    return Collection
-  end
-
-  def self.importee()
-    return 'Collection'
-  end
-
-  def self.destination_class()
-    return Collection
-  end
-
-  def create_or_update_thumbnail()
-    return if metadata['representative_image_path'].nil?
-    thumb = find_thumbnail() || CollectionThumbAsset.new()
-    the_path = thumb_image_path()
-    if thumb.file.nil? && !the_path.nil?
-      thumb.file_attacher.set_promotion_directives(promote: "inline")
-      thumb.file_attacher.set_promotion_directives(create_derivatives: "inline")
-      thumb.file = File.open(the_path)
+    def self.exportee()
+      return Collection
     end
-    thumb.title = "Thumbnail for #{target_item.title}"
-    thumb.parent = target_item
-    thumb.save!
-    target_item.representative = thumb
-    target_item.leaf_representative = thumb
-  end
 
-  def find_thumbnail()
-    return nil if metadata['representative_image_path'].nil?
-    CollectionThumbAsset.find do  |x|
-      !x.file.nil? &&
-      x.file.metadata['filename'] == metadata['representative_image_path']
+    def self.importee()
+      return 'Collection'
+    end
+
+    def self.destination_class()
+      return Collection
+    end
+
+    def create_or_update_thumbnail()
+      return if metadata['representative_image_path'].nil?
+      thumb = find_thumbnail() || CollectionThumbAsset.new()
+      the_path = thumb_image_path()
+      if thumb.file.nil? && !the_path.nil?
+        thumb.file_attacher.set_promotion_directives(promote: "inline")
+        thumb.file_attacher.set_promotion_directives(create_derivatives: "inline")
+        thumb.file = File.open(the_path)
+      end
+      thumb.title = "Thumbnail for #{target_item.title}"
+      thumb.parent = target_item
+      thumb.save!
+      target_item.representative = thumb
+      target_item.leaf_representative = thumb
+    end
+
+    def find_thumbnail()
+      return nil if metadata['representative_image_path'].nil?
+      CollectionThumbAsset.find do  |x|
+        !x.file.nil? &&
+        x.file.metadata['filename'] == metadata['representative_image_path']
+      end
+    end
+
+    def thumb_image_path()
+      return nil if metadata['representative_image_path'].nil?
+      return nil if metadata['representative_image_path'] == ""
+      path = Rails.root.join('tmp', 'collection_thumb_paths', metadata['representative_image_path'])
+      return nil unless  File.exists? path
+      path.to_s
     end
   end
-
-  def thumb_image_path()
-    return nil if metadata['representative_image_path'].nil?
-    return nil if metadata['representative_image_path'] == ""
-    path = Rails.root.join('tmp', 'collection_thumb_paths', metadata['representative_image_path'])
-    return nil unless  File.exists? path
-    path.to_s
-  end
-end
 end
--- app/importers/importers/generic_work_importer.rb
+++ app/importers/importers/generic_work_importer.rb
@@ -1,149 +1,149 @@
 module Importers
-class Importers::GenericWorkImporter < Importers::Importer
-  def self.importee()
-    return 'GenericWork'
-  end
+  class GenericWorkImporter < Importers::Importer
+    def self.importee()
+      return 'GenericWork'
+    end
 
-  def self.destination_class()
-    return Work
-  end
+    def self.destination_class()
+      return Work
+    end
 
-  def corrected_resource_type
-     (@metadata['resource_type'] || []).map! {|x| x.downcase.gsub(' ', '_') }
-  end
+    def corrected_resource_type
+       (@metadata['resource_type'] || []).map! {|x| x.downcase.gsub(' ', '_') }
+    end
 
-  def populate()
-    empty_arrays()
-    add_external_id()
-    add_scalar_attributes()
-    add_array_attributes()
-    add_resource_type()
-    add_creator()
-    add_additional_credit()
-    add_date()
-    add_place()
-    add_inscriptions()
-    add_physical_container()
-  end
+    def populate()
+      empty_arrays()
+      add_external_id()
+      add_scalar_attributes()
+      add_array_attributes()
+      add_resource_type()
+      add_creator()
+      add_additional_credit()
+      add_date()
+      add_place()
+      add_inscriptions()
+      add_physical_container()
+    end
 
-  # This may no longer be needed, but having these be nil rather than
-  # empty arrays wasn't handled well by one of the front-end templates.
-  def empty_arrays()
-      target_item.date_of_work = []
-      target_item.place = []
-      target_item.creator = []
-      target_item.exhibition = []
-      target_item.additional_credit = []
-      target_item.inscription = []
-  end
+    # This may no longer be needed, but having these be nil rather than
+    # empty arrays wasn't handled well by one of the front-end templates.
+    def empty_arrays()
+        target_item.date_of_work = []
+        target_item.place = []
+        target_item.creator = []
+        target_item.exhibition = []
+        target_item.additional_credit = []
+        target_item.inscription = []
+    end
 
-  def add_physical_container()
-    return if metadata['physical_container'].nil?
-    map = {'b'=>'box', 'f'=>'folder', 'v'=>'volume',
-      'p'=>'part', 'g'=>'page', 's'=>'shelfmark'}
-    args = metadata['physical_container'].
-      split('|').
-      map{ |x| { map[x[0]] => x[1..-1] } }.
-      inject(:merge)
-    target_item.build_physical_container(args)
-  end
+    def add_physical_container()
+      return if metadata['physical_container'].nil?
+      map = {'b'=>'box', 'f'=>'folder', 'v'=>'volume',
+        'p'=>'part', 'g'=>'page', 's'=>'shelfmark'}
+      args = metadata['physical_container'].
+        split('|').
+        map{ |x| { map[x[0]] => x[1..-1] } }.
+        inject(:merge)
+      target_item.build_physical_container(args)
+    end
 
-  def add_creator()
-    Work::Creator::CATEGORY_VALUES.each do |k|
-      next if metadata[k].nil?
-      metadata[k].each do |v|
-        target_item.build_creator({'category'=>k, 'value'=>v})
+    def add_creator()
+      Work::Creator::CATEGORY_VALUES.each do |k|
+        next if metadata[k].nil?
+        metadata[k].each do |v|
+          target_item.build_creator({'category'=>k, 'value'=>v})
+        end
       end
     end
-  end
 
-  def add_additional_credit()
-    role_map = {'photographer' => 'photographed_by'}
-    return if metadata['additional_credits'].nil?
-    metadata['additional_credits'].each do |a_c|
-      params = {
-        'role' => role_map.fetch(a_c['role'], a_c['role']),
-        'name' => a_c['name']
+    def add_additional_credit()
+      role_map = {'photographer' => 'photographed_by'}
+      return if metadata['additional_credits'].nil?
+      metadata['additional_credits'].each do |a_c|
+        params = {
+          'role' => role_map.fetch(a_c['role'], a_c['role']),
+          'name' => a_c['name']
+        }
+        target_item.build_additional_credit(params)
+      end
+    end
+
+    def add_date()
+      return if metadata['dates'].nil?
+
+      metadata['dates'].each do |d|
+        next if d.nil?
+
+        d['start_qualifier'].downcase!  if d['start_qualifier']
+        d['finish_qualifier'].downcase! if d['finish_qualifier']
+
+        target_item.build_date_of_work(d)
+      end
+    end
+
+    def add_place()
+      Work::Place::CATEGORY_VALUES.each do |k|
+        next if metadata[k].nil?
+        metadata[k].each do |v|
+          target_item.build_place({'category'=>k, 'value'=>v})
+        end
+      end
+    end
+
+    def add_inscriptions()
+      return if metadata['inscriptions'].nil?
+
+      metadata['inscriptions'].each do |ins|
+        if ins['text'].nil?
+          add_error("ERROR: bad inscription: location, but no text.")
+          next
+        end
+        params = {
+            'location' => ins['location'],
+            'text'     => ins['text']
+        }
+        target_item.build_inscription(params)
+      end
+    end
+
+    def add_external_id()
+      @metadata['identifier'].each do |x|
+        category, value = x.split('-')
+        target_item.build_external_id({'category' => category, 'value' => value})
+      end
+    end
+
+    def add_scalar_attributes()
+      mapping = {
+        'division' => 'department'
       }
-      target_item.build_additional_credit(params)
-    end
-  end
 
-  def add_date()
-    return if metadata['dates'].nil?
-
-    metadata['dates'].each do |d|
-      next if d.nil?
-
-      d['start_qualifier'].downcase!  if d['start_qualifier']
-      d['finish_qualifier'].downcase! if d['finish_qualifier']
-
-      target_item.build_date_of_work(d)
-    end
-  end
-
-  def add_place()
-    Work::Place::CATEGORY_VALUES.each do |k|
-      next if metadata[k].nil?
-      metadata[k].each do |v|
-        target_item.build_place({'category'=>k, 'value'=>v})
+      %w(description format source rights rights_holder file_creator division admin_note).each do |k|
+        next if @metadata[k].nil?
+        v = metadata[k].class == String ? metadata[k] : metadata[k].first
+        property_to_set = mapping.fetch(k, k)
+        target_item.send("#{property_to_set}=", v)
       end
     end
-  end
 
-  def add_inscriptions()
-    return if metadata['inscriptions'].nil?
+    def add_resource_type
+      # Convert the resource_type / format strings to slugs:
+      target_item.format = (metadata['resource_type'] || []).map! {|x| x.downcase.gsub(' ', '_') }
+    end
 
-    metadata['inscriptions'].each do |ins|
-      if ins['text'].nil?
-        add_error("ERROR: bad inscription: location, but no text.")
-        next
-      end
-      params = {
-          'location' => ins['location'],
-          'text'     => ins['text']
+    def add_array_attributes()
+      mapping = {
+        'genre_string' => 'genre'
       }
-      target_item.build_inscription(params)
-    end
-  end
-
-  def add_external_id()
-    @metadata['identifier'].each do |x|
-      category, value = x.split('-')
-      target_item.build_external_id({'category' => category, 'value' => value})
-    end
-  end
-
-  def add_scalar_attributes()
-    mapping = {
-      'division' => 'department'
-    }
-
-    %w(description format source rights rights_holder file_creator division admin_note).each do |k|
-      next if @metadata[k].nil?
-      v = metadata[k].class == String ? metadata[k] : metadata[k].first
-      property_to_set = mapping.fetch(k, k)
-      target_item.send("#{property_to_set}=", v)
-    end
-  end
-
-  def add_resource_type
-    # Convert the resource_type / format strings to slugs:
-    target_item.format = (metadata['resource_type'] || []).map! {|x| x.downcase.gsub(' ', '_') }
-  end
-
-  def add_array_attributes()
-    mapping = {
-      'genre_string' => 'genre'
-    }
-    %w(extent language genre_string subject additional_title exhibition series_arrangement related_url).each do |source_k|
-      dest_k = mapping.fetch(source_k, source_k)
-      if metadata[source_k].nil?
-        target_item.send("#{dest_k }=", [])
-      else
-        target_item.send("#{dest_k }=", metadata[source_k])
+      %w(extent language genre_string subject additional_title exhibition series_arrangement related_url).each do |source_k|
+        dest_k = mapping.fetch(source_k, source_k)
+        if metadata[source_k].nil?
+          target_item.send("#{dest_k }=", [])
+        else
+          target_item.send("#{dest_k }=", metadata[source_k])
+        end
       end
     end
   end
-end
 end
