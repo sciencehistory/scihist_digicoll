292dd5bdb340771b195029fcc4590008318812e8
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-03-07 13:40:39 -0500

    set published status for genericwork
    collection and fileset don't have em in export yet
    
    Closes #88
    
    It's not great that all our tests pass now for collection/fileset, and in fact probably no code has to be changed to make em work -- export files just have to include relevant key.

--- spec/importers/generic_work_importer_spec.rb
+++ spec/importers/generic_work_importer_spec.rb
@@ -37,6 +37,7 @@ RSpec.describe Importers::GenericWorkImporter do
       "physical_container" =>  "",
       "rights_holder" =>  "",
       "access_control_id" =>  "90cb04df-61a7-4d61-84e2-130fc7ddbee3",
+      "access_control" => "private",
       "representative_id" =>  "2v23vv55g",
       "thumbnail_id" =>  "2v23vv55g",
       "admin_set_id" =>  "admin_set/default",
@@ -52,15 +53,75 @@ RSpec.describe Importers::GenericWorkImporter do
     }
   end
 
-  context "simple work" do
-    let(:generic_work_importer) { Importers::GenericWorkImporter.new(metadata) }
 
-    it "imports" do
+  let(:generic_work_importer) { Importers::GenericWorkImporter.new(metadata) }
+
+  it "imports" do
+    generic_work_importer.import
+    new_work = Work.first
+
+    expect(new_work.title).to match /Adulterations/
+    expect(new_work.created_at).to eq(DateTime.parse(metadata["date_uploaded"]))
+    expect(new_work.published?).to be(false)
+  end
+
+
+  context "public work" do
+    let(:metadata) do
+      {
+        "id"=>"8049g504g",
+        "head" => [
+          "#<ActiveTriples::Resource:0x0000558a2682fa68>"
+        ],
+        "tail" => [
+          "#<ActiveTriples::Resource:0x0000558a26826030>"
+        ],
+        "depositor"=> "njoniec@sciencehistory.org",
+        "title" => [
+          "Adulterations of food; with short processes for their detection."
+        ],
+        "date_uploaded"=> "2019-02-08T20:45:54+00:00",
+        "date_modified"=> "2019-02-08T20:49:43+00:00",
+        "state" => "#<ActiveTriples::Resource:0x0000558a2d321088>",
+        "part_of" => [
+          "#<ActiveTriples::Resource:0x0000558a2681aff0>"
+        ],
+        "identifier" => [
+          "bib-b1075796"
+        ],
+        "author" => [
+          "Atcherley, Rowland J."
+        ],
+        "credit_line" => [
+          "Courtesy of Science History Institute"
+        ],
+        "division" => "",
+        "file_creator" =>  "",
+        "physical_container" =>  "",
+        "rights_holder" =>  "",
+        "access_control_id" =>  "90cb04df-61a7-4d61-84e2-130fc7ddbee3",
+        "access_control" => "public",
+        "representative_id" =>  "2v23vv55g",
+        "thumbnail_id" =>  "2v23vv55g",
+        "admin_set_id" =>  "admin_set/default",
+        "child_ids" =>  [
+          "kp78gh433",
+          "1v53jz06w",
+          "nk322f35j",
+          "0r9674786",
+          "6q182m18c",
+          "1831cm25h",
+          "8623hz81w"
+        ]
+      }
+    end
+
+    it "imports as published" do
       generic_work_importer.import
       new_work = Work.first
 
-      expect(new_work.title).to match /Adulterations/
-      expect(new_work.created_at).to eq(DateTime.parse(metadata["date_uploaded"]))
+      expect(new_work.published?).to be(true)
     end
   end
+
 end
