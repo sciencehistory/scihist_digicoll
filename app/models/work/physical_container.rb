f774ae7ee6268d25932f3361ea6a98d871f5c1b7
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-04-09 16:43:31 -0400

    solr index physical container

--- app/models/work/physical_container.rb
+++ app/models/work/physical_container.rb
@@ -9,5 +9,13 @@ class Work
     attr_json :page, :string
     attr_json :shelfmark, :string
 
+    # Human-readable string with all of it, matching how chf_sufia did it.
+    def as_human_string
+      [:box, :folder, :volume, :part, :page, :shelfmark].collect do |attr|
+        value = send(attr)
+        "#{attr.to_s.titlecase} #{value}" if value
+      end.compact.join(", ")
+    end
+
   end
 end
