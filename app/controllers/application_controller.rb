e7ad762af7967906283312ab3c02777e6fc8a7e7
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-04-11 17:42:01 -0400

    Use a Rails controller around_action to batch Kithe::Indexable updates in all actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   rescue_from "AccessGranted::AccessDenied" do |exception|
     redirect_to root_path, alert: "You don't have permission to access requested page."
   end
+
+  around_action :batch_kithe_indexable
+
+  def batch_kithe_indexable
+    Kithe::Indexable.index_with(batching: true) do
+      yield
+    end
+  end
+
 end
