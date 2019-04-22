820a01685be5a92b3a8afccd3835a61d5165da1c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-04-22 13:02:26 -0400

    much simplified, as we do not need download derivs for pdfs

--- app/derivative_transformers/kithe/vips_cli_pdf_to_jpeg.rb
+++ app/derivative_transformers/kithe/vips_cli_pdf_to_jpeg.rb
@@ -8,26 +8,13 @@ module Kithe
   # Requires vips command line utilities `vips` and `vipsthumbnail` and to be installed on your system,
   # eg `brew install vips`, or apt package `vips-tools`.
   #
-  # If thumbnail_mode:true is given, we ALSO apply some additional best practices
+  # We ALSO apply some additional best practices
   # for minimizing size when used as an image _in a browser_, such as removing
   # color profile information. See eg:
   #  * https://developers.google.com/speed/docs/insights/OptimizeImages
   #  * http://libvips.blogspot.com/2013/11/tips-and-tricks-for-vipsthumbnail.html
   #  * https://github.com/jcupitt/libvips/issues/775
-  #
-  # It takes an open `File` object in, and returns an open TempFile object. It is
-  # built for use with kithe derivatives transformations, eg:
-  #
-  #     class Asset < KitheAsset
-  #       define_derivative(thumb) do |original_file|
-  #         Kithe::VipsCliImageToJpeg.new(max_width: 100, thumbnail_mode: true).call(original_file)
-  #       end
-  #     end
-  #
-  # We use the vips CLI because we know how, and it means we can avoid worrying
-  # about ruby memory leaks or the GIL. An alternative that uses vips ruby bindings
-  # would also be possible, and might work well, but this is what for us is tried
-  # and true.
+
   class Kithe::VipsCliPdfToJpeg
     class_attribute :srgb_profile_path, default: Kithe::Engine.root.join("lib", "vendor", "icc", "sRGB2014.icc").to_s
     class_attribute :vips_thumbnail_command, default: "vipsthumbnail"
@@ -36,15 +23,13 @@ module Kithe
 
     attr_reader :max_width, :jpeg_q
 
-    def initialize(max_width:nil, jpeg_q: 98, thumbnail_mode: false)
+    def initialize(max_width:nil, jpeg_q: 98)
       @max_width = max_width
       @jpeg_q = jpeg_q
-      @thumbnail_mode = !!thumbnail_mode
-      @border_width = 1
-      @width_minus_borders = @max_width - (2 * @border_width)
+      @border_pixels = 1
       @cmd = TTY::Command.new(printer: :null)
 
-      if thumbnail_mode && max_width.nil?
+      if max_width.nil?
         # https://github.com/libvips/libvips/issues/1179
         raise ArgumentError.new("thumbnail_mode currently requires a non-nil max_width")
       end
@@ -53,55 +38,25 @@ module Kithe
     # Will raise TTY::Command::ExitError if the external Vips command returns non-null.
     def call(original)
       files = (0..2).map {|x| Tempfile.new(["kithe_vips_pdf", ".jpg"])}
-      thumbnail(
-        @width_minus_borders,
-        original, files[0]
-      )
-      sharpen(
-        @width_minus_borders,
-        files[0], files[1]
-      )
-      add_border(
-        @width_minus_borders, image_height(files[1]),
-        files[1], files[2]
-      )
+      # Make a slightly smaller thumbnail to accomodate the border we're adding.
+      width_minus_borders = @max_width - (2 * @border_pixels)
+      # Create and sharpen a thumbnail, then add a black border to it.
+      thumbnail( width_minus_borders, original, files[0])
+      sharpen(   width_minus_borders, files[0], files[1])
+      add_border(width_minus_borders, files[1], files[2])
+      # The width of the thumbnail, including the border,
+      # is now exactly @max_width.
       files[2]
     end
 
     private
 
-    def thumbnail_mode?
-      @thumbnail_mode
-    end
-
-    # Only if we're in thumbnail_mode mode, normalize to rRGB profile, and then strip
-    # embedded profile info for a smaller size, since browsers assume sRGB
-    def maybe_profile_normalization_args
-      return [] unless thumbnail_mode?
-      ["--eprofile", srgb_profile_path, "--delete"]
-    end
-
-    # Params to add on to end of JPG output path, as in:
-    # `vips convert ... -o something.jpg[Q=85]`
-    #
-    # If we are in thumbnail mode, we strip all profile information for
-    # smaller files.
-    #
-    # Either way we create an interlaced JPG and optimize coding for smaller
-    # file size.
-    #
-    # @returns [String]
-    def vips_jpg_params
-      if thumbnail_mode?
-        "[Q=#{@jpeg_q },interlace,optimize_coding,strip]"
-      else
-        # could be higher Q for downloads if we want, but we don't right now
-        # We do avoid striping metadata, no 'strip' directive.
-        "[Q=#{@jpeg_q },interlace,optimize_coding]"
-      end
-    end
-
+    # Creates a thumbnail of the specified width.
+    # If you want to add a border later on, you want to pass in a
+    # slightly smaller width to accomodate that.
     def thumbnail(width, input, output)
+      profile_normalization_args=["--eprofile", srgb_profile_path, "--delete"]
+      vips_jpg_params="[Q=#{@jpeg_q },interlace,optimize_coding,strip]"
       args = if width
         # Due to bug in vips, we need to provide a height constraint, we make
         # really huge one million pixels so it should not come into play, and
@@ -110,14 +65,14 @@ module Kithe
         [
           vips_thumbnail_command,
           input.path,
-          *maybe_profile_normalization_args,
+          *profile_normalization_args,
           "--size", "#{width}x1000000",
           "-o", "#{output.path}#{vips_jpg_params}"
         ]
       else
         [ vips_command, "copy",
           input.path,
-          *maybe_profile_normalization_args,
+          *profile_normalization_args,
           "#{output.path}#{vips_jpg_params}"
         ]
       end
@@ -139,16 +94,18 @@ module Kithe
       @cmd.run(*args)
     end
 
-    def add_border(old_width, old_height, input, output)
-      border_color = "5, 9, 57" # #050939
+    # Add a border to the thumbnail.
+    # The resulting file is taller and wider by @border_pixels * 2
+    def add_border(initial_width, input, output)
+      border_color = "0, 0, 0" # black until further notice
+      initial_height = image_height(input)
+      new_width =  initial_width  + @border_pixels * 2
+      new_height = initial_height + @border_pixels * 2
       args = [vips_command,
        'embed',
-        input.path,
-        output.path,
-        @border_width,
-        @border_width,
-        old_width + @border_width * 2,
-        old_height + @border_width * 2,
+        input.path, output.path,
+        @border_pixels, @border_pixels,
+        new_width, new_height,
         '--extend', 'background',
         '--background', border_color
       ]
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -11,34 +11,30 @@ class Asset < Kithe::Asset
     small: 800
   }
 
-  # TODO fix this...
-  # require '/Users/erubeiz/Work/code/scihist_digicoll/app/derivative_transformers/vips_cli_pdf_to_jpeg.rb'
-
-  # define thumb derivatives for TIFF and other image input: :thumb_mini, :thumb_mini_2X, etc.
+  # define thumb derivatives for TIFF, PDF, and other image input: :thumb_mini, :thumb_mini_2X, etc.
   THUMB_WIDTHS.each_pair do |key, width|
+    ['image', 'application/pdf'].each do |mime_type|
+    # Single-width thumbnails
     define_derivative("thumb_#{key}", content_type: "image") do |original_file|
       Kithe::VipsCliImageToJpeg.new(max_width: width, thumbnail_mode: true).call(original_file)
     end
     define_derivative("thumb_#{key}", content_type: "application/pdf") do |original_file|
-      Kithe::VipsCliPdfToJpeg.new(max_width: width, thumbnail_mode: true).call(original_file)
+      Kithe::VipsCliPdfToJpeg.new(max_width: width).call(original_file)
     end
+    # Double-width thumbnails
     define_derivative("thumb_#{key}_2X", content_type: "image") do |original_file|
       Kithe::VipsCliImageToJpeg.new(max_width: width * 2, thumbnail_mode: true).call(original_file)
     end
     define_derivative("thumb_#{key}_2X", content_type: "application/pdf") do |original_file|
-      Kithe::VipsCliPdfToJpeg.new(max_width: width * 2, thumbnail_mode: true).call(original_file)
+      Kithe::VipsCliPdfToJpeg.new(max_width: width * 2).call(original_file)
+    end
     end
   end
-
-  # TODO define thumb derivatives for PDFs.
 
   # Define download derivatives for TIFF and other image input.
   IMAGE_DOWNLOAD_WIDTHS.each_pair do |key, width|
     define_derivative("download_#{key}", content_type: "image") do |original_file|
       Kithe::VipsCliImageToJpeg.new(max_width: width).call(original_file)
-    end
-    define_derivative("download_#{key}", content_type: "application/pdf") do |original_file|
-      Kithe::VipsCliPdfToJpeg.new(max_width: width).call(original_file)
     end
   end
 
