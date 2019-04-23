79f2dd113ea84bee36c1d1de169627dd9208d35a
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-04-23 16:28:37 -0400

    SearchBuilder notes on TODO local customizations from chf_sufia

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+#
+# TODO: chf_sufia mixin's or equivalents for:
+# * SearchBuilder::RestrictAdminSearchFields => Makes sure admin notes are only searchable if logged in
+# * SearchBuilder::PublicDomainFilter => makes the URL param put in by our "public domain only" checkbox
+#   has an effect on search
+# * SearchBuilder::SyntheticCategoryLimit => something with making our 'topics'/synthetic categories
+#   work as limits, probably just for showing the main page for a 'topic'
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
 
