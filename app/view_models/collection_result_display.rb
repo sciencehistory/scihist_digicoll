e97e2057414726a4f4b774acbce4ccf35c2d30e1
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-05-13 19:04:04 -0400

    Basic working CollectionResultDisplay view model, sharing same ERB template as work

--- app/view_models/collection_result_display.rb
+++ app/view_models/collection_result_display.rb
@@ -2,4 +2,23 @@ class CollectionResultDisplay < ViewModel
   def display
     render "/view_models/index_result", model: model, view: self
   end
+
+  # TODO, link to Collections page, when it exists
+  def display_genres
+    ["Collection"]
+  end
+
+  # TODO, unify and eliminate from here
+  def display_permission_badge
+    ""
+  end
+
+  def additional_title
+    []
+  end
+
+  def part_of_elements
+    []
+  end
+
 end
