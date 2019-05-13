ca66b325831666fba9201f01f3c24cc8ab96dbcd
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-05-13 17:56:07 -0400

    placeholder controller for front-end work#show

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,6 @@
+# FRONT-END controller for public Works views -- mostly just the #show action.
+# Not to be confused with admin/works_controller for staff/management/edit views.
+class WorksController < ApplicationController
+  def show
+  end
+end
