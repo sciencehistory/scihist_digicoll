db08e3842ae9c5c0d3f111a91ba05bdfc712308c
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-05-13 18:01:44 -0400

    initial view_model based experiment for work result item
    Not yet including all elements.
    Won't yet work for collections.

---
+++ app/view_models/work_result_display.rb
@@ -0,0 +1,42 @@
+class WorkResultDisplay < ViewModel
+  def display
+    render "/view_models/index_result", model: model, view: self
+  end
+
+  def has_genres?
+    model.respond_to?(:genre) && model.genre.present?
+  end
+
+  def display_genres
+    # TODO, probably a better Blacklight-provided method for generating the url?
+    safe_join(
+      model.genre.map { |g| link_to g, search_catalog_path(f: { genre_facet: [g] }) },
+      ", "
+    )
+  end
+
+  # An array of elements for "part of" listing, includes 'parent' in a link,
+  # or "source" in italics
+  def part_of_elements
+    @part_of_elements ||= [].tap do |arr|
+      if model.parent.present?
+        arr << link_to(model.parent.title, work_path(model.parent))
+      end
+      if model.source.present?
+        arr << content_tag("i", model.source)
+      end
+    end
+  end
+
+  # TODO, this needs to be shared with other places, move to helper? Unify with
+  # existing helper in ApplicationHelper#publication_badge?
+  def display_permission_badge
+    unless model.published
+      content_tag("small", class: "chf-results-list-item-permission") do
+        "private"
+      end
+    end
+  end
+
+
+end
