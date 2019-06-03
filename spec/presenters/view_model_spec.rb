ad6c0e83b3419097db6c6ee03bb998de2fdf70c6
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-06-03 15:54:58 -0400

    more careful scoping for test class

--- spec/presenters/view_model_spec.rb
+++ spec/presenters/view_model_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 describe ViewModel do
 
-  class WidgetFormatter < ViewModel
-    valid_model_type_names 'String', 'NilClass'
+  class self::WidgetFormatter < ViewModel
+   valid_model_type_names 'String', 'NilClass'
     def format_the_widget
       return "formatted widget"
     end
   end
 
   it "passes basic smoke test" do
-    expect(WidgetFormatter.new('unformatted widget').format_the_widget).to eq "formatted widget"
+    result = self.class::WidgetFormatter.
+      new('unformatted widget').
+      format_the_widget
+    expect(result).to eq "formatted widget"
   end
 
   it "checks the type of constructor args belong to the list provided" do
     expect {
-      WidgetFormatter.new(123).format_the_widget
+      self.class::WidgetFormatter.new.format_the_widget
     }.to raise_error ArgumentError
   end
 
