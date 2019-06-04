54f58eb063a62aa6f7e1f9990bb8088033c121f2
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-06-04 15:08:56 -0400

    Better tests and fixing a couple other details per jrochkind's suggestions.

--- app/presenters/work_provenance_display.rb
+++ app/presenters/work_provenance_display.rb
@@ -1,7 +1,5 @@
-
 # Display the provenance of a work on the front end:
 # WorkProvenanceDisplay.new(work).display
-
 class WorkProvenanceDisplay < ViewModel
   valid_model_type_names 'Work'
   def display
@@ -18,15 +16,14 @@ class WorkProvenanceDisplay < ViewModel
     DescriptionDisplayFormatter.new(@provenance_notes).format
   end
 
+  private
 
   # To split the provenance field into regular provenance info and a set of notes,
   # use as a delimiter the string NOTES, in all caps, followed by an optional colon.
   #
   # The regular expression also captures extra blank space
   # before and after the match, as well as before and after the word "NOTES".
-
   def split_provenance
-    raise ArgumentError.new("Don't call this on a blank string.") if model.provenance.blank?
     model.provenance.split(/\s*\n\s*(?:NOTES|Notes):?\s*\n\s*/, 2)
   end
 
