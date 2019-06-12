94312dce5e676f8ab5ecbd73b977247538d9d915
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-06-12 16:01:05 -0400

    add public-domain-filter support to search builder

---
+++ app/models/search_builder/public_domain_filter.rb
@@ -0,0 +1,22 @@
+class SearchBuilder
+  # An extension to Blacklight's SearchBuilder (which is locally generated in our app), such
+  # that a "filter_public_domain=1" variable in app query params is translated to restricting
+  # to just public domain items.
+  module PublicDomainFilter
+    extend ActiveSupport::Concern
+
+    included do
+      self.default_processor_chain += [:public_domain_filter]
+    end
+
+    def public_domain_filter(solr_params)
+      # look for the public domain only checkbox
+      if blacklight_params.fetch("filter_public_domain", 0).to_i > 0
+        solr_params[:fq] ||= []
+        public_domain_filter = "{!term f=rights_facet}http://creativecommons.org/publicdomain/mark/1.0/"
+        solr_params[:fq] << public_domain_filter
+      end
+    end
+
+  end
+end
