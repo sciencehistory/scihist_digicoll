ece1a3bd288824a47e586140fc21a7af1e5ed8cb
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-06-20 15:29:05 -0400

    fix more-facets link for search-inside collections results
    Terrible hacky override of Blacklight. Copied from chf_sufia, updated for BL7

--- app/controllers/collection_show_controller.rb
+++ app/controllers/collection_show_controller.rb
@@ -12,6 +12,34 @@ class CollectionShowController < CatalogController
 
   # index action inherited from CatalogController, that's what we use.
 
+
+  #Override from Blacklight: displays values and pagination links for a single facet field
+  #
+  # We need to override to change URL to get facet_id out of :id, which we use for our collection.
+  # Need to copy-and-paste-and-change implementation, which is unfortunate.
+  def facet
+    unless params.key?(:facet_id)
+      redirect_back fallback_location: { action: "index", id: params[:id] }
+      return
+    end
+
+    @facet = blacklight_config.facet_fields[params[:facet_id]]
+    raise ActionController::RoutingError, 'Not Found' unless @facet
+
+    @response = search_service.facet_field_response(@facet.key)
+    @display_facet = @response.aggregations[@facet.field]
+    @pagination = facet_paginator(@facet, @display_facet)
+    respond_to do |format|
+      format.html do
+        # Draw the partial for the "more" facet modal window:
+        return render layout: false if request.xhr?
+        # Otherwise draw the facet selector for users who have javascript disabled.
+      end
+      format.json
+    end
+  end
+
+
   private
 
   def check_auth
@@ -31,5 +59,15 @@ class CollectionShowController < CatalogController
 
   def set_collection
     @collection = Collection.find_by_friendlier_id!(params[:id])
+  end
+
+  # override from Blacklight to put the facet id in :facet_id instead of :id, so we can keep
+  # :id for our parent collection id.
+  # Goes with overridden #facet above.
+  def search_facet_path options = {}
+    if options.has_key?(:id)
+      options[:facet_id] = options.delete(:id)
+    end
+    super(options)
   end
 end
