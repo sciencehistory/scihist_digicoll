334ac261a2db584e24d969660555d583db5df1b9
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-06-25 15:39:40 -0400

    work show page with much of the content
    Not yet tested.
    Not yet styled.
    Still missing significant trickier functionality

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,25 @@
 # FRONT-END controller for public Works views -- mostly just the #show action.
 # Not to be confused with admin/works_controller for staff/management/edit views.
 class WorksController < ApplicationController
+  before_action :set_work, :check_auth
+
+
   def show
+
   end
+
+  private
+
+  def set_work
+    @work = Work.find_by_friendlier_id!(params[:id])
+  end
+
+  def check_auth
+    authorize! :read, @work
+  end
+
+  def decorator
+    @decorator = WorkShowDecorator.new(@work)
+  end
+  helper_method :decorator
 end
