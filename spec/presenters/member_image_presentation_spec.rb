2ee26843d44f776d58a482ed8601fdd9148da9fb
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-07-02 15:15:24 -0400

    MemberImagePresentation has fail-safe access control check

--- spec/presenters/member_image_presentation_spec.rb
+++ spec/presenters/member_image_presentation_spec.rb
@@ -18,5 +18,33 @@ describe MemberImagePresentation, type: :decorator do
         expect(wrapper_div).to have_selector(".action-item-bar .action-item.view .btn")
       end
     end
+
+    describe "non-published asset without auth" do
+      let(:member) { create(:asset, published: false) }
+
+      it "outputs only placeholder" do
+        expect(wrapper_div).to be_present
+
+        expect(wrapper_div).to have_selector(".thumb img.no-access-placeholder")
+
+        expect(wrapper_div).not_to have_selector(".action-item-bar .action-item.downloads .btn")
+        expect(wrapper_div).not_to have_selector(".action-item-bar .action-item.view .btn")
+      end
+    end
+  end
+
+  describe "with child work" do
+    describe "with non-published representative" do
+      let(:member) { create(:work, representative: create(:asset, published: false)) }
+
+      it "outputs only placeholder" do
+        expect(wrapper_div).to be_present
+
+        expect(wrapper_div).to have_selector(".thumb img.no-access-placeholder")
+
+        expect(wrapper_div).not_to have_selector(".action-item-bar .action-item.downloads .btn")
+        expect(wrapper_div).not_to have_selector(".action-item-bar .action-item.view .btn")
+      end
+    end
   end
 end
