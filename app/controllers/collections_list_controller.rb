a9828b71fcff711cf2176895d3ff433b378d9bab
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-07-08 17:21:04 -0400

    explicitly asking for published collections rather than checking permissions per collection.

--- app/controllers/collections_list_controller.rb
+++ app/controllers/collections_list_controller.rb
@@ -1,7 +1,8 @@
 # Front-end controller to list public collections.
 class CollectionsListController < ApplicationController
   def index
-    @collections = Collection.all.with_representative_derivatives
+    @collections = Collection.where("published = true").
+      with_representative_derivatives
     render :template => "collections/index"
   end
 end
