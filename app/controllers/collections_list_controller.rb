40edddaba5e6b7d6ba031abe950219168a1d1189
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-07-08 17:15:10 -0400

    Using with_representative_derivatives scope when loading collections.

--- app/controllers/collections_list_controller.rb
+++ app/controllers/collections_list_controller.rb
@@ -1,11 +1,7 @@
 # Front-end controller to list public collections.
 class CollectionsListController < ApplicationController
-
   def index
-    # TODO filter collections the viewer shouldn't see.
-    # TODO avoid n+1 queries by preloading some stuff.
-    @collections = Collection.all
+    @collections = Collection.all.with_representative_derivatives
     render :template => "collections/index"
   end
-
 end
