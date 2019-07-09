1fa9ca2255f006a06c1564a6bd0156f1c84db9e3
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-07-09 16:27:02 -0400

    Fixed html indents; removing unneccessary scss variable definition; sorting collections alphabetically; basic front-end test

--- app/controllers/collections_list_controller.rb
+++ app/controllers/collections_list_controller.rb
@@ -1,7 +1,9 @@
 # Front-end controller to list public collections.
 class CollectionsListController < ApplicationController
   def index
-    @collections = Collection.where("published = true").
+    @collections = Collection.
+      where("published = true").
+      order(:title).
       with_representative_derivatives
     render :template => "collections/index"
   end
