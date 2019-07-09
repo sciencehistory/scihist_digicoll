78d5513021ac0c2b56cd7a6e7b560d9fee1c7f33
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-07-09 14:58:47 -0400

    register Rails mime-types we had in chf_sufia
    Can't hurt, just in case, especially cause we are using Rails Mime::Type for some filename determinations

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,9 +2,16 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+#
+Mime::Type.register 'application/x-endnote-refer', :endnote
 
 Mime::Type.register "application/x-research-info-systems", :ris
+Mime::Type.register "application/vnd.citationstyles.csl+json", :csl
 
 # We want mp3 not mpga
 Mime::Type.register "audio/mpeg", :mp3
 
+# Oddly, audio/flac isn't registered with IANA (and thus isn't available in many automated
+# databases), but other software often uses it anyway.
+Mime::Type.register "audio/flac",   :flac
+Mime::Type.register "audio/x-flac", :flac
