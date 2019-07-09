e907b9cfe1a6d680eea27af1a4450a3aaa90f0a7
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-07-09 11:18:31 -0400

    comments on DownloadsController

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,3 +1,36 @@
+# A controller for delivering our originals and assets to browser, via a redirect to an
+# S3 url.
+#
+# send to `/downloads/:asset_friendlier_id` (`downloads_path(asset)`)  or
+# `/downloads/:asset_friendlier_id/:derivative_key` (`downloads_derivative_path(asset, derivative_key)`)
+#
+# And a REDIRECT will be returned to an S3 asset, via a signed S3 url.
+#
+# The signed S3 urls generated will request a content-disposition header from S3, that
+# will set a download filename based on current parent work and/or Asset metadata. For
+# instance, based on Work title. This means it may not match the actual key/URL on S3,
+# and can change when Work/Asset metadata changes without needing to change actual key/URL
+# on S3.  See DownloadFilenameHelper for actual filename-generation logic, used here.
+#
+# However, since we are delivering a signed URL to do this (as well as give access to S3
+# assets that may not be public -- after verifying auth), it also means the particular
+# URL redirected to is unique to request, will expire, and is not generally cachable
+# (either by shared HTTP caches or even specific browser-caches -- since you will never
+# get the same URL twice).
+#
+# So this is an unfortuante tradeoff, and due to the redirect to an uncacheable URL, we
+# do NOT use this controller for actual image and media source (eg`src` attribute on an `img`)
+# for displaying in the browser -- for those we currently deliver the direct S3 URL to a
+# _public_ S3 asset.
+#
+# So this controller is usually used for acutal downloads, although sending
+# ?disposition=inline will result in an 'inline' disposition header, which we use for instance
+# for asking the browser to display a PDF directly (if the browser can), which we may use
+# for 'view' function on a PDF.  Even when being delivered with disposition inline, the
+# content-disposition header still has a filename set, so if the user chooses 'save as'
+# from their browser, they will still get our specified filename instead of just a filename
+# based on S3 URL. Which is neat.
+#
 class DownloadsController < ApplicationController
   # Will be sent to S3 as expires_in, seconds, max 1 week.
   URL_EXPIRES_IN = 2.days.to_i
