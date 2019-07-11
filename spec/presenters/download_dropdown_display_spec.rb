05db7b4c36dc00dbbf1ad43531a19d5ed8542497
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-07-11 13:23:32 -0400

    download dropdown can handle PDF options
    which is just Original, with a good label/subhead

--- spec/presenters/download_dropdown_display_spec.rb
+++ spec/presenters/download_dropdown_display_spec.rb
@@ -56,6 +56,30 @@ describe DownloadDropdownDisplay do
     end
   end
 
+  describe "with a PDF file" do
+    let(:asset) do
+      create(:asset_with_faked_file,
+        faked_content_type: "application/pdf",
+        faked_height: nil,
+        faked_width: nil,
+        faked_derivatives: [],
+        parent: build(:work, rights: "http://creativecommons.org/publicdomain/mark/1.0/")
+      )
+    end
+
+    it "renders just original option" do
+      expect(div).to be_present
+
+      expect(div).to have_selector(".dropdown-header", text: "Download selected document")
+      expect(div).to have_selector("a.dropdown-item", text: /Original/)
+
+      expect(div).not_to have_selector("a.dropdown-item", text: /Small JPG/)
+      expect(div).not_to have_selector("a.dropdown-item", text: /Medium JPG/)
+      expect(div).not_to have_selector("a.dropdown-item", text: /Large JPG/)
+      expect(div).not_to have_selector("a.dropdown-item", text: /Full-sized JPG/)
+    end
+  end
+
   describe "no rights statement" do
     let(:asset) { build(:asset, parent: build(:work)) }
     it "renders without error" do
