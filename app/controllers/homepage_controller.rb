00861c738de33fa0af014a1c84f0d20091ec076c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-07-15 14:54:02 -0400

    draft version of featured collection

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,4 +2,15 @@ class HomepageController < ApplicationController
   def index
 
   end
+
+
+  protected
+
+  def featured_collection_image_link(work_id, title)
+    work = Work.find_by_friendlier_id(work_id)
+    return  view_context.link_to(title, "#") if work.nil?
+    view_context.link_to(title, work_path(work_id))
+  end
+  helper_method :featured_collection_image_link
+
 end
