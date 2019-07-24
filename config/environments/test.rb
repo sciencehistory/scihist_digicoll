b14dd9470fd16a820fd0ffc32bd3a5285d175987
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-07-24 16:34:03 -0400

    Adding the cookie accept banner

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,4 +44,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  # Hide the GDPR "I accept" cookies banner.
+  config.hide_accept_cookies_banner = true
 end
