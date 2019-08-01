ff7573d7eb5955d33607f3115c7ca50c037e693a
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-08-01 12:56:56 -0400

    use Uppy from non-webpack-controlled dependency the right way

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
+// Uppy currently still being loaded via a script tag to CDN outside of webpacker,
+// with this we can still `import Uppy from 'uppy';` in webpacker sources to use it. `
+environment.config.externals = {
+  uppy: 'Uppy'
+}
+
 module.exports = environment
