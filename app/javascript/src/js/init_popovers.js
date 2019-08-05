11255f31e1e51bd922a744abad070e28a18e1581
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-08-05 16:49:29 -0400

    comment on non-imported jQuery dependency

--- app/javascript/src/js/init_popovers.js
+++ app/javascript/src/js/init_popovers.js
@@ -1,6 +1,8 @@
 // Bootstrap 4 popovers are opt-in, we need to activate them.
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 
+// Does use JQuery
+
 import domready from 'domready';
 
 console.log("here in init popovers");
