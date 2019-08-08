357503c44128610c302ce4038e7faa934396bd8e
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-08-08 14:59:55 -0400

    delete old DZI on file change

--- app/models/dzi_management.rb
+++ app/models/dzi_management.rb
@@ -137,6 +137,20 @@ class DziManagement
           DeleteDziJob.perform_later(asset.dzi_file.dzi_uploaded_file.id)
         end
       end
+    else
+      # file changed, need to delete an old dzi?
+      old_file_data, new_file_data = asset.file_data_previous_change
+      if old_file_data.present?
+        if old_file_data.kind_of?(String) # not sure why this happens, it should be JSON already
+          old_file_data = JSON.parse(old_file_data)
+        end
+        if old_file_data["id"] != new_file_data["id"] &&
+           old_md5 = old_file_data.dig("metadata", "md5")
+
+           old_id = DziManagement.new(asset, md5: old_md5).dzi_uploaded_file.id
+           DeleteDziJob.perform_later(old_id)
+         end
+      end
     end
   end
 
