c75909fc1adafaf8fa23a75b42f5fee775105ecb
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-08-09 09:37:32 -0400

    Cleaning up the test a bit prior to refactoring.

--- spec/system/audio_front_end_spec.rb
+++ spec/system/audio_front_end_spec.rb
@@ -8,37 +8,14 @@ describe "Audio front end", type: :system, js: true do # , solr:true do
 
   let(:assets) {
     (1..3).to_a.map do |i|
-      create(:asset_with_faked_file,
+      create(:asset_with_faked_file, :mp3,
         title: "Track #{i}",
         position: i - 1,
-        faked_content_type: "audio/x-flac",
-        faked_height: nil,
-        faked_width: nil,
-        faked_derivatives: [build(:faked_derivative, key: "small_mp3", uploaded_file:
-          build(:stored_uploaded_file, content_type: "audio/mpeg")
-        )],
         parent: parent_work
       )
     end
   }
 
-  let(:fake_file_data) do
-    { "id"=> SecureRandom.hex,
-      "storage"=>"kithe_derivatives",
-      "metadata"=>{"size"=>13960588,
-        "width"=>nil,
-        "height"=>nil,
-        "filename"=>"tnw8hhm_small_mp3.mpga",
-        "mime_type"=>"audio/mpeg",
-        "kithe_derivative_key"=>"small_mp3"}
-    }
-  end
-
-  before do
-    (0..2).to_a.map do |i|
-      allow(assets[i].derivatives.first.file).to receive(:data).and_return(fake_file_data)
-    end
-  end
   scenario "Non-staff user can see the playlist but not the regular item listings" do
     visit work_path(assets.first.parent.friendlier_id)
     within(".show-page-audio-playlist-wrapper") do
