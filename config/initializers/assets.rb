322efabf3e10793526cbc96942fcde3fbdbd13b5
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-08-27 16:31:10 -0400

    tell sprockets to compile our custom error page html

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+# We're going to use sprockets to compile static html error pages
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'html')
+Rails.application.config.assets.register_mime_type('text/html', '.html')
