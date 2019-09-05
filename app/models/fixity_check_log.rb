0e5a86608404b9eba5ea183b59c4aae3e218aff9
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-09-05 12:03:12 -0400

    adding naive prune method

--- app/models/fixity_check_log.rb
+++ app/models/fixity_check_log.rb
@@ -8,6 +8,9 @@
 # Files with nil checksums are ignored.
 
 class FixityCheckLog < ApplicationRecord
+
+  NUMBER_OF_RECENT_PASSED_CHECKS_TO_KEEP = 5
+
   belongs_to :asset
   validates_presence_of :asset
 
@@ -16,9 +19,18 @@ class FixityCheckLog < ApplicationRecord
     self.new(asset: asset).run_check
   end
 
+  # Returns in reverse chron order: first is most recent check.
   def self.logs_for(asset, checked_uri)
     FixityCheckLog.where(asset: asset, checked_uri: checked_uri).order('created_at desc, id desc')
   end
+
+  def self.prune_checks(asset)
+    return if asset.file.nil?
+    return if asset.file.url.nil?
+    byebug unless checks_its_ok_to_delete(asset).empty?
+    self.checks_its_ok_to_delete(asset).map(&:destroy)
+  end
+
 
   def failed?
     !passed?
@@ -44,6 +56,23 @@ class FixityCheckLog < ApplicationRecord
   end
 
   private
+
+  # Returns an array of checks that we don't want to keep.
+  # The current rules are:
+  # Never throw out FAILED checks
+  # Never throw out the earliest PASSED check
+  # Always keep N recent PASSED checks
+  def self.checks_its_ok_to_delete(asset)
+    checks = self.logs_for(asset, asset.file.url)
+    return [] if checks.empty?
+    # Throw all the passed checks INTO the trash.
+    trash = checks.select { | ch | ch.passed? }
+    # But then, pop the earliest passed check back OUT of the trash.
+    earliest_passed_check = trash.pop
+    # Finally, shift the most recent N passed checks back OUT of the trash.
+    trash.shift(NUMBER_OF_RECENT_PASSED_CHECKS_TO_KEEP)
+    return trash
+  end
 
   def expected_checksum
     asset.file.sha512
