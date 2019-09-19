da4d32d8ed3d3872db2e0ae10a90352197a71817
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-09-19 14:48:29 -0400

    link to file location in S3 console for fixity failed file

--- app/models/fixity_check.rb
+++ app/models/fixity_check.rb
@@ -20,4 +20,28 @@ class FixityCheck < ApplicationRecord
     passed
   end
 
+  # A link to the checked_uri in the AWS S3 Console. Kind of hacky and fragile, will break
+  # if AWS changes their console URLs, or if our assumptions weren't quite right in other
+  # ways, but it's so clever and useful when it works!
+  def checked_uri_in_s3_console
+    @checked_uri_in_s3_console ||= begin
+      original_storage = Shrine.storages[:store]
+      uri = URI.parse(checked_uri)
+
+      if original_storage.kind_of?(Shrine::Storage::S3)
+        region = ScihistDigicoll::Env.lookup(:aws_region)
+        bucket = original_storage.bucket.name
+        path_components = uri.path.split("/")
+        key_path = path_components.slice(1, path_components.length - 2)&.join("/")
+        end_key = path_components.last
+
+        "https://s3.console.aws.amazon.com/s3/buckets/#{bucket}/#{key_path}/?region=#{region}&tab=overview&prefixSearch=#{end_key}"
+      else
+        # Can't do it, sorry
+        nil
+      end
+    rescue URI::InvalidURIError
+      nil
+    end
+  end
 end
