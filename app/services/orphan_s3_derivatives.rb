8d0f8d91f069d7c6859b59611b7a034145aa904c
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-10-10 16:01:55 -0400

    orphan derivative checker checks every file

--- app/services/orphan_s3_derivatives.rb
+++ app/services/orphan_s3_derivatives.rb
@@ -1,6 +1,8 @@
+# TODO. Check all files not just the base prefix. To see if it is actually referenced by a Derivative model.
+
 class OrphanS3Derivatives
   # We put some other things on the 'derivatives' s3, that we want to ignore and not consider orphaned
-  IGNORE_PATHS = ["__sitemaps/"]
+  IGNORE_PATH_PREFIXES = ["__sitemaps/"]
 
   attr_reader :s3_iterator, :shrine_storage
 
@@ -10,22 +12,28 @@ class OrphanS3Derivatives
     @s3_iterator = S3PathIterator.new(
       shrine_storage: shrine_storage,
       show_progress_bar: show_progress_bar,
-      check_base_paths_only: true
+      progress_bar_total: derivative_count
     )
+  end
+
+  def derivative_count
+    @derivatives_count ||= Kithe::Derivative.count
   end
 
   def delete_orphans
     delete_count = 0
     s3_iterator.each_s3_path do |s3_path|
-      asset_id = asset_id(s3_path)
+      next if IGNORE_PATH_PREFIXES.any? {|p| s3_path.start_with?(p) }
 
-      if orphaned?(asset_id)
-        shrine_storage.delete_prefixed(asset_id)
-        s3_iterator.log "deleted derivatives at: #{shrine_storage.bucket.name}: #{s3_path}"
+      asset_id, derivative_key, shrine_path = parse_s3_path(s3_path)
+
+      if orphaned?(asset_id, derivative_key, shrine_path)
+        shrine_storage.delete(shrine_path)
+        s3_iterator.log "deleted derivative file at: #{shrine_storage.bucket.name}: #{s3_path}"
         delete_count += 1
       end
     end
-    $stderr.puts "\nDeleted #{delete_count} sets of orphaned derivatives"
+    $stderr.puts "\nDeleted #{delete_count} orphaned derivatives"
   end
 
   def report_orphans
@@ -33,11 +41,12 @@ class OrphanS3Derivatives
     orphans_found = 0
 
     report = s3_iterator.each_s3_path do |s3_path|
-      next if IGNORE_PATHS.include?(s3_path)
+      next if IGNORE_PATH_PREFIXES.any? {|p| p.start_with?(s3_path) }
 
-      asset_id = asset_id(s3_path)
 
-      if orphaned?(asset_id)
+      asset_id, derivative_key, shrine_path = parse_s3_path(s3_path)
+
+      if orphaned?(asset_id, derivative_key, shrine_path)
         orphans_found +=1
 
         if orphans_found == max_reports
@@ -45,33 +54,46 @@ class OrphanS3Derivatives
         elsif orphans_found < max_reports
           asset = Asset.where(id: asset_id).first
 
-          s3_iterator.log "orphaned derivatives"
+          s3_iterator.log "orphaned derivative!"
           s3_iterator.log "  bucket: #{shrine_storage.bucket.name}"
           s3_iterator.log "  s3 path: #{s3_path}"
           s3_iterator.log "  asset_id: #{asset_id}"
+          s3_iterator.log "  derivative_key: #{derivative_key}"
           if asset.nil?
             s3_iterator.log "  asset missing"
+          else
+            s3_iterator.log ""
+            deriv = Kithe::Derivative.where(asset_id: asset_id, key: derivative_key).first
+            s3_iterator.log "  derivative_pk: #{deriv&.id || 'missing'}"
+            s3_iterator.log "  derivative file path: #{deriv&.file&.id || 'missing'}"
           end
           s3_iterator.log ""
         end
       end
     end
 
-    $stderr.puts "\n\nTotal Asset count: #{report.asset_count}"
+    $stderr.puts "\n\nTotal Asset count: #{Asset.count}"
+    $stderr.puts "Kithe::Derivative.count: #{derivative_count}"
     $stderr.puts "Checked #{report.files_checked} files on S3"
     $stderr.puts "Found #{orphans_found} orphan files\n"
   end
 
   private
 
-  def asset_id(s3_path)
-    s3_path.split("/").last
+  def parse_s3_path(s3_path)
+    s3_path =~ %r{(([^/]+)/([^/]+)/[^/]+)\Z}
+
+    shrine_path = $1
+    asset_pk = $2
+    derivative_key = $3
+
+    return [asset_pk, derivative_key, shrine_path]
   end
 
 
-  def orphaned?(asset_id)
-    return true unless asset_id
+  def orphaned?(asset_id, derivative_key, shrine_id)
+    return true unless asset_id.present? && derivative_key.present?
 
-    ! Asset.where(id: asset_id).exists?
+    ! Kithe::Derivative.where(asset_id: asset_id, key: derivative_key).where("file_data ->> 'id' = ?", shrine_id).exists?
   end
 end
