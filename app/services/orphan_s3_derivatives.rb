6dc20de1ea1c301a49886f3e6024ab87d04fa40e
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-10-21 14:52:18 -0400

    S3 orphan finder comment docs

--- app/services/orphan_s3_derivatives.rb
+++ app/services/orphan_s3_derivatives.rb
@@ -1,11 +1,35 @@
-# TODO. Check all files not just the base prefix. To see if it is actually referenced by a Derivative model.
-
+# Derivatives are stored  on S3 at:
+#
+#     ./{asset_uuid_pk}/{derivative_type_name}/{random_string_suffix}
+#
+# Eg:
+#
+#     /00968a6c-957a-46a9-817a-a7893aeafed0/download_large/80276f556523f9619f3a5e3a473f609c.jpg
+#
+# The `./` may be bucket root, or may be at a prefix inside a bucket, depending on
+# how our `shrine-shrine_derivatives_storage` is configured.
+#
+# This class will use S3 API to iterate through ALL keys in configured shrine_store_storage,
+# for each one try to parse out the UUID-pk and derivatie key from path. Then it will look
+# in the postgres database to make sure a corresponding Derivative row exists, and points to the
+# found object on S3.
+#
+# If all those things are not true, the S3 key is considered "orphaned". This class can be used to
+# report on all discovered orphaned files, or to actually _delete_ orphaned files.  This class
+# is normally called from a rake task.
+#
+# Certain things we store in the derivatives bucket but which are not derivatives, are intentionally
+# ignored (eg in "__sitemaps/")
+#
+#
 class OrphanS3Derivatives
   # We put some other things on the 'derivatives' s3, that we want to ignore and not consider orphaned
   IGNORE_PATH_PREFIXES = ["__sitemaps/"]
 
   attr_reader :s3_iterator, :shrine_storage
 
+  # @param show_progress_bar [Boolean], default true, should we show a progress
+  #   bar with estimated progress.
   def initialize(show_progress_bar: true)
     @shrine_storage = ScihistDigicoll::Env.shrine_derivatives_storage
 
@@ -20,6 +44,8 @@ class OrphanS3Derivatives
     @derivatives_count ||= Kithe::Derivative.count
   end
 
+  # Deletes all found orphans, outputing to console what was deleted.
+  # If obj initializer show_progress_bar, there will be a progress bar.
   def delete_orphans
     delete_count = 0
     s3_iterator.each_s3_path do |s3_path|
@@ -36,6 +62,8 @@ class OrphanS3Derivatives
     $stderr.puts "\nDeleted #{delete_count} orphaned derivatives"
   end
 
+  # Prints out any orphans found, and some summary info. If show_progress_bar was
+  # set in an initializer, there will be a progress bar.
   def report_orphans
     max_reports = 40
     orphans_found = 0
