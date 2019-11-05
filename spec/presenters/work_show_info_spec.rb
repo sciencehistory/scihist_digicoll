92b01e35518fe5043ac7cddb09dc5be34b852460
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-11-05 12:28:38 -0500

    even more normalizing of bibIDs to prevent duplicate catalog links
    Ref #451

--- spec/presenters/work_show_info_spec.rb
+++ spec/presenters/work_show_info_spec.rb
@@ -8,4 +8,13 @@ describe WorkShowInfo do
       expect(links).to eq(["https://othmerlib.sciencehistory.org/record=b1075359"])
     end
   end
+
+  context "with weird almost-matching dups" do
+    let(:work) {  build(:work, external_id: {category: "bib", value: "B10722609"}, related_url: "https://othmerlib.sciencehistory.org/record=b1072260")}
+    it "still only has one link to opac" do
+      links = WorkShowInfo.new(work).links_to_opac
+      expect(links).to eq(["https://othmerlib.sciencehistory.org/record=b1072260"])
+    end
+  end
+
 end
