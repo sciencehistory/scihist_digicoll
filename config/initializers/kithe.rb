cf995d246fe8fd93bf1cf93acf46214e490b9431
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-11-07 11:20:11 -0500

    force try to use mediainfo and raise if not present

--- config/initializers/kithe.rb
+++ config/initializers/kithe.rb
@@ -1,3 +1,7 @@
 Kithe.indexable_settings.solr_url = ScihistDigicoll::Env.lookup!(:solr_url)
 # index to solr with solr `id` field being our friendlier_id
 Kithe.indexable_settings.solr_id_value_attribute = :friendlier_id
+
+# Insist on using `mediainfo` CLI for fallback content-type detection,
+# will raise if not present.
+Kithe.use_mediainfo = true
