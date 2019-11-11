a3d577ffb01c77d35cd5996325c5b3735c797da0
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-11-11 16:01:40 -0500

    preserve parent.representative when promoting to child work

--- spec/system/asset/convert_to_child_work_spec.rb
+++ spec/system/asset/convert_to_child_work_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe "Convert asset to child work", :logged_in_user, type: :system, js
   let(:conversion_source) { work.members[2] }
 
   it "converts" do
+    work.update(representative_id: conversion_source.id)
+
     original_position = conversion_source.position
     visit admin_asset_path(conversion_source)
 
@@ -14,6 +16,7 @@ RSpec.describe "Convert asset to child work", :logged_in_user, type: :system, js
       expect(page).to have_content("Asset promoted to child work")
     end.to change { Work.count }.by(1).and change { Asset.count }.by(0)
 
+    work.reload
     expect(conversion_source.reload.persisted?).to be(true)
     new_work = Work.order(:created_at).last
 
@@ -21,6 +24,7 @@ RSpec.describe "Convert asset to child work", :logged_in_user, type: :system, js
     expect(new_work.parent).to eq(work)
     expect(new_work.contained_by).to eq(work.contained_by)
     expect(new_work.position).to eq(original_position)
+    expect(work.representative_id).to eq(new_work.id)
 
     # all attr_json attributes
     expect(new_work.json_attributes).to eq(work.json_attributes)
