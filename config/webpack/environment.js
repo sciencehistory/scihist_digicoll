8a3a8d2beea107d23b2f80d952b674f43b755c53
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-11-19 18:48:50 -0500

    increase asset/entry point size limits for performance hint warnings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,14 @@ environment.config.externals = {
   jquery: 'jQuery'
 }
 
+// Default max sizes for getting warned are ~260000. This isn't bit enough
+// for our application entry point, mainly because of OpenSeadragon which is
+// ~130000 all by itself. We increase to 400000 for now; although we could
+// try to use techniques to reduce this, this is still probalby smaller than it
+// was in chf_sufia sprockets only.
+environment.config.performance = {
+  maxEntrypointSize: 400000,
+  maxAssetSize: 400000
+}
+
 module.exports = environment
