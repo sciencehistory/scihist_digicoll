c72cebefad54136f9e0d199f83a6c82a5ee475cc
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-11-27 09:12:43 -0500

    We actually don't want to raise mail delivery errors in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
   }
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ScihistDigicoll::Env.lookup!(:smtp_host),
