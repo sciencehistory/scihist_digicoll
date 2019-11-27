2581840b34ced66b31a64370a5c476c76dd51e4c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-11-27 09:27:59 -0500

    fall back to sendmail if smtp host is not present in local config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,13 +71,21 @@ Rails.application.configure do
   }
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: ScihistDigicoll::Env.lookup!(:smtp_host),
-    domain: 'sciencehistory.org',
-    ssl: false,
-    enable_starttls_auto: false
-  }
+  config.action_mailer.raise_delivery_errors = true
+
+  if ScihistDigicoll::Env.lookup(:smtp_host)
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: ScihistDigicoll::Env.lookup(:smtp_host),
+      domain: 'sciencehistory.org',
+      ssl: false,
+      enable_starttls_auto: false
+    }
+  else
+    config.action_mailer.delivery_method = :sendmail
+  end
+
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
