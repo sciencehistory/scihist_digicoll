5a86a5ce6ca4adcbd675a3219e6c8dce56e59859
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-12-19 10:56:37 -0500

    basic spec for existing behavior

---
+++ spec/controllers/admin/assets_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+RSpec.describe Admin::AssetsController, :logged_in_user, type: :controller do
+  let(:cached_file_param) do
+    bytestream = Rails.root + "spec/test_support/images/20x20.png"
+    Shrine.storages[:cache].upload(bytestream, "attached_files/sample.png")
+
+    [
+      {
+        "storage" => "cache",
+        "id" => "attached_files/sample.png",
+        "metadata" => {
+          "filename" => "20x20.png"
+        }
+      }.to_json
+    ]
+  end
+
+  let(:parent_work) { create(:work) }
+
+  describe "#attach_files" do
+    it "creates and attaches Asset" do
+      post :attach_files, params: { parent_id: parent_work, cached_files: cached_file_param }
+
+      cached_file_param.each do |json|
+        file_param = JSON.parse(json)
+        child = parent_work.members.find { |a| a.original_filename == file_param["metadata"]["filename"] }
+        expect(child).to be_present
+      end
+    end
+  end
+end
