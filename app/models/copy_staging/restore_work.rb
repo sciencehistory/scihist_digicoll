dc7f25a77768a5478cc29c043a0e0b93783a1e4a
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-01-15 10:54:27 -0500

    use concurrency in restore_work rake task for copy from staging

--- app/models/copy_staging/restore_work.rb
+++ app/models/copy_staging/restore_work.rb
@@ -10,17 +10,45 @@ module CopyStaging
   #
   #     CopyStaging::RestoreWork.new(json_file: File.open("path/to/json.json")).restore
   #
-  # WARNING: May not be safe for calling except in rake task, it changes attributes of the db
-  # connection, should not be used when it might be sharing db connection. Registers shrine
-  # storages not expected to persist past rake task. etc.
+  # CONTEXT WARNING: Meant for use by rake task!
+  #
+  # This was written for use by the rake task, just extracting out the code for better
+  # organization. Some choices were made that might cause problems if it were NOT run in a rake
+  # task. Like use of threads, changing postgres session attributes, registering shrine storages.
+  # All are fine in a short-lived single-purpose process, but might cause problems in
+  # a multi-purpose long-running web app process.
+  #
+  # ## CONCURRENCY WARNING
+  #
+  # We use some multi-threaded concurrency for the operations to copy files/bytestreams
+  # from staging to local dev.
+  #
+  # This works out well because when we are copying from S3 to S3, shrine optimizes to
+  # an S3 API "COPY" operation -- we aren't doing any work or IO locally, but do have to wait
+  # for confirmation from S3 that the copy succeeded. And it makes sense to do these waits in
+  # parallel, even massively parallel.
+  #
+  # But if the local storage is local file system or something other than S3, this
+  # could be disastrous. This object can take thread_pool_size as an init
+  # argument, but the wrapping rake task isn't currently written to exersize that.
   class RestoreWork
     REMOTE_STORE_STORAGE_KEY = :remote_store_storage
     REMOTE_DERIVATIVES_STORAGE_KEY = :remote_derivatives_storage
 
-    attr_accessor :json_file
+    attr_accessor :json_file, :thread_pool, :tracked_futures, :thread_pool_size
 
-    def initialize(json_file:)
+    # @param json_file path to file of json of the format we expect, normally
+    #   exported from correspoinding SerializeWork file.
+    #
+    # @param thread_pool_size how big a thread pool to use for our parallel
+    #   bytestrea copy operations. See docs at top of class. Defaults to 50,
+    #   could be faster if we made it unlimited, but could also be more disastrous.
+    def initialize(json_file:, thread_pool_size: 50)
       @json_file = json_file
+      @thread_pool_size = thread_pool_size
+
+      @thread_pool = Concurrent::FixedThreadPool.new(thread_pool_size)
+      @tracked_futures = Concurrent::Array.new
 
       # make sure they get registered
       remote_store_storage
@@ -42,7 +70,7 @@ module CopyStaging
             model_class = json_hash.keys.first.classify.constantize
             attributes  = json_hash.values.first
 
-            progress = "#{((i+1).to_f / model_count * 100).round(1)}%"
+            progress = "#{((i+1).to_f / model_count * 100).round(2)}%"
             puts "Saving #{model_id_string(model_class, attributes)} (#{progress})\n\n"
 
             model = model_class.new(attributes)
@@ -53,7 +81,20 @@ module CopyStaging
             elsif model_class <= Kithe::Derivative
               restore_derivative_file(model)
             end
+
+            # let's keep from getting too far in front of the thread pool --
+            # not totally sure this matters, and it does slowdown total throughput
+            # some, but keeps our progress % count more accurate the way we are
+            # currently doing it, and seems safer. The less often we block waiting,
+            # the more throughput we get, so we do a generous *8.
+            if tracked_futures.size > thread_pool_size * 8
+              puts "...Waiting for file copies to catch up...\n\n"
+              tracked_futures.each(&:value!)
+              tracked_futures.clear
+            end
           end
+          puts "...Waiting for file copies to catch up...\n\n"
+          tracked_futures.each(&:value!)
         ensure
           Kithe::Model.connection.execute("SET session_replication_role TO 'origin'")
         end
@@ -61,8 +102,6 @@ module CopyStaging
     end
 
     private
-
-
 
     # just a nice human-readable identification of the model being restored,
     # attributes vary depending on type.
@@ -84,17 +123,21 @@ module CopyStaging
     end
 
     def restore_asset_file(asset_model)
-      puts "  -> Copying original file for #{asset_model.class.name}/#{asset_model.friendlier_id}\n\n"
+      tracked_futures << Concurrent::Promises.future_on(thread_pool) do
+        puts "  -> Copying original file for #{asset_model.class.name}/#{asset_model.friendlier_id}\n\n"
 
-      remote_file = Shrine::UploadedFile.new(asset_model.file.data.merge("storage" => REMOTE_STORE_STORAGE_KEY))
-      Shrine.storages[:store].upload(remote_file, asset_model.file.id)
+        remote_file = Shrine::UploadedFile.new(asset_model.file.data.merge("storage" => REMOTE_STORE_STORAGE_KEY))
+        Shrine.storages[:store].upload(remote_file, asset_model.file.id)
+      end
     end
 
     def restore_derivative_file(derivative_model)
-      puts "  -> Copying derivative file for #{derivative_model.class.name}/#{derivative_model.id}/#{derivative_model.key}\n\n"
+      tracked_futures << Concurrent::Promises.future_on(thread_pool) do
+        puts "  -> Copying derivative file for #{derivative_model.class.name}/#{derivative_model.asset_id}/#{derivative_model.key}\n\n"
 
-      remote_file = Shrine::UploadedFile.new(derivative_model.file.data.merge("storage" => REMOTE_DERIVATIVES_STORAGE_KEY))
-      Shrine.storages[:kithe_derivatives].upload(remote_file, derivative_model.file.id)
+        remote_file = Shrine::UploadedFile.new(derivative_model.file.data.merge("storage" => REMOTE_DERIVATIVES_STORAGE_KEY))
+        Shrine.storages[:kithe_derivatives].upload(remote_file, derivative_model.file.id)
+      end
     end
 
     def input_hash
