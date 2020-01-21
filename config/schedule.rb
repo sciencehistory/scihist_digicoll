c5e874b2790f662098d1a79bfb854ffed98a4adf
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-01-21 16:06:05 -0500

    removing fake checks from schedule.rb; adding copious logging for now.

--- config/schedule.rb
+++ config/schedule.rb
@@ -25,13 +25,3 @@ require File.expand_path(File.dirname(__FILE__) + "/../lib/scihist_digicoll/asse
 every :day, :at => ScihistDigicoll::ASSET_CHECK_WHENEVER_CRON_TIME, roles: [:cron] do
   rake "scihist:check_fixity"
 end
-
-# Temporary...
-every :tuesday, :at => '11:25 am', roles: [:cron] do
-  rake "scihist:check_fixity"
-end
-
-# Temporary...
-every :tuesday, :at => '3:40 pm', roles: [:cron] do
-  rake "scihist:check_fixity"
-end
