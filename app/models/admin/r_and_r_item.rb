4fc26a788417dcf5c07305f4f7038a7344364cdc
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-01-30 12:04:05 -0500

    de-uglification of the way constants STATUSES and COLLECTING_AREAS are referenced

--- app/models/admin/r_and_r_item.rb
+++ app/models/admin/r_and_r_item.rb
@@ -19,6 +19,14 @@ class Admin::RAndRItem < ApplicationRecord
   end
   validates :bib_number, length: { is: 8 }, allow_blank: true, starts_with_b: true
 
+  # Called by /app/views/presenters/_digitization_queue_item_status_form.html.erb
+  # Both this class and Admin::DigitizationQueueItem need to implement this,
+  # as the presenter is used to show the `status` of
+  # instances of both classes.
+  def available_statuses
+    Admin::DigitizationQueueItem::STATUSES
+  end
+
   COLLECTING_AREAS = Admin::DigitizationQueueItem::COLLECTING_AREAS
 
   validates :collecting_area, inclusion: { in: COLLECTING_AREAS }
