f4a1d67d86cc93d2920966b1408ebca014bd0914
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-02-24 15:00:36 -0500

    store combined audio derivs at location we want
    in a sub-prefix in derivatives bucket, but filed by work_id

--- app/uploaders/combined_audio_uploader.rb
+++ app/uploaders/combined_audio_uploader.rb
@@ -3,4 +3,20 @@
 # Pretty bare-bones, does set a custom storage location path.
 class CombinedAudioUploader < Shrine
 
+    # File it by work id, include the original filename in the S3 key, but also
+    # generated UUID and suffix. Locations on S3 will look something like:
+    #
+    #     [work_uuid_pk]/combined_[uuid].mp3
+    #
+    # ie
+    #
+    #     "9ed358d2-37b9-4cdc-8c3e-bcad4c0aee95/combined_b16f4aa27ec4e7d88fc73c1923ceebfb.mp3"
+    def generate_location(io, context)
+      # assumes we're only used with OralHistoryContent model, that has a work_id
+      work_id = context[:record].work_id
+      original_uuid = super
+
+      orig_filename = File.basename(context.dig(:metadata, "filename") || "", ".*")
+      "#{work_id}/#{orig_filename}_#{original_uuid}"
+    end
 end
