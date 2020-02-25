282e71d18e53f187253ebcbbca1be3e6f45569fe
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-02-25 14:29:19 -0500

    spec for OhmsTranscriptDisplay

---
+++ spec/presenters/ohms_transcript_display_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe OhmsTranscriptDisplay, type: :presenter do
+  let(:ohms_xml_path) { Rails.root + "spec/test_support/ohms_xml/duarte_OH0344.xml"}
+  let(:ohms_xml) { OralHistoryContent::OhmsXml.new(File.read(ohms_xml_path))}
+  let(:ohms_transcript_display) { OhmsTranscriptDisplay.new(ohms_xml) }
+
+  let(:transcript_text) { ohms_xml.parsed.at_xpath("//ohms:transcript", ohms: OralHistoryContent::OhmsXml::OHMS_NS).text }
+
+  it "produces good html" do
+    # we're just gonna spot check, while by the by ensuring that display does not raise.
+    parsed = Nokogiri::HTML.fragment(ohms_transcript_display.display)
+
+    expect(parsed.css("span.ohms-transcript-line").count).to eq(transcript_text.split("\n").count)
+    expect(parsed.css("p.ohms-transcript-paragraph").count).to eq(transcript_text.split("\n\n").count)
+    expect(parsed.css("a.ohms-transcript-timestamp").count).to eq(ohms_xml.sync_timecodes.count)
+
+    first_line = parsed.at_css("div.ohms-transcript-container > p.ohms-transcript-paragraph:first-child > span.ohms-transcript-line:first-child")
+    expect(first_line.to_html).to eq(
+      %Q{<span class="ohms-transcript-line" id="ohms_line_1"><span class="ohms-speaker">BROCK:</span> This is an oral history interview with Ron Duarte taking place on 13 June \n</span>}
+    )
+  end
+end
