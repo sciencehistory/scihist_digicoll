3f7d74a9c247941a15ce884369b47c68f4efc3ef
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-03-25 15:37:19 -0400

    removing audio asset derivatives

--- app/presenters/download_options/audio_download_options.rb
+++ app/presenters/download_options/audio_download_options.rb
@@ -13,14 +13,6 @@ module DownloadOptions
       # We don't use content_type in derivative option subheads,
       # cause it's in the main label. But do use it for original.
 
-      if mp3_deriv = asset.derivative_for(:small_mp3)
-        options << DownloadOption.with_formatted_subhead("Optimized MP3",
-          url: download_derivative_path(asset, :small_mp3),
-          analyticsAction: "download_optimized_mp3",
-          size: mp3_deriv.size
-        )
-      end
-
       if asset.stored?
         options << DownloadOption.with_formatted_subhead("Original file",
           url: download_path(asset),
