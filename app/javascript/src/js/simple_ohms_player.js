2fbd0e255cf8aa0037064c8bd49bab1ee03530f5
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-03-27 16:54:21 -0400

    Show start and duration for all segments; offer segment download.

--- app/javascript/src/js/simple_ohms_player.js
+++ app/javascript/src/js/simple_ohms_player.js
@@ -12,3 +12,20 @@ $(document).on("click", "*[data-ohms-timestamp-s]", function(event) {
   html5Audio.currentTime = seconds;
   html5Audio.play();
 });
+
+function showDuration() {
+    var startTime = parseFloat(this.dataset.ohmsTimestampS);
+    var nextTrack = jQuery(this).next('.track-listing')
+    var endTime = jQuery('audio')[0].duration;
+    if (nextTrack.length > 0) {
+        endTime = parseFloat(nextTrack[0].dataset.ohmsTimestampS);
+    }
+    durationInSeconds = endTime - startTime
+    durationInMinutes = durationInSeconds / 60.0
+    roundedDurationInMinutes = Math.round(durationInMinutes * 10.0) / 10.0
+    jQuery(this).find("*[data-role=track-duration]").html(roundedDurationInMinutes);
+}
+
+jQuery('audio')[0].onloadedmetadata = function () {
+    jQuery(".track-listing").each(showDuration);
+};
