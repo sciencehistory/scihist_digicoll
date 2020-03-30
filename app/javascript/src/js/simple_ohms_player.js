3ff99fdf8c985fa3e296bfc2c41e337324c28b35
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-03-30 08:48:14 -0400

    Better comments to explain hacky JS.

--- app/javascript/src/js/simple_ohms_player.js
+++ app/javascript/src/js/simple_ohms_player.js
@@ -13,19 +13,49 @@ $(document).on("click", "*[data-ohms-timestamp-s]", function(event) {
   html5Audio.play();
 });
 
+
+////////////
+////////////
+// START HACK
+//
+// Let's temporarily rig the front end to display the starting point
+// of each segment, as well as its duration.
+// The starting points are stored in the DB;
+// we can redily calculate the duration of each segment
+// by subtracting its start time from its end time.
+//
+// The way we get the end time of the last element is a hack that
+// we definitely won't want to keep in the long run:
+// loading the combined audio into the DOM and then asking the browser
+// to give us the length of the audio tag.
+//
+
 function showDuration() {
     var startTime = parseFloat(this.dataset.ohmsTimestampS);
-    var nextTrack = jQuery(this).next('.track-listing')
-    var endTime = jQuery('audio')[0].duration;
-    if (nextTrack.length > 0) {
+    var nextTrack = jQuery(this).next('.track-listing');
+    var endTime;
+    if (nextTrack.length) {
+        // this track ends when the next one begins
         endTime = parseFloat(nextTrack[0].dataset.ohmsTimestampS);
     }
-    durationInSeconds = endTime - startTime
-    durationInMinutes = durationInSeconds / 60.0
-    roundedDurationInMinutes = Math.round(durationInMinutes * 10.0) / 10.0
+    else {
+        // this track ends when the combined audio ends
+        endTime = jQuery('audio')[0].duration;
+    }
+    var durationInSeconds = endTime - startTime;
+    var durationInMinutes = durationInSeconds / 60.0;
+    var roundedDurationInMinutes = Math.round(durationInMinutes * 10.0) / 10.0;
     jQuery(this).find("*[data-role=track-duration]").html(roundedDurationInMinutes);
 }
 
+// Wait until we have the length metadata for the total play.
+// We're temporarily rigging the front end to display
 jQuery('audio')[0].onloadedmetadata = function () {
     jQuery(".track-listing").each(showDuration);
 };
+
+//
+//
+// END HACK
+////////////
+////////////
