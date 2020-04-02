5bab82f4f0ee1c6a6eeb9dc3b45f328b74ad03a2
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-04-02 10:35:01 -0400

    simpler and probably better solution: leave the downloads menu as is.

--- app/presenters/download_options/audio_download_options.rb
+++ app/presenters/download_options/audio_download_options.rb
@@ -7,33 +7,8 @@ module DownloadOptions
       super(asset)
     end
 
-    # How many of this asset's parent's members are audio assets.
-    def parent_audio_member_count
-      @audio_members ||= begin
-        members = asset.parent.members.with_representative_derivatives
-        members = members.where(published: true) if current_user.nil?
-        members.count { |m| m.leaf_representative&.content_type&.start_with?("audio/") }
-      end
-    end
-
     def options
       options = []
-
-      # If this is the only audio asset,
-      # let's also allow the user to download the "optimized mp3"
-      # which also happens to be the "combined audio" for this work.
-      #
-      # If there are more than one audio assets, the audio_work_show_decorator
-      # provides the user with way to download the combined audio in a separate section of the page.
-      if parent_audio_member_count == 1
-        if combined_mp3_deriv = asset.parent&.oral_history_content&.combined_audio_mp3
-          options << DownloadOption.with_formatted_subhead("Optimized MP3",
-            url: combined_mp3_deriv&.url(public:true),
-            analyticsAction: "download_optimized_mp3",
-            size: combined_mp3_deriv.size
-          )
-        end
-      end
 
       # We don't use content_type in derivative option subheads,
       # cause it's in the main label. But do use it for original.
