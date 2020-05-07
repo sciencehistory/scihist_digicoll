cd4e9ee4d4d649bb3b94971ecfb50e249bd66008
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-05-07 16:31:25 -0400

    more robust validation for ohms XML

--- app/models/oral_history_content/ohms_xml_validator.rb
+++ app/models/oral_history_content/ohms_xml_validator.rb
@@ -12,6 +12,8 @@ class OralHistoryContent
   #   # if not then
   #   validator.errors # => array of strings
   class OhmsXmlValidator
+    OHMS_NS = "https://www.weareavp.com/nunncenter/ohms"
+
     attr_reader :errors
 
     def initialize(xml_str)
@@ -21,21 +23,58 @@ class OralHistoryContent
     def valid?
       @errors = []
 
-      doc = Nokogiri::XML(@xml_str) { |config| config.strict }
+      @doc = Nokogiri::XML(@xml_str) { |config| config.strict }
 
-      self.class.xsd.validate(doc).each do |error|
+      self.class.xsd.validate(@doc).each do |error|
         @errors << "XSD validation error: #{error.message}"
       end
 
+      @errors = @errors + footnote_errors
+
       return @errors.empty?
+
     rescue Nokogiri::XML::SyntaxError => e
       @errors << "#{e.class}: #{e.message}"
       return false
+    end
+
+    def footnote_errors
+      errors = []
+      text_lines = text.split("\n")
+      text_lines_with_footnotes =  text_lines.select{ |l| l.include?('[[footnote]]') }
+      text_lines_with_footnotes.each do |line|
+        footnote_re = %r{\[\[footnote\]\] *(\d+?) *\[\[\/footnote\]\]}
+        line.scan(footnote_re).each do |f_match|
+          footnote_reference_number = f_match[0].to_i
+          if footnote_array[footnote_reference_number - 1] == nil
+            errors << "Reference to missing footnote #{footnote_reference_number}"
+          end
+        end
+      end
+      errors
     end
 
     # lazy load/parse
     def self.xsd
       @xsd ||= Nokogiri::XML::Schema(File.read(Rails.root + "lib/ohms/ohms.xsd"))
     end
+
+    def text
+      @text ||= begin
+        transcript = @doc.at_xpath("//ohms:transcript", ohms: OHMS_NS)
+        transcript ? transcript.text : ''
+      end
+    end
+
+    def footnote_array
+      @footnote_array ||= begin
+        footnotes_re = /\[\[footnotes\]\](.*)\[\[\/footnotes\]\]/m
+        return [] unless notes = text.scan(footnotes_re)[0]
+        one_footnote_re = /\[\[note\]\](.*?)\[\[\/note\]\]/m
+          notes[0].scan(one_footnote_re).
+          map{ |x| x[0].gsub(/\s+/, ' ').strip }
+      end
+    end
+
   end
 end
--- spec/models/oral_history_content/ohms_xml_validator_spec.rb
+++ spec/models/oral_history_content/ohms_xml_validator_spec.rb
@@ -24,11 +24,57 @@ describe OralHistoryContent::OhmsXmlValidator do
   end
 
   describe "valid OHMS xml" do
-    let(:xml_str) { File.read(Rails.root + "spec/test_support/ohms_xml/duarte_OH0344.xml")}
-
+    let(:xml_str) { File.read(Rails.root + "spec/test_support/ohms_xml/hanford_OH0139.xml")}
     it "is valid" do
+      foonote_re = /\[\[note\]\].*?\[\[\/note\]\]/m
+      xml_str_minus_first_footnote = xml_str.sub(foonote_re, '')
+      expect(xml_str.scan(foonote_re).count).to eq 2
+      expect(xml_str_minus_first_footnote.scan(foonote_re).count).to eq 1
+
       expect(validator.valid?).to be(true)
       expect(validator.errors).to be_empty
     end
   end
+
+  describe "reference to a missing footnote" do
+    let(:xml_str) { File.read(Rails.root + "spec/test_support/ohms_xml/hanford_OH0139.xml")}
+    it "is not valid" do
+      foonote_re = /\[\[note\]\].*?\[\[\/note\]\]/m
+      xml_str_minus_first_footnote = xml_str.sub(foonote_re, '')
+      expect(xml_str.scan(foonote_re).count).to eq 2
+      expect(xml_str_minus_first_footnote.scan(foonote_re).count).to eq 1
+      val = OralHistoryContent::OhmsXmlValidator
+        .new(xml_str_minus_first_footnote)
+      expect(val.valid?).to be(false)
+      expect(val.errors).to eq ["Reference to missing footnote 2"]
+    end
+  end
+
+  describe "no footnote section at all, but references present" do
+    let(:xml_str) { File.read(Rails.root + "spec/test_support/ohms_xml/hanford_OH0139.xml")}
+    it "is not valid" do
+      foonotes_re = /\[\[footnotes\]\].*?\[\[\/footnotes\]\]/m
+      xml_str_minus_all_footnotes = xml_str.sub(foonotes_re, '')
+      expect(xml_str.scan(foonotes_re).count).to eq 1
+      expect(xml_str_minus_all_footnotes.scan(foonotes_re).count).to eq 0
+      val = OralHistoryContent::OhmsXmlValidator
+        .new(xml_str_minus_all_footnotes)
+      expect(val.valid?).to be(false)
+      expect(val.errors).to eq ["Reference to missing footnote 1", "Reference to missing footnote 2"]
+    end
+  end
+
+  describe "space next to the number in a footnote reference" do
+    let(:xml_str) { File.read(Rails.root + "spec/test_support/ohms_xml/hanford_OH0139.xml")}
+    it "is tolerated" do
+      foonote_ref_re = %r{\[\[footnote\]\] *(\d+?) *\[\[\/footnote\]\]}
+      xml_str_with_spaces_in_footnote = xml_str.sub(foonote_ref_re, '[[footnote]]   2   [[/footnote]]')
+      expect(xml_str.scan(foonote_ref_re).count).to eq 2
+      expect(xml_str_with_spaces_in_footnote.scan(foonote_ref_re).count).to eq 2
+      val = OralHistoryContent::OhmsXmlValidator
+        .new(xml_str_with_spaces_in_footnote)
+      expect(val.valid?).to be(true)
+    end
+  end
+
 end
