f4b4bbc2ee8ebb1ad11b9dd831398dab34080343
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-05-11 15:48:06 -0400

    move derivative definitions to kithe 2.x in local custom shrine uploaders

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,51 +1,12 @@
 class Asset < Kithe::Asset
-
   has_many :fixity_checks, foreign_key: "asset_id", inverse_of: "asset", dependent: :destroy
 
-  THUMB_WIDTHS = {
-    mini: 54,
-    large: 525,
-    standard: 208
-  }
+  set_shrine_uploader(AssetUploader)
 
-  IMAGE_DOWNLOAD_WIDTHS = {
-    large: 2880,
-    medium: 1200,
-    small: 800
-  }
+  THUMB_WIDTHS = AssetUploader::THUMB_WIDTHS
 
-  # define thumb derivatives for TIFF, PDF, and other image input: :thumb_mini, :thumb_mini_2X, etc.
-  THUMB_WIDTHS.each_pair do |key, width|
-    # Single-width thumbnails
-    define_derivative("thumb_#{key}", content_type: "image") do |original_file|
-      Kithe::VipsCliImageToJpeg.new(max_width: width, thumbnail_mode: true).call(original_file)
-    end
-    define_derivative("thumb_#{key}", content_type: "application/pdf") do |original_file|
-      Kithe::VipsCliPdfToJpeg.new(max_width: width).call(original_file)
-    end
-    # Double-width thumbnails
-    define_derivative("thumb_#{key}_2X", content_type: "image") do |original_file|
-      Kithe::VipsCliImageToJpeg.new(max_width: width * 2, thumbnail_mode: true).call(original_file)
-    end
-    define_derivative("thumb_#{key}_2X", content_type: "application/pdf") do |original_file|
-      Kithe::VipsCliPdfToJpeg.new(max_width: width * 2).call(original_file)
-    end
-  end
+  IMAGE_DOWNLOAD_WIDTHS = AssetUploader::IMAGE_DOWNLOAD_WIDTHS
 
-  # Define download derivatives for TIFF and other image input.
-  IMAGE_DOWNLOAD_WIDTHS.each_pair do |key, width|
-    define_derivative("download_#{key}", content_type: "image") do |original_file|
-      Kithe::VipsCliImageToJpeg.new(max_width: width).call(original_file)
-    end
-  end
-
-  # and a full size jpg
-  define_derivative("download_full", content_type: "image") do |original_file, record:|
-    # No need to do this if our original is a JPG
-    unless record.content_type == "image/jpeg"
-      Kithe::VipsCliImageToJpeg.new.call(original_file)
-    end
-  end
 
   # Our DziFiles object to manage associated DZI (deep zoom, for OpenSeadragon
   # panning/zooming) file(s).
