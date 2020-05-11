b83038d1176c3a43c9b9938250913b0e41fb2578
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-05-11 16:46:18 -0400

    fix asset factory for shrine3/kithe2 derivatives

---
+++ spec/models/asset_factory_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+# Yeah, the asset factory is such a pain we need to spec it. In the future we should probably have
+# kithe supply re-usable factories.
+describe "FactoryBot Asset factory" do
+  describe ":asset_with_faked_file" do
+
+    describe "without derivatives" do
+      it "can build" do
+        asset = FactoryBot.build(:asset_with_faked_file, faked_derivatives: {})
+
+
+        expect(asset).to be_present
+        expect(asset.persisted?).to be(false)
+        expect(asset.file).to be_present
+        expect(asset.file.exists?).to be(true)
+        expect(asset.file.size > 0).to be(true)
+      end
+
+      it "can create" do
+        asset = FactoryBot.create(:asset_with_faked_file, faked_derivatives: {})
+
+        expect(asset).to be_present
+        expect(asset.persisted?).to be(true)
+        expect(asset.file).to be_present
+        expect(asset.file.exists?).to be(true)
+        expect(asset.file.size > 0).to be(true)
+      end
+    end
+
+    describe "with default derivatives" do
+      it "can build" do
+        asset = FactoryBot.build(:asset_with_faked_file)
+
+        expect(asset).to be_present
+        expect(asset.persisted?).to be(false)
+
+        expect(asset.file_derivatives).to be_present
+
+        [ "thumb_mini", "thumb_mini_2X", "thumb_large", "thumb_large_2X", "thumb_standard",
+          "thumb_standard_2X", "download_large", "download_medium", "download_small",
+          "download_full"].each do |key|
+            deriv = asset.file_derivatives[key.to_sym]
+            expect(deriv).to be_present
+            expect(deriv.exists?).to be(true)
+            expect(deriv.size > 0).to be(true)
+            expect(deriv.metadata).to be_present
+        end
+      end
+    end
+
+    describe "with specified derivatives hash" do
+      it "can build" do
+        asset = FactoryBot.build(:asset_with_faked_file,
+                  faked_derivatives: {
+                    jpeg: FactoryBot.build(:stored_uploaded_file, content_type: "image/jpeg"),
+                    pdf: FactoryBot.build(:stored_uploaded_file, content_type: "application/pdf"),
+                  })
+
+        expect(asset).to be_present
+        expect(asset.persisted?).to be(false)
+
+        expect(asset.file_derivatives.count).to eq(2)
+        expect(asset.file_derivatives[:jpeg].content_type).to eq("image/jpeg")
+        expect(asset.file_derivatives[:pdf].content_type).to eq("application/pdf")
+      end
+    end
+  end
+end
