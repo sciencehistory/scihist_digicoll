c90c629ccc29a3174c6c3ff0131b54a02f6182df
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-08-10 14:12:39 -0400

    add oral history available_by_request_mode as PG enum value
    Default 'off', nil not allowed

---
+++ db/migrate/20200810180533_add_available_by_request_mode_to_oral_history_content.rb
@@ -0,0 +1,7 @@
+class AddAvailableByRequestModeToOralHistoryContent < ActiveRecord::Migration[6.0]
+  def change
+    create_enum :available_by_request_mode_type, %w[off automatic manual_review]
+
+    add_column :oral_history_content, :available_by_request_mode, :available_by_request_mode_type, null: false, default: "off"
+  end
+end
