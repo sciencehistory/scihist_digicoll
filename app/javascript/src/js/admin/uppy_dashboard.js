98c56c2065d93d9b07d280fab0f89f74c1c51727
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-09-09 10:51:04 -0400

    replace remove button with icon

--- app/javascript/src/js/admin/uppy_dashboard.js
+++ app/javascript/src/js/admin/uppy_dashboard.js
@@ -116,7 +116,9 @@ function kithe_createFileUploader(container) {
 
     row.appendChild(document.createElement("td")).innerText = size;
     row.appendChild(document.createElement("td")).innerHTML =
-      "<button type='button' data-cached-file-remove='true' class='btn btn-outline-primary'>Remove</button>";
+      "<a type='button' data-cached-file-remove='true' href='#' aria-label='Remove' title='Remove'>" +
+        "<i class='fa fa-times-circle' style='font-size: 150%' aria-hidden='true'></i>" +
+      "</a>";
 
     return row;
   }
