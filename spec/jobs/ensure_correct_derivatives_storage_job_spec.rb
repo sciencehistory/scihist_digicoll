82ce9cf1888e96b1d2b98becc8f9d9ec2934ec0b
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-09-14 12:40:46 -0400

    EnsureCorrectDerivativesStorageJob

---
+++ spec/jobs/ensure_correct_derivatives_storage_job_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "EnsureCorrectDerivativesStorageJob" do
+  let(:job) { EnsureCorrectDerivativesStorageJob.new(asset) }
+
+  describe "with derivatives in incorrect public location" do
+    let(:sample_file_location) {  Rails.root + "spec/test_support/images/20x20.png" }
+    let(:asset) do
+      # create one with no derivatives
+      a = create(:asset_with_faked_file,
+        derivative_storage_type: "restricted",
+        faked_derivatives: {})
+
+      derivatives_on_public_storage = {
+        "thumb_small" => a.file_attacher.upload_derivative("thumb_small", File.open(sample_file_location), storage: :kithe_derivatives, delete: false),
+        "thumb_large" => a.file_attacher.upload_derivative("thumb_large", File.open(sample_file_location), storage: :kithe_derivatives, delete: false),
+      }
+
+      a.file_attacher.set_derivatives(derivatives_on_public_storage)
+      a.save!
+
+      expect(a.file_derivatives.values.collect(&:storage_key)).to all(eq(:kithe_derivatives))
+
+      a
+    end
+
+    it "copies and sets derivatives to correct location" do
+      job.perform_now
+
+      expect(asset.file_derivatives.values).to be_present
+
+      asset.file_derivatives.values.each do |deriv|
+        expect(deriv.storage_key).to eq(:restricted_kithe_derivatives)
+        expect(deriv.exists?).to be(true)
+      end
+    end
+  end
+end
