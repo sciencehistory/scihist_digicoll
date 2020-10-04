f35cfa40371db6f8c747709bc5f62fee2c5897f7
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-04 15:49:12 -0400

    Adding an actual model

---
+++ app/models/asset_derivative_storage_type_audit_log.rb
@@ -0,0 +1,5 @@
+# Logs a report of an audit that checks whether all assets have their derivatives stored
+# in the correct buckets.
+class AssetDerivativeStorageTypeAuditLog < ApplicationRecord
+  # Empty for now: all functionality is provided by the DB for now.
+end
