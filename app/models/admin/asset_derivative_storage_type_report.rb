ce015eb52329fe227bf67f7017bcaa7da162d87a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-05 09:15:40 -0400

    Renaming the report model and moving it to app/models/admin

--- app/models/admin/asset_derivative_storage_type_report.rb
+++ app/models/admin/asset_derivative_storage_type_report.rb
@@ -1,5 +1,6 @@
 # Logs a report of an audit that checks whether all assets have their derivatives stored
 # in the correct buckets.
-class AssetDerivativeStorageTypeAuditLog < ApplicationRecord
+
+class Admin::AssetDerivativeStorageTypeReport < ApplicationRecord
   # Empty for now: all functionality is provided by the DB for now.
 end
