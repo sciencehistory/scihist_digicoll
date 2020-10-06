8105e01077720b189b840c943760dd801481986e
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-10-06 16:57:44 -0400

    change to 1 puma worker by default

--- config/heroku_puma.rb
+++ config/heroku_puma.rb
@@ -1,10 +1,11 @@
 # Heroku-recommende configuration from
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
 #
-# By default will be creating 2 puma processes, each with 5 thread workers, so 10 workers total.
+# By default without setting heroku config this will create **1** puma processes, with 5 threads. (1
+# worker is all that fits for our app in a small heroku dyno?)
 # Setting heroku config vars can tune these.
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
