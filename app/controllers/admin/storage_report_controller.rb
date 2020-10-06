e48d6f6d605380b2f8f95064b26d29fe332a726b
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-06 16:20:58 -0400

    Only save one report

--- app/controllers/admin/storage_report_controller.rb
+++ app/controllers/admin/storage_report_controller.rb
@@ -1,7 +1,7 @@
 # Lists recent asset derivative storage audits.
 class Admin::StorageReportController < AdminController
   def index
-    @reports = Admin::AssetDerivativeStorageTypeReport.order(created_at: :desc)
+    @reports = Admin::AssetDerivativeStorageTypeReport.order(created_at: :desc).limit(1)
     render :storage_report
   end
 end
