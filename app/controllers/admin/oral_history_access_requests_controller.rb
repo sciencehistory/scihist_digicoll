ad74213aee48f759d7d8c393067523aa97e2bb8c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-12 10:54:13 -0400

    Removing staff-facing features; they will go in another PR.

--- app/controllers/admin/oral_history_access_requests_controller.rb
+++
@@ -1,7 +0,0 @@
-# STAFF FACING
-# Public facing actions are in app/controllers/oral_history_access_requests_controller.rb
-class Admin::OralHistoryAccessRequestsController < AdminController
-  def index
-    @oral_history_access_requests = Admin::OralHistoryAccessRequest.all.to_a
-  end
-end
