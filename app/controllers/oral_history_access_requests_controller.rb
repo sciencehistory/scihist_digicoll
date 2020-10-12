6f8b1b0d60e43a172295254a944a54444e48f345
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-12 17:06:04 -0400

    Just adding a controller check that it's a good idea to display the form for this particular OH.

--- app/controllers/oral_history_access_requests_controller.rb
+++ app/controllers/oral_history_access_requests_controller.rb
@@ -5,6 +5,7 @@ class OralHistoryAccessRequestsController < ApplicationController
   # GET /works/4j03d097t/request_oral_history_access
   def new
     @work = Work.find_by_friendlier_id(params['work_friendlier_id'])
+    check_assets_available_by_request
     @oral_history_access_request = Admin::OralHistoryAccessRequest.new
   end
 
@@ -12,6 +13,7 @@ class OralHistoryAccessRequestsController < ApplicationController
   def create
     work_friendlier_id = params['admin_oral_history_access_request'].delete('work_friendlier_id')
     @work = Work.find_by_friendlier_id(work_friendlier_id)
+    check_assets_available_by_request
     @oral_history_access_request = Admin::OralHistoryAccessRequest.new(oral_history_access_request_params)
     @oral_history_access_request.work = @work
     if @oral_history_access_request.save
@@ -31,4 +33,10 @@ private
       :work_friendlier_id, :patron_name, :patron_email,
       :patron_institution, :intended_use, :status, :notes)
   end
+
+  def check_assets_available_by_request
+    return if WorkFileListShowDecorator.new(@work).available_by_request_assets.present?
+    raise RuntimeError, "You can't request this work using this form: it's either freely available or not available to the public."
+  end
+
 end
