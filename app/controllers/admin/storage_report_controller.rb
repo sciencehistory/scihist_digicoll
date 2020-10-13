809bff13fdbb12165416dbfa2ac1114ac5ffbeb2
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-13 08:55:02 -0400

    fixing incorrect template (links were not actually linking to broken works)

--- app/controllers/admin/storage_report_controller.rb
+++ app/controllers/admin/storage_report_controller.rb
@@ -2,5 +2,8 @@
 class Admin::StorageReportController < AdminController
   def index
     @report = Admin::AssetDerivativeStorageTypeReport.order(created_at: :desc).first
+    @data = @report.data_for_report
+    @incorrectly_published_sample_array = @data['incorrectly_published_sample'].split(",")
+    @incorrect_storage_locations_sample_array = @data['incorrect_storage_locations_sample'].split(",")
   end
 end
