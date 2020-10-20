89e9112a7754b582bd639286f6fd1935a66417be
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-10-20 11:15:25 -0400

    missing comma

--- app/controllers/admin/oral_history_access_requests_controller.rb
+++ app/controllers/admin/oral_history_access_requests_controller.rb
@@ -24,7 +24,7 @@ class Admin::OralHistoryAccessRequestsController < AdminController
     data << [
       "Date",
       "Work",
-      "Work URL"
+      "Work URL",
       "Name of patron",
       "Email",
       "Institution",
