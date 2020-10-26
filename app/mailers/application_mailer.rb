90c204c526a517b1d606ba93afa9696bf8dc7c03
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-26 10:11:19 -0400

    Removing extra env key I added unneccessarily

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: (ScihistDigicoll::Env.lookup(:digital_collections_email_address) || "from@example.com")
+  default from: ScihistDigicoll::Env.lookup!(:digital_email_address)
   layout 'mailer'
+
+
+  def hostname
+    ScihistDigicoll::Env.lookup!(:app_url_base)
+  end
+
 end
