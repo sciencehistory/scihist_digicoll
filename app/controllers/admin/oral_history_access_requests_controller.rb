56ef7554a4d4afbb1272e25dc83068ee825e8315
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-10-27 09:59:11 -0400

    Adding a method on the model to safely look up the oral history number. (2)

--- app/controllers/admin/oral_history_access_requests_controller.rb
+++ app/controllers/admin/oral_history_access_requests_controller.rb
@@ -6,7 +6,6 @@ class Admin::OralHistoryAccessRequestsController < AdminController
   def index
     @oral_history_access_requests = Admin::OralHistoryAccessRequest.
     where('created_at > ?', 3.months.ago).order(created_at: :desc).to_a
-    @oral_history_numbers = oh_numbers(@oral_history_access_requests)
   end
 
   def report
@@ -19,9 +18,6 @@ class Admin::OralHistoryAccessRequestsController < AdminController
     # we think of this as an inclusive date range.
     end_date =  params.dig('report', 'end_date')
     scope = scope.where('created_at <= ?', (Time.parse(end_date) + 1.day)) if end_date.present?
-
-    oral_history_numbers = oh_numbers(scope.all)
-
     date_label = Date.today.to_s
     data = []
     data << [
@@ -40,8 +36,8 @@ class Admin::OralHistoryAccessRequestsController < AdminController
         request.created_at,
         request.work.title,
         work_url(request.work),
+        request.oral_history_number,
         request.patron_name,
-        oral_history_numbers[request.work.id],
         request.patron_email,
         request.patron_institution,
         request.intended_use,
@@ -59,19 +55,5 @@ class Admin::OralHistoryAccessRequestsController < AdminController
     ensure
       output_csv_file.close
     end
-
   end
-
-  private
-    # Fail gracefully if one of these works is missing an external ID for whatever reason.
-    def oh_numbers(requests)
-      # Note: see also oh_request.work.oral_history_content!.ohms_xml.accession
-      result = {}
-      requests.each do |oh_request|
-        external_id = oh_request.work.external_id.
-          find {|id| id.attributes["category"] == "interview"}
-        result[oh_request.work.id] = external_id.attributes['value'] if external_id.present?
-      end
-      result
-    end
 end
