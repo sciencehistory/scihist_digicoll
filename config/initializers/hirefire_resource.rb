3ff5ea40c2f74a7bf5eb64366691109aaf6dfa2b
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-12-15 15:29:38 -0500

    configure hirefire

---
+++ config/initializers/hirefire_resource.rb
@@ -0,0 +1,18 @@
+# The hirefire_resource gem has our app providing some info to hirefire.io for
+# heroku autoscaling.
+#
+# Let's only configure it to do anything if we're actually on heroku with HIREFIRE_TOKEN
+# configured. This config  won't be operative in dev or CI, OR if/when we're still
+# deploying to non-heroku infrastructure, hopefully minimizing any performance
+# impact on non-heroku.
+
+if ENV['HIREFIRE_TOKEN']
+  HireFire::Resource.configure do |config|
+
+    # https://help.hirefire.io/article/53-job-queue-ruby-on-rails
+    # https://github.com/hirefire/hirefire-resource
+    config.dyno(:worker) do
+      HireFire::Macro::Resque.queue
+    end
+  end
+end
