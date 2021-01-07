374b7fb0bfa6a9a52330aeabe20038e24bbefd89
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-01-07 16:46:27 -0500

    better comment

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -492,7 +492,8 @@ class CatalogController < ApplicationController
   # When a user (in practice, a bot) makes a call directly to /catalog/range_limit
   # rather than the usual /catalog?q=&range, this bypasses the preprocessing normally
   # performed by the `before_action` methods for #index, which supplies a
-  # range_start and range_end parameter and ensures they are in the correct order.
+  # range_start and range_end parameter and ensures they are in the correct order,
+  # then makes a second request to #range_limit .
   def screen_params_for_range_limit
     if (params['range_end'].nil?) ||
       (params['range_start'].nil?) ||
