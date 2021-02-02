6d6f82e1748a0a78cb3541bc8063286cd1fcafe9
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-02-02 12:26:32 -0500

    can't really make IE alert dismissable when right now none of our JS will load on IE at all, so the dismissable func ends up not working.

--- app/javascript/src/js/ie_warning_dismiss_remember.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-  $('#ieWarning').on('close.bs.alert', function () {
-    // Set a cookie so we can now not to show the warning again
-    document.cookie = "ieWarnDismiss=1";
-  })
-});
