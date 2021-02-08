0b7ef3d7e25ffdbc8a3201b49467a3574c6c6522
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-02-08 15:54:23 -0500

    remove superfluous OralHistoryDeliveryJob, can just call deliver_later ourselves

--- app/jobs/oral_history_delivery_job.rb
+++
@@ -1,8 +0,0 @@
-class OralHistoryDeliveryJob < ApplicationJob
-  def perform(request)
-    OralHistoryDeliveryMailer.
-      with(request: request).
-      oral_history_delivery_email
-      .deliver_now
-  end
-end
