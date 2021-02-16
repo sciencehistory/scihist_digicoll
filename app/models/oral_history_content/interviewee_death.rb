8ed9006136f0ff9834623b42b9d0a5881912766a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-02-16 16:39:04 -0500

    refactoring birth and death to separate "dates". Just better.

--- app/models/oral_history_content/interviewee_death.rb
+++ app/models/oral_history_content/interviewee_death.rb
@@ -1,23 +1,14 @@
 require 'attr_json'
 class OralHistoryContent
-  class IntervieweeDate
+  class IntervieweeDeath
     include AttrJson::Model
-    CATEGORY_VALUES = %w{birth death}
 
     validates_format_of :date, with: /\A\d{4}(-\d{2}(-\d{2})?)?\z/,
       message: "must be of format YYYY[-MM-DD]"
 
-    validates_presence_of :category
-
-    validates :category, inclusion:
-      { in: CATEGORY_VALUES,
-        allow_blank: false,
-        message: "%{value} is not a valid category. Choose birth or death." }
-
     # TODO validate `category` string can be either 'birth' or 'death'
     attr_json :date,  :string
     attr_json :place, :string
-    attr_json :category,  :string
 
   end
 end
