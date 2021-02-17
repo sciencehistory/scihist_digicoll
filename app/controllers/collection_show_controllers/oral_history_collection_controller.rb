7aac6e6f0377c9acfd4e31b3a0229603986be2a9
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-02-17 11:58:01 -0500

    a scheme to have specific controllers for public view of single collections

---
+++ app/controllers/collection_show_controllers/oral_history_collection_controller.rb
@@ -0,0 +1,9 @@
+# A sub-class of collection show controller that is specifically for the Oral History
+# collection -- it's routed to for that collection in our routes.rb
+#
+# It lets us override configuration and views.
+module CollectionShowControllers
+  class OralHistoryCollectionController < CollectionShowController
+
+  end
+end
