9e8898eb50af94453eaa504ff6e323221b646c2e
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-02-17 13:17:19 -0500

    customize Oral History collection facets

--- app/controllers/collection_show_controllers/oral_history_collection_controller.rb
+++ app/controllers/collection_show_controllers/oral_history_collection_controller.rb
@@ -5,5 +5,26 @@
 module CollectionShowControllers
   class OralHistoryCollectionController < CollectionShowController
 
+    # Add and remove some facet fields from inherited default configuration
+    configure_blacklight do |config|
+
+      # Remove some facets we don't want, not relevant in this specific collection search
+      config.facet_fields.delete("genre_facet")
+      config.facet_fields.delete("place_facet")
+      config.facet_fields.delete("department_facet")
+      config.facet_fields.delete("creator_facet") # we're going ot use Interviewer specifically instead
+
+      # re-label "date" per stakeholder request
+      config.facet_fields["year_facet_isim"].label = "Interview Date"
+
+      # Make "Rights" staff-only, at least fo rnow, with label matching
+      config.facet_fields["rights_facet"].tap do |facet_config|
+        facet_config.if = :current_user
+        facet_config.label = "Rights (Staff-only)"
+      end
+
+      # Some facets we don't use generally but we do want to use here.
+      config.add_facet_field "interviewer_facet", label: "Interviewer", limit: 5
+    end
   end
 end
