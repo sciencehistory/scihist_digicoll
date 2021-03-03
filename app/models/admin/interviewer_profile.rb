4532cbcb4bc2830138886ebf6ae335149075fd76
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-03-03 12:26:55 -0500

    add many to many between OralHistoryContent and InterviewerProfile
    Did this as a rails has_and_belongs_to_many which some say not to use (in favor of has_many :through), but it's just so much simpler, and this is already kinda confusing and we're tripping over rails already with like the 'Admin::' prefix, and I really don't think we're going to need has_many :through, we're going with this.

--- app/models/admin/interviewer_profile.rb
+++ app/models/admin/interviewer_profile.rb
@@ -1,3 +1,5 @@
 class Admin::InterviewerProfile < ApplicationRecord
+  has_and_belongs_to_many :oral_history_content
+
   validates_presence_of :name, :profile
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_165319) do
+ActiveRecord::Schema.define(version: 2021_03_03_165609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -119,6 +119,11 @@ ActiveRecord::Schema.define(version: 2021_03_02_165319) do
     t.text "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "interviewer_profiles_oral_history_content", id: false, force: :cascade do |t|
+    t.bigint "oral_history_content_id", null: false
+    t.bigint "interviewer_profile_id", null: false
   end
 
   create_table "kithe_model_contains", id: false, force: :cascade do |t|
