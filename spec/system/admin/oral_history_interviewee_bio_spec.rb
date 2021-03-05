4a4212f398abf30a4683e3f86330ff008271f8b4
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-03-05 13:10:19 -0500

    adding simple front-end test for interviewee metadata stuff

--- spec/system/admin/oral_history_interviewee_bio_spec.rb
+++ spec/system/admin/oral_history_interviewee_bio_spec.rb
@@ -1,58 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "Oral History Access Request Administration", :logged_in_user, type: :system, queue_adapter: :test  do
-  let(:preview_pdf) { create(:asset_with_faked_file, :pdf, published: true) }
-  let(:protected_pdf) { create(:asset_with_faked_file, :pdf, published: false, oh_available_by_request: true) }
-  let(:protected_mp3) { create(:asset_with_faked_file, :mp3, published: false, oh_available_by_request: true) }
+RSpec.describe "Oral History Access Interviewee bio", :logged_in_user, type: :system, queue_adapter: :test  do
 
-  let (:date) { [
-        OralHistoryContent::IntervieweeDate.new(date: '1923', category:'birth', place: 'poland',  ),
-        OralHistoryContent::IntervieweeDate.new(date: '2223', category: 'death', place: 'finland' )
-      ] }
-  let (:school) { [
-        OralHistoryContent::IntervieweeSchool.new(date: "1958", institution: 'Columbia University', degree: 'BA', discipline: 'Chemistry'),
-        OralHistoryContent::IntervieweeSchool.new(date: "1960", institution: 'Harvard University',  degree: 'MS', discipline: 'Physics')
-      ] }
-  let (:job) { [
-        OralHistoryContent::IntervieweeJob.new({start: "1962", end: "1965", institution: 'Harvard University',  role: 'Junior Fellow, Society of Fellows'}),
-        OralHistoryContent::IntervieweeJob.new( {start: "1965", end: "1968",  institution: 'Cornell University', role: 'Associate Professor, Chemistry'})
-      ] }
-  let (:honor) { [
-        OralHistoryContent::IntervieweeHonor.new(date: "1981", honor: 'Nobel Prize in Chemistry'),
-        OralHistoryContent::IntervieweeHonor.new(date: "1998", honor: 'Corresponding Member, Nordrhein-Westfälische Academy of Sciences')
-      ] }
+  let!(:work) { create(:oral_history_work, published: true) }
 
 
-  let!(:work) do
-    create(:oral_history_work, published: true).tap do |work|
-      work.members << preview_pdf
-      work.members << protected_pdf
-      work.members << protected_mp3
+  it "interviewee bio data shows up" do
+    visit admin_work_path(work, :anchor => "nav-oral-histories")
 
-      work.representative =  preview_pdf
+    interviewee_bio = find("h2", text: "Interviewee bio").ancestor('.card')
 
-      work.oral_history_content!.update(interviewee_date:   date)
-      work.oral_history_content!.update(interviewee_school: school)
-      work.oral_history_content!.update(interviewee_job:    job)
-      work.oral_history_content!.update(interviewee_honor:  honor)
+    within(interviewee_bio) do
 
-      work.save!
+      expect(page).to have_text("Edit")
+      expect(page).to have_text("Interviewee biography")
+      expect(page).to have_text("Place of Birth, CA, United States of America, 1923")
+      expect(page).to have_text("Died: Place of Death, NU, Canada, 2223")
+      expect(page).to have_text("MS, Physics, Harvard University")
+      expect(page).to have_text("1981: Nobel Prize in Chemistry")
+      click_link "Edit"
+    end
+
+    honors = find("fieldset.oral_history_content_interviewee_honor")
+
+    click_link "Add another Interviewee honor"
+    click_link "Add another Interviewee honor"
+
+    within(honors) do
+      within find_all(".nested-fields")[2] do
+        fill_in("Date", with: "2234")
+        fill_in("Honor", with: "Beatified")
+      end
+      within find_all(".nested-fields")[3] do
+        fill_in("Date", with: "2334-12-34")
+        fill_in("Honor", with: "Canonized")
+      end
+    end
+    find('input[name="commit"]').click
+
+    interviewee_bio = find("h2", text: "Interviewee bio").ancestor('.card')
+    within(interviewee_bio) do
+      expect(page).to have_text("Died: Place of Death, NU, Canada, 2223")
+      expect(page).to have_text("2234: Beatified")
+      expect(page).to have_text("2334-12-34: Canonized")
+      click_link "Edit"
     end
   end
 
-
-  context "A request exists for a manual_review work" do
-    let!(:oh_request) { Admin::OralHistoryAccessRequest.create!(
-      patron_name: "George Washington Carver",
-      patron_email: "george@example.org",
-      patron_institution: "Tuskegee Institute",
-      intended_use: "Recreational reading.",
-      work: work
-    )}
-
-    it "interviewee bio data shows up" do
-      visit admin_work_path(work, :anchor => "nav-oral-histories-bio")
-      # to be continued ...
-    end
-  end
 end
