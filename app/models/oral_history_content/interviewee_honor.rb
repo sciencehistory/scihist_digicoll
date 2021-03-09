9b514bc0489069fcd22baed75245396d766b0ca0
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-03-09 15:57:58 -0500

    OH microsite doesn't actually require any fields, so we're going out consistent

--- app/models/oral_history_content/interviewee_honor.rb
+++ app/models/oral_history_content/interviewee_honor.rb
@@ -2,10 +2,8 @@ class OralHistoryContent
   class IntervieweeHonor
     include AttrJson::Model
     validates_with StandardDateValidator, fields: [:date]
-    validates_presence_of :date
-    validates_presence_of :honor
+
     attr_json :date, :string
     attr_json :honor, :string
-
   end
 end
