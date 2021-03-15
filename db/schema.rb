eaee280659467e8d2bd08a2102f53fa80855eb00
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-03-15 11:49:20 -0400

    corrected db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_165609) do
+ActiveRecord::Schema.define(version: 2021_03_09_152257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
