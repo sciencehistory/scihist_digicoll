636d5185d62e6be4c493b54df6bf3cf52a971944
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-03-22 09:26:50 -0400

    Interviewee birth and death dates

--- bin/oh_microsite_export/export.sh
+++ bin/oh_microsite_export/export.sh
@@ -5,11 +5,11 @@ else
 fi
 EXPORT_CMD="mysql -u `cat local_database_user.txt` -s `cat local_database_name.txt` -p$DATABASE_PASSWORD"
 
-
 $EXPORT_CMD  < queries/name.sql             > data/name.json
 
 $EXPORT_CMD  < queries/birth_date_1.sql     > data/birth_date_1.json
 $EXPORT_CMD  < queries/birth_date_2.sql     > data/birth_date_2.json
+$EXPORT_CMD  < queries/birth_date_3.sql     > data/birth_date_3.json
 $EXPORT_CMD  < queries/birth_city.sql       > data/birth_city.json
 $EXPORT_CMD  < queries/birth_state.sql      > data/birth_state.json
 $EXPORT_CMD  < queries/birth_province.sql   > data/birth_province.json
@@ -17,6 +17,7 @@ $EXPORT_CMD  < queries/birth_country.sql    > data/birth_country.json
 
 $EXPORT_CMD  < queries/death_date_1.sql     > data/death_date_1.json
 $EXPORT_CMD  < queries/death_date_2.sql     > data/death_date_2.json
+$EXPORT_CMD  < queries/death_date_3.sql     > data/death_date_3.json
 $EXPORT_CMD  < queries/death_city.sql       > data/death_city.json
 $EXPORT_CMD  < queries/death_state.sql      > data/death_state.json
 $EXPORT_CMD  < queries/death_province.sql   > data/death_province.json
