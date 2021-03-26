a3bf7deb2fbdf1f4613c0afea78aede2298c546a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-03-26 14:23:19 -0400

    Removing Gemfile: using https://dev.mysql.com/doc/mysql-shell/8.0/en/mysql-shell-commands.html for json export

--- bin/oh_microsite_export/export_institutions.sh
+++
@@ -1,9 +0,0 @@
-if [ -f "local_database_password.txt" ]; then
-    DATABASE_PASSWORD=`cat local_database_password.txt`
-else
-    read -s -p "Enter the password for the local database : " DATABASE_PASSWORD
-fi
-EXPORT_CMD="mysql -u `cat local_database_user.txt` -s `cat local_database_name.txt` -p$DATABASE_PASSWORD"
-
-# We'll use this for the crosswalk to FAST headings
-$EXPORT_CMD  < queries/institutions.sql     > data/institutions.txt
