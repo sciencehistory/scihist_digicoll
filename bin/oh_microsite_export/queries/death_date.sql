16a8448885088cd5c8baa7984429d97ca9b6ebed
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-03-30 16:51:07 -0400

    make the DB do more work for birth and death dates.

---
+++ bin/oh_microsite_export/queries/death_date.sql
@@ -0,0 +1,34 @@
+select
+    field_interviewee_name_value interviewee_name,
+    field_data_field_interviewee_name.entity_id interview_entity_id,
+    field_data_field_interview_number.field_interview_number_value interview_number,
+    COALESCE (
+        LEFT(field_interviewee_death_date_value, 10),  -- YYY-MM-DD
+        LEFT(field_death_date_month_year_value,   7),  -- YYYY-MM
+        LEFT(field_death_date_year_only_value,    4)   -- YYYY
+    ) death_date
+from
+    field_data_field_interview_number,
+    field_data_field_interviewee_name
+LEFT JOIN
+    field_data_field_death_date_month_year target_table_1
+    ON
+        field_data_field_interviewee_name.entity_id =  target_table_1.entity_id
+    AND
+        field_data_field_interviewee_name.revision_id =  target_table_1.revision_id
+LEFT JOIN
+    field_data_field_death_date_year_only target_table_2
+    ON
+        field_data_field_interviewee_name.entity_id =  target_table_2.entity_id
+    AND
+        field_data_field_interviewee_name.revision_id = target_table_2.revision_id
+LEFT JOIN
+    field_data_field_interviewee_death_date target_table_3
+    ON
+        field_data_field_interviewee_name.entity_id =  target_table_3.entity_id
+    AND
+        field_data_field_interviewee_name.revision_id = target_table_3.revision_id
+WHERE
+    field_data_field_interviewee_name.entity_id =  field_data_field_interview_number.entity_id
+AND
+    field_data_field_interviewee_name.revision_id = field_data_field_interview_number.revision_id
