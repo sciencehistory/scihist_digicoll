8722fafcf7d14853dd948e796d59e30018c4504a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-04-02 10:34:09 -0400

    Make interviewer profile text optional; a name is enough to create a record

--- bin/oh_microsite_export/queries/interviewer_profile.sql
+++ bin/oh_microsite_export/queries/interviewer_profile.sql
@@ -1,11 +1,11 @@
-select
+SELECT
     interviewer_node.nid interviewer_id,
     REPLACE(interviewer_node.title,'Interviewer > ','') interviewer_name,
     interviewer_profile.body_value interviewer_profile
-from
-    node interviewer_node,
-    field_data_body interviewer_profile
+FROM
+    node interviewer_node
+LEFT JOIN field_data_body interviewer_profile
+    ON interviewer_node.nid = interviewer_profile.entity_id
+
 WHERE
     interviewer_node.type = 'interviewer'
-AND
-    interviewer_node.nid = interviewer_profile.entity_id
