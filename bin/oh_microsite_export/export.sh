9a10ada75e865438991e5c32a72aed596404fa3b
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-04-04 10:04:43 -0400

    adding query for url aliases; removing url alias column from name query

--- bin/oh_microsite_export/export.sh
+++ bin/oh_microsite_export/export.sh
@@ -10,10 +10,13 @@ EXPORT_DESTINATION=/tmp/ohms_microsite_import_data
 # A text file for converting institution names to FAST headings
 $EXPORT_CMD  < queries/institutions.sql     > $EXPORT_DESTINATION/institutions.txt
 
+# A query to create URL mappings:
+$EXPORT_CMD  < queries/url.sql             > $EXPORT_DESTINATION/url.json
+
 # Mapping check: contains name, source URL, source URL alias, and interview number
 $EXPORT_CMD  < queries/name.sql             > $EXPORT_DESTINATION/name.json
 
-# # Biographical data export:
+# Biographical data export:
 $EXPORT_CMD  < queries/birth_date.sql       > $EXPORT_DESTINATION/birth_date.json
 $EXPORT_CMD  < queries/birth_city.sql       > $EXPORT_DESTINATION/birth_city.json
 $EXPORT_CMD  < queries/birth_state.sql      > $EXPORT_DESTINATION/birth_state.json
