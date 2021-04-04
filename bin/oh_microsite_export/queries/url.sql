3e6ec905d3a3849aa95b464252c8db49ae2ada0b
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-04-04 10:04:43 -0400

    adding query for url aliases; removing url alias column from name query

--- bin/oh_microsite_export/queries/url.sql
+++ bin/oh_microsite_export/queries/url.sql
@@ -2,11 +2,13 @@ select
     field_interviewee_name_value interviewee_name,
     field_data_field_interviewee_name.entity_id interview_entity_id,
     CONCAT("https://oh.sciencehistory.org/node/", field_data_field_interviewee_name.entity_id) source_url,
+    CONCAT("https://oh.sciencehistory.org/", url_alias.alias) url_alias,
     field_interview_number_value interview_number,
     published.status published
 from
     field_data_field_interviewee_name,
     field_data_field_interview_number accession_number_table,
+    url_alias,
     node published
 WHERE
     field_data_field_interviewee_name.entity_id =  accession_number_table.entity_id
@@ -14,5 +16,7 @@ AND
     field_data_field_interviewee_name.revision_id = accession_number_table.revision_id
 AND
     published.nid = field_data_field_interviewee_name.entity_id
+AND
+    url_alias.source =  CONCAT('node/', field_data_field_interviewee_name.entity_id)
 AND
     published.status = 1
