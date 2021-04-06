1a76da607a2e6aea956894d349a1dd04cb515aa4
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-04-06 15:09:23 -0400

    allow editing request-by status if it's ALREADY set to something other than off, even if no non-published assets

--- app/presenters/work_manage_oral_history_available_by_request.rb
+++ app/presenters/work_manage_oral_history_available_by_request.rb
@@ -11,4 +11,11 @@ class WorkManageOralHistoryAvailableByRequest < ViewModel
   def private_asset_members
     @private_asset_members ||= work.members.find_all { |m| m.kind_of?(Asset) && !m.published? }
   end
+
+  # only show the panel to change by-request status if we have
+  # non-published items, OR if it's already set to something other than
+  # 'off'
+  def allow_change_by_request?
+    private_asset_members.present? || (work.oral_history_content && !work.oral_history_content.available_by_request_off?)
+  end
 end
