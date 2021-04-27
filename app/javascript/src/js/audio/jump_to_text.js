e2140b2d5a32b116a3679986f7188e9bf1372462
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-04-27 14:51:38 -0400

    Oral history audio player, scroll to media sync button

---
+++ app/javascript/src/js/audio/jump_to_text.js
@@ -0,0 +1,102 @@
+ // OK, but now if audio is playing, and we're on transcript or ToC tab, we actually want to
+// scroll to relevant portion...
+
+
+document.body.addEventListener("click", function (event) {
+  if (event.target.matches("*[data-trigger='ohms-jump-to-text']")) {
+    var player = document.querySelector("audio[data-role=ohms-audio-elem]");
+    var timeCodeSeconds = player.currentTime;
+    var activeTab = getActiveTab();
+
+    if (activeTab.id == "ohTocTab") {
+      var collapsible = findTocCollapsibleSection(timeCodeSeconds);
+      if (collapsible) {
+        if (collapsible.classList.contains("collapse")) {
+          $(collapsible).collapse("show");
+        }
+        // And scroll to the
+        scrollToElement(collapsible.closest(".card").querySelector(".card-header"));
+      }
+    } else if (activeTab) {
+      // if we have anything else, jump to transcript point, activating
+      // transcript tab first if needed.
+      if (activeTab.id != "ohTranscriptTab") {
+        $('*[data-toggle="tab"][href="#ohTranscript"]').tab("show");
+      }
+
+      var element = findTranscriptAnchor(timeCodeSeconds);
+      if (element) {
+        scrollToElement(element);
+      }
+    }
+  }
+});
+
+
+
+
+
+
+  //   if (tabId == "ohTranscriptTab") {
+  //     var anchor = findTranscriptAnchor(timeCodeSeconds);
+
+  //     if (anchor) {
+  //       scrollToElement(anchor);
+  //     }
+  //   } else if (tabId == "ohTocTab") {
+  //     var collapsible = findTocCollapsibleSection(timeCodeSeconds);
+  //     if (collapsible) {
+  //       if (collapsible.classList.contains("collapse")) {
+  //         $(collapsible).collapse("show");
+  //       }
+  //       // And scroll to the
+  //       scrollToElement(collapsible.closest(".card").querySelector(".card-header"));
+  //     }
+  //   }
+  // }
+
+
+
+
+
+function findTranscriptAnchor(timeInSeconds) {
+  return findOhmsTimestampElementIncluding(timeInSeconds, "a")
+}
+
+function findOhmsTimestampElementIncluding(timeInSeconds, baseSelector) {
+  var previousEl = undefined;
+  for (element of document.querySelectorAll(baseSelector + "[data-ohms-timestamp-s]")) {
+    if (element.getAttribute("data-ohms-timestamp-s") > timeInSeconds) {
+      return previousEl;
+    }
+    previousEl = element;
+  }
+  return undefined;
+}
+
+// Returns an element that you can call boostrap element.collapse()
+// on, for ToC section corresponding to timecode.
+function findTocCollapsibleSection(timeInSeconds) {
+  var button = findOhmsTimestampElementIncluding(timeInSeconds, "button");
+
+  // need to find it's parent collapsible
+  return button && button.closest("*[data-parent='#ohmsIndexAccordionParent']")
+}
+
+// What makes this slightly less than completely simple is allowing for
+// the fixed navbar on top; built-in browser functions will often wind
+// up scrolling it top of window, under navbar.
+//
+function scrollToElement(element) {
+  var targetDocumentXPosition = element.getBoundingClientRect().top + window.scrollY;
+  var navbarHeight = document.querySelector("#ohmsAudioNavbar").offsetHeight;
+
+  window.scrollTo({top: (targetDocumentXPosition - navbarHeight)});
+}
+
+// returns dom element for tab (the button that triggers the tab, that you can
+// call tab() on).
+function getActiveTab() {
+  var content = document.querySelector("#ohmsScrollable .tab-pane.active");
+  return content && document.querySelector(`*[data-toggle="tab"][href="#${content.id}"]`);
+}
