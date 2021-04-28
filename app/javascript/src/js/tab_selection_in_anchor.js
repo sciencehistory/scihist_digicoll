4eacf7aad18b3baab9c54138b3cfef302772809b
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-04-28 12:37:34 -0400

    preserve tab selection in url as eg #tab=ohTranscript
    We add tab selection to URL #anchor on selection, and on page load switch to that tab. This lets tab selection be reflected in the URL, so when you bookmark you get current tab. Oral History audio navbar is the use case, although it's active on all bootstrap tabs.
    
    We were already doing this prior to this PR, but putting the ID of the selected tab in teh anchor as the entire anchor, eg "#ohTranscript"
    
    we now use a key/value format instead, like "#tab=ohTranscript".  This gives us room to store more than one thing in anchor, in particular upcoming planned feature for linking specifically to a timecode, which might look like #tab=ohTranscript&t=1212" where 1212 is a number of seconds.

--- app/javascript/src/js/tab_selection_in_anchor.js
+++ app/javascript/src/js/tab_selection_in_anchor.js
@@ -1,22 +1,30 @@
-// Based on https://github.com/twbs/bootstrap/issues/25220
+// Informed by https://github.com/twbs/bootstrap/issues/25220
 //
-// We are using jQuery here, because bootstrap 4 JS for tabs uses/requires
-// it anyway.
+// We will store current tab selection in "anchor" as #tab=[tabID],
+// the query param format in anchor is compatible with storing additional things
+// there too, like timecodes. Then we tag on page load if we see an anchor. This
+// let's user bookmark specific tab selections. Motivated by Oral History Audio navbar.
+//
+// Uses jQuery here only for bootstrap 4 JS tabs which uses/requires it.
 
 import domready from 'domready';
 
 domready(function() {
   // if there's an #anchor in the URL referencing a bootstrap tab, make that tab selected.
   // should we limit to only certain data tags instead of all bootstrap tab links?
-  const anchor = window.location.hash;
+  var anchor = new URLSearchParams(window.location.hash.replace(/^#/, '')).get("tab");
   if (anchor) {
-    $(`*[data-toggle="tab"][href="${anchor}"]`).tab("show")
+    $(`*[data-toggle="tab"][href="#${anchor}"]`).tab("show")
   }
 
   // when showing on a bootstrap tab, put the relevant ID in anchor,
   // without adding to browser history
   $(document).on("show.bs.tab", function(event) {
-    window.history.replaceState(undefined, undefined, event.target.getAttribute("href"));
+    var anchorValues = new URLSearchParams(window.location.hash.replace(/^#/, ''));
+    var tabId = event.target.getAttribute("href").replace(/^#/,"")
+
+    anchorValues.set("tab", tabId);
+    window.history.replaceState(undefined, undefined, "#" + anchorValues.toString());
   });
 });
 
