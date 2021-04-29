2588d89d7909d34b5b9a4269de92dfca11b2819c
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-04-29 14:49:32 -0400

    support jumping to timecode in audio with #t=[timecode] anchors in urls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,5 +28,5 @@ import '../src/js/audio/ohms_search.js';
 import '../src/js/audio/ohms_footnotes.js';
 import '../src/js/audio/accordion_open_on_screen.js';
 import '../src/js/audio/navbar_tabs.js';
-
+import "../src/js/audio/timecode_in_anchor.js";
 
---
+++ app/javascript/src/js/audio/timecode_in_anchor.js
@@ -0,0 +1,57 @@
+// If page anchor has a timecode in it like #t=13434 where that's a number of seconds, then
+// on page load, we should jump to that timecode in audio player.
+//
+// Using "t" as a key is roughly compatible with WC3 "media fragment" standard, and other
+// common practice.
+//
+// Note this is similar to but different from the play_at_timecode JS that has an on-screen element that
+// can be clicked to advance to timecode, without changing the #fragmentIdentifier.
+
+import domready from 'domready';
+
+domready(function() {
+  var timeCodeSeconds = window.location.hash.includes("=") && new URLSearchParams(window.location.hash.replace(/^#/, '')).get("t");
+
+  if (timeCodeSeconds) {
+    var player = document.querySelector("*[data-role=now-playing-container] audio");
+    if (player) {
+
+      // player might not be in state where it can seek yet, if not then wait.
+      if (player.readyState >=  player.HAVE_METADATA) {
+        setupTimeSeek(player, timeCodeSeconds);
+      } else {
+        player.addEventListener("loadedmetadata", function(event) {
+          setupTimeSeek(player, timeCodeSeconds);
+        });
+      }
+    }
+  }
+});
+
+// Must be called when player is in a readyState where we can seek,
+// at least HAVE_METADATA. https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
+function setupTimeSeek(player, timeCodeSeconds) {
+  player.currentTime = timeCodeSeconds;
+
+  startPlaying(player);
+}
+
+// Now we want to play on page load (we think?), but Modern browsers don't let us play
+// before user has interacted with document. if we catch the error, we will sneakily
+// start playing as soon as they click anywhere. We are not trying to annoy
+// them, but think this may be what they want, needs more investigation.
+function startPlaying(player) {
+  var playPromise  = player.play();
+  if (playPromise !== undefined) {
+    playPromise.catch(error => {
+      // Autoplay was prevented, let's start plaing on any click.
+      document.addEventListener("click",
+        function(event) {
+          player.play();
+        },
+        { once: true} // only execute once.
+      );
+    });
+  }
+}
+
