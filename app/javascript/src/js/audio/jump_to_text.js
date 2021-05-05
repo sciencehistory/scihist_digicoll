7fc79a4bf1bd1847a7f421df503639cd7bae343c
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-05-05 14:33:59 -0400

    extract helpers because we're going to want to re-use them in ToC segment bookmarks

--- app/javascript/src/js/audio/jump_to_text.js
+++ app/javascript/src/js/audio/jump_to_text.js
@@ -1,6 +1,7 @@
 // A "jump to text" button will scroll to portion of transcript matching current timecode
 // in player. OR if you are in ToC tab, open/scroll to segment of ToC matching current timecode.
 
+import {findTranscriptAnchor, scrollToElement, findTocCollapsibleSection, getActiveTab, goToTocCollapsible} from "./helpers/ohms_player_helpers.js";
 import domready from 'domready';
 
 domready(function() {
@@ -13,11 +14,7 @@ domready(function() {
       if (activeTab.id == "ohTocTab") {
         var collapsible = findTocCollapsibleSection(timeCodeSeconds);
         if (collapsible) {
-          if (collapsible.classList.contains("collapse")) {
-            $(collapsible).collapse("show");
-          }
-          // And scroll to the
-          scrollToElement(collapsible.closest(".card").querySelector(".card-header"));
+          goToTocCollapsible(collapsible);
         }
       } else if (activeTab) {
         // if we have anything else, jump to transcript point, activating
@@ -36,47 +33,3 @@ domready(function() {
 });
 
 
-function findTranscriptAnchor(timeInSeconds) {
-  return findOhmsTimestampElementIncluding(timeInSeconds, "#ohTranscript a")
-}
-
-function findOhmsTimestampElementIncluding(timeInSeconds, baseSelector) {
-  var previousEl = undefined;
-  for (var element of document.querySelectorAll(baseSelector + "[data-ohms-timestamp-s]")) {
-    if (element.getAttribute("data-ohms-timestamp-s") > timeInSeconds) {
-      return previousEl;
-    }
-    previousEl = element;
-  }
-
-  // in case it's in last segment, need to return it here.
-  // This may also return last segment for weird non-matches.
-  return previousEl;
-}
-
-// Returns an element that you can call boostrap element.collapse()
-// on, for ToC section corresponding to timecode.
-function findTocCollapsibleSection(timeInSeconds) {
-  var button = findOhmsTimestampElementIncluding(timeInSeconds, "#ohToc button");
-
-  // need to find it's parent collapsible
-  return button && button.closest("*[data-parent='#ohmsIndexAccordionParent']")
-}
-
-// What makes this slightly less than completely simple is allowing for
-// the fixed navbar on top; built-in browser functions will often wind
-// up scrolling it top of window, under navbar.
-//
-function scrollToElement(element) {
-  var targetDocumentXPosition = element.getBoundingClientRect().top + window.scrollY;
-  var navbarHeight = document.querySelector("#ohmsAudioNavbar").offsetHeight;
-
-  window.scrollTo({top: (targetDocumentXPosition - navbarHeight)});
-}
-
-// returns dom element for tab (the button that triggers the tab, that you can
-// call tab() on).
-function getActiveTab() {
-  var content = document.querySelector("#ohmsScrollable .tab-pane.active");
-  return content && document.querySelector(`*[data-toggle="tab"][href="#${content.id}"]`);
-}
