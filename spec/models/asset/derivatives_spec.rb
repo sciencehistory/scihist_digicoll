53c86201c6c0325c4d8a92ad3c42da96487d1af9
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-07-21 11:37:36 -0400

    use newer factorybot pdf asset to test deriv creation

--- spec/models/asset/derivatives_spec.rb
+++ spec/models/asset/derivatives_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 describe "derivative creation" do
-  let(:pdf_file_path) { Rails.root.join("spec/test_support/pdf/sample.pdf")}
-  let(:pdf_file_sha512) { Digest::SHA512.hexdigest(File.read(pdf_file_path)) }
-  let!(:pdf_asset) { FactoryBot.create(:asset, file: File.open(pdf_file_path)) }
+  let!(:pdf_asset) { create(:asset_with_faked_file, :pdf, faked_derivatives: {}) }
   it "creates pdf derivatives" do
-    pdf_asset.file.metadata['sha512'] = pdf_file_sha512
-    pdf_asset.save!
     pdf_asset.create_derivatives
     expect(pdf_asset.file_derivatives.keys.sort).
       to contain_exactly(:thumb_large,:thumb_large_2X,
