9883a7e36aab5c26d4b54b4ebf810943cea6e8df
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-08-10 10:56:39 -0400

    ActiveJob retry_on to retry ALL failed jobs once
    * retry once for StandardError (basically any error) in ApplicationJob super-class of all our jobs. The intent is that any failure should be re-enqueued for a retry ONCE, immediately, before giving up. This should resolve intermittent errors or errors caused by the Job dyno being shut down mid-job.
    * Basic resque isn't able to schedule future jobs. Patch the resque adapter so it IS willing to `retry_on` with `wait:0`, just by doing an ordinary job enqueue.
    * Retry logging is not great. Customize the ActiveJob retry logging by making a custom rails log subscriber for ActiveJob (based on the default one, by subclass), and using our custom one in place of the default one.

---
+++ config/initializers/patch_resque_for_scheduled_at_now.rb
@@ -0,0 +1,22 @@
+
+
+# monkey patch resque adapter to be willing to enqueue_at NOW,
+# to let us use ActiveJob retry_on with wait: 0. Resque
+# can't enqueue in the future without resque-scheduler,
+# but we can enqueue at wait:0/NOW.
+
+
+module AllowResqueEnqueueAtNow
+  def enqueue_at(job, timestamp) #:nodoc:de
+    if !Resque.respond_to?(:enqueue_at_with_queue) && timestamp.to_i <= Time.now.to_i
+      # we don't have resque-scheduler, but it's asking for it to run now anyway,
+      # just enqueue as usual.
+      enqueue(job)
+    else
+      super
+    end
+  end
+end
+
+ActiveJob::QueueAdapters::ResqueAdapter.prepend(AllowResqueEnqueueAtNow)
+
