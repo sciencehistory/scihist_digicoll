958aa80a0a6cdf5fe50b4994ab669e3c298956ce
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-09-21 13:26:41 -0400

    replace WorkTitleAndDates ViewModel with WorkTitleAndDatesComponent

---
+++ app/components/work_title_and_dates_component.rb
@@ -0,0 +1,20 @@
+# Shows the title and dates of a work in the public work display.
+#
+class WorkTitleAndDatesComponent < ApplicationComponent
+  attr_reader :work
+
+  # delegate to WORK
+  delegate :genre, :title, :additional_title, :parent, :source, :date_of_work, :published?, to: :work
+
+  delegate :publication_badge, :can?, to: :helpers
+
+  def initialize(work)
+    @work = work
+  end
+
+
+
+  def display_genres
+    GenreLinkListDisplay.new(work.genre).display
+  end
+end
