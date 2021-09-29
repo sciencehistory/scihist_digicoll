16f4e3d5404dde54b9dbd2778885f07891734c08
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-09-29 15:16:29 -0400

    turn _transcription_tabs_layout partial layout into TranscriptionTabsComponent
    Requires some annoying duplication in the template with the conditional on whether to wrap. May try another refactor

---
+++ app/components/transcription_tabs_component.rb
@@ -0,0 +1,17 @@
+# The tabs for Description/Transcription/Translation on Work show page,
+# also including the actual content for Transcription and Translation -- the
+# "Description" ccontent is  passed in as a content block!
+#
+#    render TranscriptionTabsComponent(transcription_pages: x, translation_pages: y) do
+#       # description tab content
+#    end
+class TranscriptionTabsComponent < ApplicationComponent
+  attr_reader :transcription_texts, :translation_texts
+
+  # @param transcription_pages [Array<WorkImageShowComponent::TextPage>]
+  # @param translation_pages [Array<WorkImageShowComponent::TextPage>]
+  def initialize(transcription_texts:, translation_texts:)
+    @transcription_texts = transcription_texts
+    @translation_texts = translation_texts
+  end
+end
