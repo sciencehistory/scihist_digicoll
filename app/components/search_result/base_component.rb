0d808d1e1244f29a0212fa0da080eecc8ff96ca9
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-10-28 13:53:51 -0400

    refactor preparing to add another field

--- app/components/search_result/base_component.rb
+++ app/components/search_result/base_component.rb
@@ -4,6 +4,9 @@ module SearchResult
   # Meant to be an abstract superclass, which holds the view template .html.erb too. Not meant to be
   # used directly, sub-classes are expected to implement some methods used by the view template.
   class BaseComponent < ApplicationComponent
+    HIGHLIGHT_SOLR_FIELDS = ["searchable_fulltext", "searchable_fulltext_language_agnostic"]
+    MAX_HIGHLIGHT_SNIPPETS = 3
+
     attr_reader :model, :child_counter, :cart_presence, :solr_document
 
     delegate :current_user, :publication_badge, :search_on_facet_path, to: :helpers
@@ -25,7 +28,7 @@ module SearchResult
     # and end either way. html_safe string is returned, with the <em> tags around highlights.
     def search_highlights
       @search_highlights ||= begin
-        highlights = get_highlights("searchable_fulltext") + get_highlights("searchable_fulltext_language_agnostic")
+        highlights = HIGHLIGHT_SOLR_FIELDS.collect {|field| get_highlights(field) }.flatten
         if highlights.present?
           "…".html_safe + safe_join(highlights, " …") + "…".html_safe
         else
