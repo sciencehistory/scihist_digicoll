2f91201f7d8b5db1e5fd4b75d674fc4482d1a096
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-11-23 10:45:51 -0500

    respect redirect-path-type featured topic in new index page
    And alter featured topic show controller to error if you try to use it with a redirect-path-type

--- spec/controllers/featured_topic_controller_spec.rb
+++ spec/controllers/featured_topic_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe FeaturedTopicController, solr: true, type: :controller do
+  render_views
+
   describe "basic featured topic" do
     describe "smoke test" do
       it "can see page" do
         get :index, params: { slug: 'scientific-education' }
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "404 response" do
+      it "for no such topic" do
+        expect {
+          get :index, params: { slug: 'no-such-topic' }
+        }.to raise_error(ActionController::RoutingError)
+      end
+
+      it "for path-type topic" do
+        expect {
+          get :index, params: { slug: 'oral-histories' }
+        }.to raise_error(ActionController::RoutingError)
       end
     end
   end
