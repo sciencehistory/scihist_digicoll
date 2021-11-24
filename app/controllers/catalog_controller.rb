70febd9f9e81f22472214f1ee13971182bd80941
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-11-24 14:00:40 -0500

    use CREATED, not MODIFIED, as the tiebreaker for the "recently added"  search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -423,16 +423,15 @@ class CatalogController < ApplicationController
 
     config.add_sort_field("recently_added") do |field|
       field.label = "recently added"
-      field.sort = "date_published_dtsi desc, date_created_dtsi desc"
+      field.sort = "date_published_dtsi desc, date_modified_dtsi desc"
       # will be used by our custom code as default sort when no query has been entered
       field.blank_query_default = true
     end
 
     # limit to just available for logged-in admins, with `if ` param
-
     config.add_sort_field("oldest_added") do |field|
       field.label = "oldest added"
-      field.sort = "date_published_dtsi asc, date_created_dtsi asc"
+      field.sort = "date_published_dtsi asc, date_modified_dtsi asc"
       field.if = ->(controller, field) { controller.current_user }
     end
 
