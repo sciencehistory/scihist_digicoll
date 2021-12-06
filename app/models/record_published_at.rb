051604dfd79c22c9b47091bda20755d7c401ff4a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-12-06 15:32:40 -0500

    moving method out of the included do block

--- app/models/record_published_at.rb
+++ app/models/record_published_at.rb
@@ -5,9 +5,9 @@ module RecordPublishedAt
   extend ActiveSupport::Concern
   included do
     before_validation :set_published_at
-    private
-    def set_published_at
-      self.published_at = DateTime.now if published? && published_changed?
-    end
+  end
+  private
+  def set_published_at
+    self.published_at = DateTime.now if published? && published_changed?
   end
 end
