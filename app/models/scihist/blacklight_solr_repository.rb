7330c9ca0afeb3a9a865945b3fed693b0fd9b957
Jonathan Rochkind <jrochkind@sciencehistory.org>
2022-01-05 12:50:24 -0500

    integration tests of our custom Blacklight Solr Repository with retries

--- app/models/scihist/blacklight_solr_repository.rb
+++ app/models/scihist/blacklight_solr_repository.rb
@@ -6,6 +6,8 @@ module Scihist
   # thing to do, but the only/best way we could find to add-in
   # Solr retries.
   class BlacklightSolrRepository < Blacklight::Solr::Repository
+    # this is really only here for use in testing, skip the wait in tests
+    class_attribute :zero_interval_retry, default: false
 
     # call super, but then mutate the faraday_connection on
     # the returned RSolr 2.x+ client, to customize the middleware
@@ -16,10 +18,6 @@ module Scihist
 
         # remove if already present, so we can add our own
         faraday_connection.builder.delete(Faraday::Request::Retry)
-
-        # remove so we can make sure it's there AND added AFTER our
-        # retry, so our retry can succesfully catch it's exceptions
-        faraday_connection.builder.delete(Faraday::Response::RaiseError)
 
         # add retry middleware with our own confiuration
         # https://github.com/lostisland/faraday/blob/main/docs/middleware/request/retry.md
@@ -36,7 +34,7 @@ module Scihist
         # Log retries. I wonder if there's a way to have us alerted if
         # there are more than X in some time window Y...
         faraday_connection.request :retry, {
-          interval: 0.300,
+          interval: (zero_interval_retry ? 0 : 0.300),
           # exponential backoff 2 means: 1) 0.300; 2) .600; 3) 1.2; 4) 2.4
           backoff_factor: 2,
           # But we only allow the first two before giving up.
@@ -51,7 +49,7 @@ module Scihist
             # on our observations:
             Faraday::ConnectionFailed,  # nothing listening there at all,
             Faraday::ResourceNotFound, # HTTP 404
-            Faraday::ServerError # HTTP 5xx
+            Faraday::ServerError # any HTTP 5xx
           ],
 
           retry_block: -> (env, options, retries_remaining, exc) do
@@ -59,6 +57,10 @@ module Scihist
             # other things we could log include `env.url` and `env.response.body`
           end
         }
+
+        # remove so we can make sure it's there AND added AFTER our
+        # retry, so our retry can succesfully catch it's exceptions
+        faraday_connection.builder.delete(Faraday::Response::RaiseError)
 
         # important to add this AFTER retry, to make sure retry can
         # rescue and retry it's errors
