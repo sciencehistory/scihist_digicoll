181fecf68226fe1ce8163564f56d0537810d69d5
Jonathan Rochkind <jrochkind@sciencehistory.org>
2022-01-13 16:48:58 -0500

    funding credit images

--- app/models/funding_credit.rb
+++ app/models/funding_credit.rb
@@ -13,13 +13,61 @@ class FundingCredit
     "test" => {
       label: "Test Graphic",
       path: "test.jpg"
+    },
+    "asms" => {
+      label: "American Society for Mass Spectrometry",
+      path: "ASMS.jpg"
+    },
+    "beckman" => {
+      label: "Beckman Foundation",
+      path: "Beckman.jpg"
+    },
+    "clir" => {
+      label: "CLIR",
+      path: "clir-red-symbol.png"
+    },
+    "cns_asu" => {
+      label: "CNS-ASU",
+      path: "CNS-ASU.jpg"
+    },
+    "cusp" => {
+      label: "cusp",
+      path: "CUSP.jpg"
+    },
+    "cns_asu" => {
+      label: "CNS-ASU",
+      path: "CNS-ASU.jpg"
+    },
+    "moore" => {
+      label: "GordonBettyMoore",
+      path: "GordonBettyMoore.png"
+    },
+    "life_sciences_foundation" => {
+      label: "Life Sciences Foundation",
+      path: "Life_Sciences_Foundation.jpg"
+    },
+    "nih" => {
+      label: "NIH",
+      path: "nih.jpg"
+    },
+    "newman" => {
+      label: "Newman Numismatic Portal",
+      path: "Newman-Numismatic-Portal.jpg"
+    },
+    "nhprc" => {
+      label: "nhprc",
+      path: "NHPRC.jpg"
+    },
+    "pew" => {
+      label: "Pew Charitable Trusts",
+      path: "PEW_Charitable_Trusts.jpg"
     }
   }.freeze
 
   def self.image_collection_input
-    IMAGES.collect do |key, definition|
+    @image_collection_input ||= IMAGES.collect do |key, definition|
       [definition[:label], key]
-    end
+    end.sort_by { |label,key| label }
   end
 
   validates :name, presence: true
