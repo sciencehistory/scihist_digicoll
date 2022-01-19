2cbe00edffb66e333d4ce951b8d2390c5ecc2f13
Jonathan Rochkind <jrochkind@sciencehistory.org>
2022-01-19 12:51:46 -0500

    display more of orphaned file path

--- app/controllers/admin/orphan_report_controller.rb
+++ app/controllers/admin/orphan_report_controller.rb
@@ -4,4 +4,15 @@ class Admin::OrphanReportController < AdminController
     @report = Admin::OrphanReport.order(created_at: :desc).first
     @report_available =  @report&.end_time.present?
   end
+
+  private
+
+  # Strip out any prefix part from shrine storage to get just the id
+  helper_method def display_s3_url(url, storage:)
+    path = URI.parse(url).path.delete_prefix("/")
+    if storage.prefix.present?
+      path = path.delete_prefix(storage.prefix).delete_prefix("/")
+    end
+    path
+  end
 end
