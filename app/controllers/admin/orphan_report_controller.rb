12bc74c2a8c975582fca2a1e3ce85c3544f887ad
Jonathan Rochkind <jrochkind@sciencehistory.org>
2022-01-19 11:00:37 -0500

    encapsulate JSONB model access to model, simplifying view a bit

--- app/controllers/admin/orphan_report_controller.rb
+++ app/controllers/admin/orphan_report_controller.rb
@@ -2,7 +2,6 @@
 class Admin::OrphanReportController < AdminController
   def index
     @report = Admin::OrphanReport.order(created_at: :desc).first
-    @report_available =  @report&.data_for_report.dig('end_time').present?
-    @data = @report.data_for_report unless @report.nil?
+    @report_available =  @report&.end_time.present?
   end
 end
