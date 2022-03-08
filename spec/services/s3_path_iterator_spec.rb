dc913f3cafa0688cc65acd67ef4240f69749a379
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-03-08 14:21:52 -0500

    simplify s3 path iterator test using the new helper

--- spec/services/s3_path_iterator_spec.rb
+++ spec/services/s3_path_iterator_spec.rb
@@ -1,59 +1,24 @@
 require 'rails_helper'
 
 describe S3PathIterator do
-  # Regular storage:
-  let(:fake_aws_s3_client_1) { Aws::S3::Client.new(stub_responses: true) }
   let (:it_1) do
     S3PathIterator.new(
       shrine_storage: ScihistDigicoll::Env.shrine_store_storage,
-      extra_prefix: 'asset',
-      show_progress_bar: false
-    )
+      extra_prefix: 'asset', show_progress_bar: false)
   end
-  let(:fake_aws_list_output_1) do
-     Aws::S3::Types::ListObjectsOutput.new(
-      is_truncated: false,   marker: '',
-      next_marker: nil,      name: 'bucket',
-      delimiter: '/',        max_keys: 1000,
-      encoding_type: 'url',
-      contents: file_paths_1.map{|path| Struct.new(:key).new(path)},
-      common_prefixes: []
-    )
-  end
-
-  # Video storage:
-  let(:fake_aws_s3_client_2) { Aws::S3::Client.new(stub_responses: true) }
   let (:it_2) do
     S3PathIterator.new(
       shrine_storage: ScihistDigicoll::Env.shrine_store_video_storage,
-      extra_prefix: 'asset',
-      show_progress_bar: false
-    )
-  end
-  let(:fake_aws_list_output_2) do
-     Aws::S3::Types::ListObjectsOutput.new(
-      is_truncated: false,   marker: '',
-      next_marker: nil,      name: 'bucket',
-      delimiter: '/',        max_keys: 1000,
-      encoding_type: 'url',
-      contents: file_paths_2.map{|path| Struct.new(:key).new(path)},
-      common_prefixes: []
-    )
+      extra_prefix: 'asset', show_progress_bar: false )
   end
 
   before do
-    # This is arbitrary 
     allow_any_instance_of(S3PathIterator).to receive(:s3_bucket_name).and_return('arbitrary string')
-
     # Regular storage:
-    fake_aws_s3_client_1.stub_responses(:list_objects_v2, fake_aws_list_output_1)
-    allow(it_1).to receive(:s3_client).and_return(fake_aws_s3_client_1)
-
+    allow(it_1).to receive(:s3_client).and_return(AwsHelpers::MockAwsClient.new(paths: file_paths_1).client)
     # Video storage:
-    fake_aws_s3_client_2.stub_responses(:list_objects_v2, fake_aws_list_output_2)
-    allow(it_2).to receive(:s3_client).and_return(fake_aws_s3_client_2)
+    allow(it_2).to receive(:s3_client).and_return(AwsHelpers::MockAwsClient.new(paths: file_paths_2).client)
   end
-
 
   describe "Two path iterators pointed at two different local buckets" do
     let(:file_paths_1) { ['aaa', 'bbb', 'ccc'] }
@@ -71,7 +36,6 @@ describe S3PathIterator do
   end
 
   describe "Using actual files" do
-
     let!(:asset_1)  { create(:asset, :inline_promoted_file,
         position: 1,
         file: File.open((Rails.root + "spec/test_support/images/20x20.png"))
@@ -83,13 +47,11 @@ describe S3PathIterator do
       )
     }
     let!(:work) { FactoryBot.create( :public_work, members: [asset_1, asset_2]) }
-
     let(:file_paths_1) { [work.members.first.file_data["id"], work.members.second.file_data["id"]] }
     let(:file_paths_2) { [] }
-
-    it "It's possible to feed real paths to the iterators" do
+    
+    it "is possible to feed real paths to the iterators" do
       an_asset = work.members[0]
-
       s3_keys_found_1 = []
       it_1.each_s3_path { |s3_key| s3_keys_found_1 << s3_key }
       expect(s3_keys_found_1).to eq file_paths_1
