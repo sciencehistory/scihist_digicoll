2ecb005e064265a1509a7290632565c875b5607e
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-03-11 12:36:41 -0500

    testing the sample

--- spec/services/orphan_s3_originals_spec.rb
+++ spec/services/orphan_s3_originals_spec.rb
@@ -14,12 +14,17 @@ describe OrphanS3Originals do
   let(:file_paths_v) {['va', 'vb', 'vc']}
   let(:file_paths_n) {['na', 'nb', 'nc']}
 
+  let(:v_prefix) { ScihistDigicoll::Env.shrine_store_video_storage.prefix }
+  let(:n_prefix) { ScihistDigicoll::Env.shrine_store_storage.      prefix }
+
   let(:fake_clients) do
     {
-      video: AwsHelpers::MockS3Client.new(paths: file_paths_v).client,
+      video:    AwsHelpers::MockS3Client.new(paths: file_paths_v).client,
       nonvideo: AwsHelpers::MockS3Client.new(paths: file_paths_n).client
     }
   end
+
+
 
   let(:orphan_checker) do
     OrphanS3Originals.new(show_progress_bar: false)
@@ -40,6 +45,11 @@ describe OrphanS3Originals do
       orphan_checker.report_orphans
       expect(orphan_checker.files_checked).to eq 6
       expect(orphan_checker.orphans_found).to eq 6
+      expect(orphan_checker.sample).
+        to eq file_paths_v.
+          map {|s| "/#{v_prefix}/#{s}" } +
+        file_paths_n.
+          map {|s| "/#{n_prefix}/#{s}" }
     end
 
     it "deletes the orphans" do
@@ -50,15 +60,19 @@ describe OrphanS3Originals do
 
   describe "two assets, one video and one image, plus two orphans" do
     let(:file_paths_v) do
-      ["#{ScihistDigicoll::Env.shrine_store_video_storage.prefix}/#{asset_v.file_data['id']}", "video_orphan.mp4"]
+      ["#{v_prefix}/#{asset_v.file_data['id']}", "video_orphan.mp4"]
     end
     let(:file_paths_n) do
-       ["#{ScihistDigicoll::Env.shrine_store_storage.prefix}/#{asset_n.file_data['id']}", "image_orphan.jpg"]
+       ["#{n_prefix}/#{asset_n.file_data['id']}", "image_orphan.jpg"]
     end
     it "finds the orphans" do
       orphan_checker.report_orphans
       expect(orphan_checker.files_checked).to eq 4
       expect(orphan_checker.orphans_found).to eq 2
+      expect(orphan_checker.sample).to eq [
+        "/#{v_prefix}/video_orphan.mp4",
+        "/#{n_prefix}/image_orphan.jpg"
+      ]
     end
     it "deletes the orphans" do
       orphan_checker.delete_orphans
