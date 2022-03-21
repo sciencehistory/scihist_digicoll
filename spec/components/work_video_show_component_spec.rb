8182afa35110f805b14b7d0f0e77cee97f251116
Jonathan Rochkind <jonathan@dnil.net>
2022-03-21 12:55:54 -0400

    test missing derivatives for video show page

--- spec/components/work_video_show_component_spec.rb
+++ spec/components/work_video_show_component_spec.rb
@@ -3,12 +3,9 @@ require 'rails_helper'
 describe WorkVideoShowComponent, type: :component do
   let(:work) { create(:video_work, :published) }
 
-  before do
-    render_inline described_class.new(work)
-  end
-
-
   it "has video element properly set up" do
+    render_inline described_class.new(work)
+
     video_element = page.first("video")
 
     expect(video_element).to be_present
@@ -18,6 +15,21 @@ describe WorkVideoShowComponent, type: :component do
 
     expect(source_element["src"]).to eq work.representative.file_url
     expect(source_element["type"]).to eq work.representative.content_type
+  end
+
+  describe "when it is missing derivatives" do
+    let(:work) do
+      create(:video_work, :published).tap do |work|
+        work.representative.remove_derivatives( *work.representative.file_derivatives.keys )
+      end
+    end
+
+    it "can still display page without errors" do
+      expect(work.representative.file_derivatives).to be_empty
+
+      render_inline described_class.new(work)
+      expect(page.first("video")).to be_present
+    end
   end
 end
 
