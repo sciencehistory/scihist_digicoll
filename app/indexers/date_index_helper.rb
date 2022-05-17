53376340dcd12c525c704c7e29908167e683e547
Jonathan Rochkind <jonathan@dnil.net>
2022-05-17 14:46:43 -0500

    new functions to support full date sort

--- app/indexers/date_index_helper.rb
+++ app/indexers/date_index_helper.rb
@@ -1,16 +1,79 @@
-# Takes DateOfWork metadata from a Work, expands it into
-# an array of integer years to be used to fill an int facet
-# in solr for date range limit or sortable fields.
+# A Work can have one or more DateOfWork objects representing a date, or date range,
+# or partial date. Including things like "the 1930s" or "May 1940 to June 1941"
 #
-# Copied from https://github.com/sciencehistory/chf-sufia/blob/master/app/indexers/chf/generic_work_indexer/date_values.rb
+# This class interprets/expands those DateOfWork objects into more specific things
+# that we need for Solr indexing.
+#
+# #expanded_years is an array of years, any year that might be represented
+# by any dates included, that we use for some solr fields to facet upon
+#
+# #min_date and #max_date are actual Ruby date objects, the latest or
+# earliest dates that might be represented by the DateOfWork set,
+# that we use for Solr fields to sort on.
+#
 class DateIndexHelper
   attr_reader :work
   def initialize(work)
     @work = work
   end
 
+  # An array of int, all the years that might be included by any dates/ranges in work
+  # We use for a year facet value
   def expanded_years
     @expanded_years ||= work.date_of_work.collect { |date_of_work| years_for_date_of_work(date_of_work) }.flatten
+  end
+
+
+  # @return [Date] the earliest date in the work -- for dates effectively
+  #   representing ranges,the earliest possible date in the range is it.
+  #
+  #   Returns nil if none can be found because of no dates or malformed dates.
+  def min_date
+    work.date_of_work.collect do |date_of_work|
+      start, start_qualifier, finish, finish_qualifier = date_of_work.start, date_of_work.start_qualifier, date_of_work.finish, date_of_work.finish_qualifier
+
+      case start_qualifier
+      when "Undated"
+        nil
+      when "decade"
+        complete_min_date(start.strip.slice(0..2)) # send first three year digits
+      when "century"
+        complete_min_date(start.strip.slice(0..1)) # send first two year digits
+      else
+        # For all other types of start date (circa, before, after, as well as blank)
+        # we're just going to index it as the date we've got, if we've got one.
+        complete_min_date(start)
+      end
+    end.compact.min
+  end
+
+  # @return [Date] The latest date in the work -- for dates effectively
+  #    representing ranges, the latest possible date in the range is it.
+  #
+  #    Returns nil if none can be found because of no dates or malformed dates.
+  def max_date
+    work.date_of_work.collect do |date_of_work|
+      start, start_qualifier, finish, finish_qualifier = date_of_work.start, date_of_work.start_qualifier, date_of_work.finish, date_of_work.finish_qualifier
+
+      if finish.present?
+        operative_date, operative_qualifier = finish, finish_qualifier
+      else
+        operative_date, operative_qualifier = start, start_qualifier
+      end
+
+      case operative_qualifier
+      when "Undated"
+        nil
+      when "decade"
+        complete_max_date(operative_date.strip.slice(0..2)) # send first three year digits
+      when "century"
+        complete_max_date(operative_date.strip.slice(0..1)) # send first two year digits
+      else
+        # For all other types of start date (circa, before, after, as well as blank)
+        # we're just going to index it as the date we've got, if we've got one.
+        complete_max_date(operative_date)
+      end
+    end.compact.max
   end
 
   def min_year
@@ -21,6 +84,11 @@ class DateIndexHelper
     @maximum_year ||= expanded_years.max
   end
 
+
+  private
+
+  # helper method for expanded_years, take a single DateOfWork and expand it into
+  # all the years that might be included by dates referenced.
   def years_for_date_of_work(repo_d)
     start, start_qualifier, finish, finish_qualifier = repo_d.start, repo_d.start_qualifier, repo_d.finish, repo_d.finish_qualifier
 
@@ -69,6 +137,77 @@ class DateIndexHelper
       # like a year.
       return (start_year..finish_year).to_a
     end
+  end
+
+  # Helper method for min_date, completes partial dates into a ruby Date
+  #
+  # Can pass in a yyyy-mm-dd date, OR a partial one:
+  #
+  # * \d\d (century, eg `20`)
+  # * \d\d\d (decade, eg `193`)
+  # * \d\d\d\d (just year, `1973`)
+  # * \d\d\d\d-\d\d (just year and month, `2020-09`)
+  # * \d\d\d\d-\d\d-\d\d (full date, 2020-04-21)
+  #
+  # @ returns [Date] representing the MINIMUM possible date for input that may
+  #   represent a date. Like for `193` (decade), that means 1930-01-01
+  #   Returns nil if bad unparseable unexpected input.
+  #
+  def complete_min_date(date_str)
+    unless date_str && date_str =~ /(\d\d\d?\d?)(?:-(\d\d))?(?:-(\d\d))?/
+      return nil # malformed date
+    end
+
+    year  = $1
+    month = $2
+    day   = $3
+
+    if year.length == 2 # century
+      Date.iso8601("#{date_str}00-01-01")
+    elsif year.length == 3 #decade
+      Date.iso8601("#{date_str}0-01-01")
+    else
+      # full year, maybe with month, maybe with date
+      month ||= "01"
+      day ||= "01"
+      Date.iso8601("#{year}-#{month}-#{day}")
+    end
+  rescue Date::Error
+    nil
+  end
+
+  # Helper method for max_date, completes partial dates into a ruby Date.
+  #
+  # like complete_min_date, but returns the date representing the MAXIMUM
+  # possible date for input that may represent a date.
+  #
+  # @returns [Date] represneting MAXIMIM possible date for input,
+  #   like for `193` (decade), that means `1939-12-31`. Returns
+  #   nil if bad, unparseable, or unexpected input.
+  def complete_max_date(date_str)
+    unless date_str && date_str =~ /(\d\d\d?\d?)(?:-(\d\d))?(?:-(\d\d))?/
+      return nil # malformed date
+    end
+
+    year  = $1
+    month = $2
+    day   = $3
+
+    if year.length == 2 # century
+      Date.iso8601("#{date_str}99-12-31")
+    elsif year.length == 3 #decade
+      Date.iso8601("#{date_str}9-12-31")
+    elsif day.nil?
+      month ||= "12" # maybe we don't have month either
+
+      # have to auto-complete to last day in month, which
+      # we can have date class figure out for us this way:
+      Date.iso8601("#{year}-#{month}-01").next_month.prev_day
+    else
+      Date.iso8601("#{year}-#{month}-#{day}")
+    end
+  rescue Date::Error
+    nil
   end
 end
 
