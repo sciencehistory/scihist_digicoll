2fb6ba1b833f74eb693ef0ab0acb4a8bdd17271d
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-05-24 09:53:16 -0400

    commenting out the offending lines to make sure (2)

--- app/models/interviewee_biography.rb
+++ app/models/interviewee_biography.rb
@@ -5,8 +5,9 @@ class IntervieweeBiography < ApplicationRecord
   include AttrJson::Record
   include AttrJson::NestedAttributes
 
-  scope :find_by_name_substring, ->(query) do
-  end
+  # These two lines cause CI to break:
+  #scope :find_by_name_substring, ->(query) do
+  #end
 
   has_and_belongs_to_many :oral_history_content
 
