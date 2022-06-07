718c7f1907e92cc1d05baf356ea9be594566737c
Jonathan Rochkind <jonathan@dnil.net>
2022-06-07 12:19:17 -0400

    render just a link with simple_link mode

--- spec/components/rights_icon_component_spec.rb
+++ spec/components/rights_icon_component_spec.rb
@@ -34,7 +34,6 @@ describe RightsIconComponent, type: :component do
     end
 
     describe "dropdown-item mode" do
-      let(:rights_id) { "http://creativecommons.org/publicdomain/mark/1.0/" }
       let(:rendered) { render_inline(RightsIconComponent.new(rights_id: rights_id, mode: :dropdown_item)) }
 
       it "renders" do
@@ -47,6 +46,19 @@ describe RightsIconComponent, type: :component do
         expect(link).to have_selector("img.rights-statement-logo[src*='rightsstatements-NoC.Icon-Only.dark']")
 
         expect(link.inner_html).to include("Public Domain")
+      end
+    end
+
+    describe "simple_link mode" do
+      let(:rendered) { render_inline(RightsIconComponent.new(rights_id: rights_id, mode: :simple_link)) }
+
+      it "renders" do
+        expect(rendered.children.length).to eq 1
+        expect(rendered.children.first.name).to eq "a"
+
+        link = rendered.at_xpath("a")
+        expect(link["href"]).to eq(rights_id)
+        expect(link.inner_text).to eq "Public Domain Mark 1.0"
       end
     end
   end
