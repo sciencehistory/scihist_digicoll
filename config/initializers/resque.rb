4c8562631543d18e1ea351f00377bfc767fcaf13
Jonathan Rochkind <jonathan@dnil.net>
2022-06-08 11:59:06 -0400

    Silence redis deprecations caused by unmaintained resque
    Ref #1745

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,5 @@
 Resque.redis = ScihistDigicoll::Env.persistent_redis_connection!
+
+# Silence copious deprecation warnings until resque fixes them
+# https://github.com/sciencehistory/scihist_digicoll/issues/1745
+Redis.silence_deprecations = true
