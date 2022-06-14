163824f187fccb3cb2bb06a6f9caa106c0c3c598
Jonathan Rochkind <jonathan@dnil.net>
2022-06-14 12:45:05 -0400

    include Oral History Number in metadata

--- spec/components/work_show_info_component_spec.rb
+++ spec/components/work_show_info_component_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe WorkShowInfoComponent do
+describe WorkShowInfoComponent, type: :component do
   let(:work) {  build(:work, external_id: {category: "bib", value: "b1075359"}, related_url: "https://othmerlib.sciencehistory.org/record=b1075359")}
 
   context "#links_to_opac" do
@@ -17,6 +17,20 @@ describe WorkShowInfoComponent do
         links = WorkShowInfoComponent.new(work: work).links_to_opac
         expect(links).to eq(["https://othmerlib.sciencehistory.org/record=b1072260"])
       end
+    end
+  end
+
+  context "Oral History Number" do
+    let(:work) {
+      build(:oral_history_work, external_id: [
+          {'category' => 'bib', 'value' => 'b1043559'},
+          {'category' => 'interview', 'value' => '0012'}
+      ])
+    }
+
+    it "displays oral history number" do
+      render_inline WorkShowInfoComponent.new(work: work)
+      expect(page).to have_text(/Oral history number\s+0012/)
     end
   end
 end
