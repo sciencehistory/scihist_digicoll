94d374f7c90003d9dade50cb5c9a76cb50dcfb51
Jonathan Rochkind <jonathan@dnil.net>
2022-06-28 12:12:26 -0400

    refactor creative_commons alternate display branching
    This is not a great way for the code to be organized, it kind of just grew this way. One step to it being more sensible, plus setting the way for us to diverge creative commons a bit more

--- app/components/rights_icon_component.rb
+++ app/components/rights_icon_component.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
-
+#
+# Display a logo and text for a rights statement -- can display a large one, a one-line one for
+# use in a menu, or just a link.
+#
+# Can also display for a "standard" one-icon rights statement OR a Creative Commons one
+# that can have multiple CC bubbles, and requires a different layout.
+#
+# Having to handle so many cases which have ended up different visually have meant there are a LOT
+# of weird conditionals in here, this should probably be refactored, into several different components
+# with shared func located elsewhere.
 class RightsIconComponent < ApplicationComponent
     attr_reader :mode, :rights_id, :rights_term, :work
 
@@ -33,7 +42,7 @@ class RightsIconComponent < ApplicationComponent
   def display_dropdown_item
     # since we're using short label, the icon needs an alt tag if appropriate to explain the icon.
 
-    link_to(rights_url, target: "_blank", class: ['rights-statement', mode.to_s.dasherize, layout_class]) do
+    link_to(rights_url, target: "_blank", class: ['rights-statement', mode.to_s.dasherize]) do
       image_tag(rights_category_icon_src, class: "rights-statement-logo", alt: rights_term.icon_alt) +
       " ".html_safe +
       content_tag("span",
@@ -48,20 +57,24 @@ class RightsIconComponent < ApplicationComponent
 
   # a sort of logotype lock-up, with an internal link, so we can put a "rel: license" on it for CC.
   def display_large
-    content_tag("div", class: ['rights-statement', mode.to_s.dasherize, layout_class]) do
-      large_graphical_element +
-      " ".html_safe +
-      content_tag("span", rights_icon_label, class: "rights-statement-label")
+    if rights_category == "creative_commons"
+      content_tag("div", class: ['rights-statement', mode.to_s.dasherize, "creative-commons-org"]) do
+        creative_commons_large_linked_icons +
+        " ".html_safe +
+        content_tag("span", creative_commons_rights_icon_label, class: "rights-statement-label")
+      end
+    else
+      # The category icon, does need alt text because we just display a short label
+      # next to it, which doesn't include what the icon conveys.
+      content_tag("div", class: ['rights-statement', mode.to_s.dasherize, "rights-statements-org"]) do
+        image_tag(rights_category_icon_src, class: "rights-statement-logo", alt: rights_term.icon_alt) +
+        " ".html_safe +
+        content_tag("span", rights_icon_label, class: "rights-statement-label")
+      end
     end
   end
 
   private
-
-  # our CSS does different things for rightsstatement-style and creative_commons style, we want to
-  # give it a class so we can.
-  def layout_class
-    rights_category == "creative_commons" ? "creative-commons-org" : "rights-statements-org"
-  end
 
   def has_rights_statement?
     rights_id.present?
@@ -77,34 +90,27 @@ class RightsIconComponent < ApplicationComponent
     end
   end
 
+  def creative_commons_rights_icon_label
+    "This work is licensed under a ".html_safe + link_to(rights_term.label + ".", rights_url, target: "_blank", rel: "license")
+  end
+
   # HTML label (becuase it includes a <br> at the right point) for the rights statement,
   # from our local metadata, adapted from rightstatements.org
   def rights_icon_label
-    if rights_category == "creative_commons"
-      # special long form
-      "This work is licensed under a ".html_safe + link_to(rights_term.label + ".", rights_url, target: "_blank", rel: "license")
-    else
-      link_to((rights_term.short_label_html || "").html_safe, rights_url, target: "_blank")
-    end
+    link_to((rights_term.short_label_html || "").html_safe, rights_url, target: "_blank")
   end
 
-  def large_graphical_element
-    # creative commons can show multiple "bubble" icons, they don't need alt text
-    # becuase we display a complete textual label next to it.
-    if rights_category == "creative_commons"
-      images =  [image_tag(rights_category_icon_src, class: "rights-statement-logo", alt: "")]
+  # creative commons can show multiple "bubble" icons, they don't need alt text
+  # becuase we display a complete textual label next to it.
+  def creative_commons_large_linked_icons
+    images =  [image_tag(rights_category_icon_src, class: "rights-statement-logo", alt: "")]
 
-      (rights_term.pictographs || []).each do |pictograph_image|
-        images << image_tag("cc_pictographs/#{pictograph_image}", class: "rights-statement-logo", alt: "")
-      end
+    (rights_term.pictographs || []).each do |pictograph_image|
+      images << image_tag("cc_pictographs/#{pictograph_image}", class: "rights-statement-logo", alt: "")
+    end
 
-      link_to rights_url, target: "_blank", alt: rights_term.label, title: rights_term.label do
-        safe_join images
-      end
-    else
-      # just the category icon, it does need alt text because we just display a short label
-      # next to it, which doesn't include what the icon conveys.
-      image_tag(rights_category_icon_src, class: "rights-statement-logo", alt: rights_term.icon_alt)
+    link_to rights_url, target: "_blank", alt: rights_term.label, title: rights_term.label do
+      safe_join images
     end
   end
 
