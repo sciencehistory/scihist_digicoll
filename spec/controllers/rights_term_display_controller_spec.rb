9c5639205354dbce3c2b82c9e90b69afe20b417a
Jonathan Rochkind <jonathan@dnil.net>
2022-06-29 11:21:14 -0400

    refuse to show rights page with conflicting wrong info

--- spec/controllers/rights_term_display_controller_spec.rb
+++ spec/controllers/rights_term_display_controller_spec.rb
@@ -30,7 +30,7 @@ describe RightsTermDisplayController do
 
     let(:rights_param) { "NoC-US" }
     let(:rights_term) { RightsTerm.find_by_param_id(rights_param) }
-    let(:work) { create(:work, rights_holder: "Some Rightsholder") }
+    let(:work) { create(:work, rights: rights_term.id, rights_holder: "Some Rightsholder") }
     let(:work_friendlier_id) { work.friendlier_id }
 
     it "displays page with rights holder" do
@@ -40,6 +40,15 @@ describe RightsTermDisplayController do
       expect(response.body).to include(rights_term.description)
       expect(response.body).to include(work.rights_holder)
     end
-  end
 
+    describe "with conflicting params" do
+      let(:work) { create(:work, rights: "http://rightsstatements.org/vocab/InC-EDU/1.0/") }
+
+      it "won't display page, 404s" do
+        expect {
+          get :show, params: { id: rights_param, work_id: work_friendlier_id }
+        }.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
 end
