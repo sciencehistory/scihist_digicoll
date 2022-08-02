74aa648e96638a49c4001dcd3d5c122e4c3f9843
Jonathan Rochkind <jonathan@dnil.net>
2022-08-02 12:00:39 -0400

    provide mailer preview for both MP3 and FLAC, since we're going to diverge delivery a bit

--- spec/mailers/previews/oral_history_delivery_mailer_preview.rb
+++ spec/mailers/previews/oral_history_delivery_mailer_preview.rb
@@ -6,24 +6,40 @@ require 'factory_bot_rails'
 # http://localhost:3000/rails/mailers/oral_history_delivery_mailer/oral_history_delivery_email
 #
 class OralHistoryDeliveryMailerPreview < ActionMailer::Preview
-  SAMPLE_WORK_TITLE = "Sample OH Request work"
+  CUSTOM_MESSAGE = "[[This is optional customized per-email instructions written by staff to patron, possibly including usage restrictions.]]"
 
-  def oral_history_delivery_email
+  def oral_history_delivery_email_mp3
     OralHistoryDeliveryMailer.
-      with(request: oral_history_request, custom_message: "[[This is optional customized per-email instructions written by staff to patron, possibly including usage restrictions.]]").
+      with(request: oral_history_request(file_type: :mp3), custom_message: CUSTOM_MESSAGE).
       oral_history_delivery_email
   end
 
-  protected
-
-  # find or create a test request, we're hopefully in development here!
-  def oral_history_request
-    existing_sample = Admin::OralHistoryAccessRequest.joins(:work).where(work: { title: SAMPLE_WORK_TITLE }).first
-    existing_sample || create_sample_request
+  def oral_history_delivery_email_flac
+    OralHistoryDeliveryMailer.
+      with(request: oral_history_request(file_type: :flac), custom_message: CUSTOM_MESSAGE).
+      oral_history_delivery_email
   end
 
-  def create_sample_request
-    work = FactoryBot.create(:oral_history_work, :available_by_request, title: SAMPLE_WORK_TITLE, published: true)
+
+  protected
+
+  def sample_work_title(file_type:)
+    "Faked Sample OH Request work: #{file_type.to_s}"
+  end
+
+  # find or create a test request, we're hopefully in development here!
+  def oral_history_request(file_type:)
+    existing_sample = Admin::OralHistoryAccessRequest.joins(:work).
+                        where(work: { title: sample_work_title(file_type: file_type) }).first
+    existing_sample || create_sample_request(file_type: file_type)
+  end
+
+
+  def create_sample_request(file_type:)
+    work = FactoryBot.create(:oral_history_work, :available_by_request,
+              title: sample_work_title(file_type: file_type),
+              audio_asset_factory: file_type,
+              published: true)
 
     request = Admin::OralHistoryAccessRequest.create!(
       work: work,
