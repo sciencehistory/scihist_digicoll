d8968320c108a1a4e7f0f9e60cf0c3a9a029cd5d
Jonathan Rochkind <jonathan@dnil.net>
2022-08-10 10:52:47 -0400

    DB migration to automatically apply rake scihist:data_fixes:remove_related_url_opac

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_213542) do
+ActiveRecord::Schema.define(version: 2022_08_10_145124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
