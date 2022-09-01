ba70e4dd203ca97da9a8b7e5998674891e9b6c3f
Jonathan Rochkind <jonathan@dnil.net>
2022-09-01 11:18:53 -0400

    remove specs for now non-existing behavior around OPAC links in related_url

--- spec/components/work_show_info_component_spec.rb
+++ spec/components/work_show_info_component_spec.rb
@@ -1,25 +1,6 @@
 require 'rails_helper'
 
 describe WorkShowInfoComponent, type: :component do
-  let(:work) {  build(:work, external_id: {category: "bib", value: "b1075359"}, related_url: "https://othmerlib.sciencehistory.org/record=b1075359")}
-
-  context "#links_to_opac" do
-    context "with duplicate bibnum and related url" do
-      it "has only one links_to_opac" do
-        links = WorkShowInfoComponent.new(work: work).links_to_opac
-        expect(links).to eq(["https://othmerlib.sciencehistory.org/record=b1075359"])
-      end
-    end
-
-    context "with weird almost-matching dups" do
-      let(:work) {  build(:work, external_id: {category: "bib", value: "B10722609"}, related_url: "https://othmerlib.sciencehistory.org/record=b1072260")}
-      it "still only has one link to opac" do
-        links = WorkShowInfoComponent.new(work: work).links_to_opac
-        expect(links).to eq(["https://othmerlib.sciencehistory.org/record=b1072260"])
-      end
-    end
-  end
-
   context "Oral History Number" do
     let(:work) {
       build(:oral_history_work, external_id: [
