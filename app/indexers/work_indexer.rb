401a435ee479d7edd177ce7ef2ac24a1a501cd83
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-09-09 10:41:22 -0400

    indexing just subject and description

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -1,171 +1,71 @@
 # Solr indexing for our work class.
 class WorkIndexer < Kithe::Indexer
-
-
-  # instead of a Solr field per attribute, we group them into fields
-  # that will have similar boosting, to use solr more efficiently.
-  # text1 is boosted highest, text2 next highest, etc.
-  field_map = {
-    :model_pk_ssi =>      ["model_pk_ssi"],
-    :title =>             ["text1_tesim"],
-    :additional_title =>  ["text1_tesim"],
-    :creator =>           ["text2_tesim","creator_facet"],
-    :genre =>             ["text2_tesim", "genre_facet"],
-    :subject =>           ["text3_tesim", "subject_facet"],
-    :interviewer =>       ["interviewer_facet"],
-
-    # index description to its own field for highlighting purposes. Fields with
-    # HTML in them need to have it stripped before indexing.
-    :description =>       ["description_text4_tesim"],
-
-    :provenance =>        ["text4_tesim"],
-    :language =>          ["text_no_boost_tesim", "language_facet"],
-    :external_id =>       ["text_no_boost_tesim"],
-    :related_url =>       ["text_no_boost_tesim"],
-    :rights =>            ["text_no_boost_tesim", "rights_facet"],
-    :rights_label =>      ["text_no_boost_tesim"],
-    :rights_holder =>     ["text_no_boost_tesim"],
-    :series_arrangement =>["text_no_boost_tesim"],
-    :inscription =>       ["text_no_boost_tesim"],
-    :additional_credit => ["text_no_boost_tesim"],
-    :digitization_funder =>["text_no_boost_tesim"],
-    :extent =>            ["text_no_boost_tesim"],
-    :physical_container =>["text_no_boost_tesim"],
-
-    # We put these two in a separate field,
-    # cause we only allow logged in users
-    # to search them
-    :admin_note =>        ["admin_only_text_tesim"],
-    :exhibition =>        ["admin_only_text_tesim", "exhibition_facet"],
-
-    :expanded_years =>    ["year_facet_isim"],
-    :earliest_date =>     ["earliest_date"],
-    :latest_date =>       ["latest_date"],
-    
-    :collection_id =>     ["collection_id_ssim"],
-    
-    :created_at =>        ["date_created_dtsi"],
-    :updated_at =>        ["date_modified_dtsi"],
-    :published_at =>      ["date_published_dtsi"],
-
-    :published_boolean => ["published_bsi"],
-
-    :oh_feature_facet =>        ["oh_feature_facet"],
-    :oh_availability_facet =>   ["oh_availability_facet"],
-    :oh_institution_facet =>    ["oh_institution_facet"],
-    :oh_birth_country_facet =>  ["oh_birth_country_facet"],
-    :bredig_feature_facet =>    ["bredig_feature_facet"],
-
-    :place =>             ["text_no_boost_tesim", "place_facet"],
-    :department =>        ["text_no_boost_tesim", "department_facet"], 
-    :medium =>            ["text_no_boost_tesim", "medium_facet"],
-    :format =>            ["text_no_boost_tesim", "format_facet"],
-
-    :fulltext_en =>       ["searchable_fulltext_en"],
-    :fulltext_de =>       ["searchable_fulltext_de"],
-    :fulltext_agnostic => ["searchable_fulltext_language_agnostic"]
-  }
-
-
-
-  more_like_this_settings = ENV['MORE_LIKE_THIS_SETTINGS'] || "OFF"
-
-  unless ["OFF", "SIMPLE", "MEDIUM", "FANCY"].include? more_like_this_settings
-    raise RuntimeError.new("Set ENV['MORE_LIKE_THIS_SETTINGS'] to OFF, SIMPLE, MEDIUM or FANCY.")
-  end
-
-  # This could work.
-  if more_like_this_settings == "SIMPLE"
-    [:subject, :creator, :description].each do |field|
-      field_map[field] << "more_like_this_keywords_tsimv"
-    end
-
-  # Add title and other bib metadata
-  elsif more_like_this_settings == "MEDIUM"
-    [ :title, :additional_title, :creator,
-      :genre, :subject, :description, :place,
-      :department, :medium, :format ].each do |field|
-      field_map[field] << "more_like_this_keywords_tsimv"
-    end
-
-  # Attempt at a fulltext similarity index.
-  # Probably overkill.
-  elsif more_like_this_settings == "FANCY"
-    [:title, :additional_title, :creator,
-      :genre, :subject, :description, :place,
-      :department, :medium, :format].each do |field|
-      field_map[field] << "more_like_this_keywords_tsimv"
-    end
-    [:fulltext_en, :fulltext_de, :fulltext_agnostic].each do |field|
-      field_map[field] << "more_like_this_fulltext_tsimv"
-    end
-  end
-
   configure do
+    # instead of a Solr field per attribute, we group them into fields
+    # that will have similar boosting, to use solr more efficiently.
+    # text1 is boosted highest, text2 next highest, etc.
 
-    to_field field_map[:model_pk_ssi], obj_extract("id") # the actual db pk, a UUID
+    to_field "model_pk_ssi", obj_extract("id") # the actual db pk, a UUID
 
-    to_field field_map[:title],             obj_extract("title")
-    to_field field_map[:additional_title],  obj_extract("additional_title")
-    to_field field_map[:creator],           obj_extract("creator", "value")
-    to_field field_map[:genre],             obj_extract("genre")
-    to_field field_map[:subject],           obj_extract("subject")
+    to_field "text1_tesim", obj_extract("title")
+    to_field "text1_tesim", obj_extract("additional_title")
+
+    to_field ["text2_tesim","creator_facet"], obj_extract("creator", "value")
+    to_field ["text2_tesim", "genre_facet"], obj_extract("genre")
+
+    to_field ["text3_tesim", "subject_facet", "more_like_this_keywords_tsimv"], obj_extract("subject")
 
     # Interviewer out of creator facet for use specifically for Oral History collection
-    to_field field_map[:interviewer] do |record, acc|
+    to_field "interviewer_facet" do |record, acc|
       acc.concat record.creator.find_all { |creator| creator.category == "interviewer"}.collect(&:value)
     end
 
-    to_field field_map[:description],
-      obj_extract("description"), transform(->(val) { ActionView::Base.full_sanitizer.sanitize(val) })
-    to_field field_map[:provenance],
-      obj_extract("provenance"), transform(->(val) { ActionView::Base.full_sanitizer.sanitize(val) })
 
-    to_field field_map[:language],    obj_extract("language")
-    to_field field_map[:external_id], obj_extract("external_id", "value")
-    to_field field_map[:related_url], obj_extract("related_url")
-    to_field field_map[:place],       obj_extract("place", "value")
-    to_field field_map[:department],  obj_extract("department")
-    to_field field_map[:medium],      obj_extract("medium")
-    to_field field_map[:format],      obj_extract("format"), transform(->(v) { v.titleize })
-    to_field field_map[:rights],      obj_extract("rights") # URL id
+    # index description to it's own field for highlighting purposes. Fields with
+    # HTML in them need to have it stripped before indexing.
+    to_field ["description_text4_tesim", "more_like_this_keywords_tsimv"], obj_extract("description"), transform(->(val) { ActionView::Base.full_sanitizer.sanitize(val) })
+    to_field "text4_tesim", obj_extract("provenance"), transform(->(val) { ActionView::Base.full_sanitizer.sanitize(val) })
 
-    to_field field_map[:rights_label],
-      obj_extract("rights"), transform(->(v) { RightsTerm.label_for(v) }) # human label
+    to_field ["text_no_boost_tesim", "language_facet"], obj_extract("language")
+    to_field "text_no_boost_tesim", obj_extract("external_id", "value")
+    to_field "text_no_boost_tesim", obj_extract("related_url")
+    to_field ["text_no_boost_tesim", "place_facet"], obj_extract("place", "value")
+    to_field "text_no_boost_tesim", obj_extract("related_url")
+    to_field ["text_no_boost_tesim", "department_facet"], obj_extract("department")
+    to_field ["text_no_boost_tesim", "medium_facet"], obj_extract("medium")
+    to_field ["text_no_boost_tesim", "format_facet"], obj_extract("format"), transform(->(v) { v.titleize })
+    to_field ["text_no_boost_tesim", "rights_facet"], obj_extract("rights") # URL id
+    to_field ["text_no_boost_tesim"], obj_extract("rights"), transform(->(v) { RightsTerm.label_for(v) }) # human label
+    to_field "text_no_boost_tesim", obj_extract("rights_holder")
+    to_field "text_no_boost_tesim", obj_extract("series_arrangement")
 
-    to_field field_map[:rights_holder],       obj_extract("rights_holder")
-    to_field field_map[:series_arrangement],  obj_extract("series_arrangement")
+    to_field "text_no_boost_tesim", obj_extract("inscription"), transform(->(v) { "#{v.location}: #{v.text}" })
+    to_field "text_no_boost_tesim", obj_extract("additional_credit"), transform(->(v) { "#{v.role}: #{v.name}" })
 
-    to_field field_map[:inscription],
-      obj_extract("inscription"), transform(->(v) { "#{v.location}: #{v.text}" })
+    to_field "text_no_boost_tesim", obj_extract("digitization_funder")
+    to_field "text_no_boost_tesim", obj_extract("extent")
 
-    to_field field_map[:additional_credit],
-      obj_extract("additional_credit"), transform(->(v) { "#{v.role}: #{v.name}" })
+    to_field "text_no_boost_tesim", obj_extract("physical_container"), transform( ->(v) { v.display_as })
 
-    to_field field_map[:digitization_funder], obj_extract("digitization_funder")
-    to_field field_map[:extent], obj_extract("extent")
-
-    to_field field_map[:physical_container],
-      obj_extract("physical_container"), transform( ->(v) { v.display_as })
-
-
-    to_field field_map[:admin_note], obj_extract("admin_note")
-    to_field field_map[:exhibition], obj_extract("exhibition")
+    # We put these in a separate field, cause we only allow logged in users
+    # to search them
+    to_field "admin_only_text_tesim", obj_extract("admin_note")
+    to_field ["admin_only_text_tesim", "exhibition_facet"], obj_extract("exhibition")
 
     # for date/year range facet
-    to_field field_map[:expanded_years] do |record, acc|
+    to_field "year_facet_isim" do |record, acc|
       acc.concat(DateIndexHelper.new(record).expanded_years)
     end
 
     # For sorting by oldest first
-    to_field field_map[:earliest_date]  do |record, acc|
+    to_field "earliest_date" do |record, acc|
       # for Solr, we need in "xml schema" format, with 00:00:00 time, and UTC timezone
       # Rails date extensions are confusing, but this works to get it.
       acc << DateIndexHelper.new(record).min_date&.in_time_zone("UTC")&.xmlschema
     end
 
     # for sorting by newest first
-    to_field field_map[:latest_date] do |record, acc|
+    to_field "latest_date" do |record, acc|
       # for Solr, we need in "xml schema" format, with 00:00:00 time, and UTC timezone
       # Rails date extensions are confusing, but this works to get it.
       acc << DateIndexHelper.new(record).max_date&.in_time_zone("UTC")&.xmlschema
@@ -181,25 +81,25 @@ class WorkIndexer < Kithe::Indexer
     # Right now we only allow collection membership to be changed in the UI on the Work
     # edit page, and clicking save will re-index the work anyway (need to test to be sure).
     # But if you are programmatically changing the join table objects, beware.
-    to_field field_map[:collection_id], obj_extract("contains_contained_by", "to_a", "container_id")
+    to_field "collection_id_ssim", obj_extract("contains_contained_by", "to_a", "container_id")
 
 
     # Note standard created_at, updated_at, and published are duplicated
     # in CollectionIndexer. Maybe we want to DRY it somehow.
-    to_field field_map[:created_at] do |rec, acc|
+    to_field "date_created_dtsi" do |rec, acc|
       if rec.created_at
         acc << rec.created_at.utc.iso8601
       end
     end
 
-    to_field field_map[:updated_at] do |rec, acc|
+    to_field "date_modified_dtsi" do |rec, acc|
       if rec.updated_at
         acc << rec.updated_at.utc.iso8601
       end
     end
 
 
-    to_field field_map[:published_at] do |rec, acc|
+    to_field "date_published_dtsi" do |rec, acc|
       if rec.published_at
         acc << rec.published_at.utc.iso8601
       end
@@ -208,7 +108,7 @@ class WorkIndexer < Kithe::Indexer
 
     # for now we index 'published', not sure if we'll move to ONLY indexing
     # things that are published.
-    to_field field_map[:published_boolean], obj_extract("published?")
+    to_field "published_bsi", obj_extract("published?")
 
     # For oral histories, we want a facet with what features/media types are included.
     # We use format including "Sound" as a proxy for whether there are audio recordings -- we
@@ -216,7 +116,7 @@ class WorkIndexer < Kithe::Indexer
     # children of audio type, so we just count on 'format' being set appropriately.
     #
     # For OHMS transcript, we can actually check directly.
-    to_field field_map[:oh_feature_facet] do |rec, acc|
+    to_field "oh_feature_facet" do |rec, acc|
       if rec.is_oral_history?
         acc << "Audio recording" if rec.format&.include?("sound")
         acc << "Transcript" if rec.format&.include?("text")
@@ -227,7 +127,7 @@ class WorkIndexer < Kithe::Indexer
       end
     end
 
-    to_field field_map[:oh_availability_facet] do |rec, acc|
+    to_field "oh_availability_facet" do |rec, acc|
       if rec.is_oral_history? && rec.oral_history_content
         acc << case rec.oral_history_content.available_by_request_mode
         when "automatic"
@@ -246,12 +146,12 @@ class WorkIndexer < Kithe::Indexer
 
     # We need the #to_a in there to get past the `ActiveRecord::Associations::CollectionProxy` cause it isn't
     # REALLY an array. https://github.com/sciencehistory/kithe/issues/119
-    to_field field_map[:oh_institution_facet],
+    to_field "oh_institution_facet",
         obj_extract("oral_history_content", "interviewee_biographies", "to_a", "school", "institution"),
         obj_extract("oral_history_content", "interviewee_biographies",  "to_a", "job", "institution"),
         unique
 
-    to_field field_map[:oh_birth_country_facet], obj_extract("oral_history_content", "interviewee_biographies", "to_a", "birth", "country_name")
+    to_field "oh_birth_country_facet", obj_extract("oral_history_content", "interviewee_biographies", "to_a", "birth", "country_name")
 
     # Oral History Transcript and Table of Contents elements to a highlighted field
     #
@@ -259,7 +159,7 @@ class WorkIndexer < Kithe::Indexer
     # we got it.
     #
     # 2. If OHMS Table of Contents is present: title, synopsis, and keywords
-    to_field field_map[:fulltext_en] do |rec, acc|
+    to_field "searchable_fulltext_en" do |rec, acc|
       if rec.oral_history_content
         if rec.oral_history_content.has_ohms_transcript?
           text = rec.oral_history_content.ohms_xml.transcript_text
@@ -288,21 +188,21 @@ class WorkIndexer < Kithe::Indexer
       acc.concat get_string_from_each_published_member(rec, :transcription) if rec.language == ['English']
     end
 
-    to_field field_map[:fulltext_de] do |rec, acc|
+    to_field "searchable_fulltext_de" do |rec, acc|
       # Index the transcription here if we can assume that the work is entirely in German.
       acc.concat get_string_from_each_published_member(rec, :transcription) if rec.language == ['German']
     end
 
 
     # Index the transcription here unless we have place to index it in our language-specific indexes.
-    to_field field_map[:fulltext_agnostic] do |rec, acc|
+    to_field "searchable_fulltext_language_agnostic" do |rec, acc|
       entirely_in_english = (rec.language == ['English'])
       entirely_in_german =  (rec.language == ['German'])
       acc.concat get_string_from_each_published_member(rec, :transcription) unless entirely_in_english || entirely_in_german
     end
 
     # add a 'translation' token in bredig_feature_facet if we have any translations
-    to_field field_map[:bredig_feature_facet] do |rec, acc|
+    to_field "bredig_feature_facet" do |rec, acc|
       if rec.members && rec.members.any? {|m| m.is_a?(Asset) && m.english_translation.present? }
         acc << "English Translation"
       end
