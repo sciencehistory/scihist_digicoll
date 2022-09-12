d88bfbfa1135f0e3e284fa70a7ebaee34f53a04a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-09-12 16:44:04 -0400

    actually testing that the error is logged

--- spec/services/more_like_this_getter_spec.rb
+++ spec/services/more_like_this_getter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapter: :inline do
   let(:getter) {MoreLikeThisGetter.new(work_to_match)}
-  let(:getter_with_only_three_works) {MoreLikeThisGetter.new(work_to_match, max_number_of_works: 3)}
+  let(:getter_of_two_works) {MoreLikeThisGetter.new(work_to_match, max_number_of_works: 2)}
   let(:work_to_match)   { create(:public_work, subject: "aaa", description: "aaa")  }
   let(:five_public_works) { [
       create(:public_work, subject: "aaa", description: "aaa"),
@@ -26,11 +26,11 @@ describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapt
 
   it "can limit the number of works returned" do
     # mocked_solr_docs has 5 items...
-    allow(getter_with_only_three_works).to receive(:more_like_this_doc_set).and_return mocked_solr_docs
-    expect(getter_with_only_three_works.works).to eq five_public_works[0..2]
+    allow(getter_of_two_works).to receive(:more_like_this_doc_set).and_return mocked_solr_docs
+    expect(getter_of_two_works.works).to eq five_public_works[0..1]
   end
 
-  it "correctly retrieves only the public works" do
+  it "retrieves only public works" do
     allow(getter).to receive(:mlt_params).and_return({
       "q"         => "id:#{work_to_match.friendlier_id}",
       "mlt.fl"    => 'more_like_this_keywords_tsimv,more_like_this_fulltext_tsimv',
@@ -50,23 +50,20 @@ describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapt
     expect(getter.works.include? work_to_match).to be false
   end
 
-  it "correctly orders items returned by solr" do
+  it "delivers works in the same order their ids arrived from solr" do
     ids_in_order_of_similarity = five_public_works.map(&:friendlier_id)
     allow(getter).to receive(:friendlier_ids).and_return ids_in_order_of_similarity
     expect(getter.works).to eq five_public_works
   end
 
-
-  it "correctly handles and logs a solr error" do
-   allow(getter).to receive(:solr_connection).and_raise(RSolr::Error::ConnectionRefused)
-   expect(getter.works).to eq []
-   # TODO determine why this is failing:
-   # expect(Rails.logger).to have_received(:error).with(/while trying to fetch/)
+  it "recovers from a solr error and logs the error" do
+    expect(Rails.logger).to receive(:error).with(/ConnectionRefused .* #{work_to_match.friendlier_id}/)
+    allow(getter).to receive(:solr_connection).and_raise(RSolr::Error::ConnectionRefused)
+    expect(getter.works).to eq []
   end
 
   it "fails gracefully if the solr connection isn't available" do
     allow(getter).to receive(:solr_connection).and_return(nil)
     expect(getter.more_like_this_doc_set).to eq []
   end
-
 end
