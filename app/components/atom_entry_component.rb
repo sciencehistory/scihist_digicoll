b47c49e5b0f655b1343d3f26e6bd571bd3e7a8ce
Jonathan Rochkind <jonathan@dnil.net>
2022-09-22 11:14:51 -0400

    atom results support collections in results too
    But without links to .xml and .json metadata, since we don't have them

--- app/components/atom_entry_component.rb
+++ app/components/atom_entry_component.rb
@@ -3,6 +3,7 @@ class AtomEntryComponent < ApplicationComponent
 
   def initialize(model)
     raise ArgumentError.new("#{self.class} requires non-nil model argument") unless model
+    raise ArgumentError.new("#{self.class} only supports Work and Collection, not `#{model.class.name}`") unless model.kind_of?(Work) || model.kind_of?(Collection)
     @model = model
   end
 
@@ -12,4 +13,31 @@ class AtomEntryComponent < ApplicationComponent
     WorkOaiDcSerialization.shareable_thumbnail_url(model)
   end
 
+  def model_html_url
+    if model.kind_of?(Work)
+      work_url(model)
+    else
+      collection_url(model)
+    end
+  end
+
+  def model_alternate_links
+    if model.kind_of?(Work)
+      [
+        {
+          type: "application/xml",
+          title: "OAI-DC metadata in XML",
+          href: work_url(model, format: "xml")
+        },
+        {
+          type: "application/json",
+          title: "local non-standard metadata in JSON",
+          href: work_url(model, format: "json")
+        }
+      ]
+    else
+      # we don't currently have .xml and .json metadata links for Collections
+      []
+    end
+  end
 end
--- spec/components/atom_entry_component_spec.rb
+++ spec/components/atom_entry_component_spec.rb
@@ -20,4 +20,26 @@ describe AtomEntryComponent, type: :component do
       expect(rendered.at_css("summary[type=html]").text.strip).to eq DescriptionDisplayFormatter.new(work.description).format
     end
   end
+
+  describe "for collection" do
+    let(:collection) { create(:collection, :with_representative, published: true) }
+    let(:rendered) { render_inline(AtomEntryComponent.new(collection)) }
+
+    it "produces atom entry" do
+      expect(rendered.at_css("title")&.text).to eq collection.title
+      expect(rendered.at_css("updated")&.text).to eq collection.updated_at.iso8601
+
+      expect(rendered.at_css("link[rel=alternate][type='text/html']")["href"]).to eq collection_url(collection)
+
+      # we don't support collection metadata APIs at present
+      expect(rendered.at_css("link[rel=alternate][type='application/json']")).to be_nil
+      expect(rendered.at_css("link[rel=alternate][type='application/xml']")).to be_nil
+
+      # I don't know why the namespace is being funny here, not letting us use ordinary
+      # nokogiri actual nameespace checking for "media:thumbnanil"
+      expect(rendered.at_css("thumbnail").text).to eq WorkOaiDcSerialization.shareable_thumbnail_url(collection)
+
+      expect(rendered.at_css("summary[type=html]").text.strip).to eq DescriptionDisplayFormatter.new(collection.description).format
+    end
+  end
 end
