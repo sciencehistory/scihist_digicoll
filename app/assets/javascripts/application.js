4b8f0505b38ef66fc0eab3c5276f06a6154ba373
Jonathan Rochkind <jonathan@dnil.net>
2022-10-18 14:11:11 -0400

    avoid browse_everything doing a double-include of bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+// browse_everything https://github.com/samvera/browse-everything
 //
+// * JS is at the moment only available via sprockets.
+// * The standard way to include `require browse_everything` will also
+//   include`bootstrap. We want to control bootstrap inclusion ourselves,
+//   possibly via vite. So we include the sub-parts directly, excluding
+//   bootstrap.
+// * See https://github.com/samvera/browse-everything/issues/411
 //
-//
-//= require browse_everything
+//= require jquery.treetable
+//= require browse_everything/behavior
 
 // Required by Blacklight
       // not currently using blacklight 'suggest' func which uses twitter typeahead.
