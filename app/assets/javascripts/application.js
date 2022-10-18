467c54dc4e1288c4e4645d3e6eced4e6ac7addff
Jonathan Rochkind <jonathan@dnil.net>
2022-10-18 14:16:30 -0400

    update comment header in sprockets application.js, for move to vite

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// This is a SPROCCKETS manifest file that'll be compiled into an application.js
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
+// It can include files via sprockets `//= require` directives in JS comments,
+// below.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// Our sciencehistory app generally tries to avoid using sprockets for JS now, we
+// use vite. This *only* includes assets which cannot be conveniently included via
+// vite, for instance from dependencies that *only* provide their assets in manner
+// for sprockets inclusion.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+// This will produce a secondary `application.js` included via rails
+// `javascript_include_tag`. For bulk of our JS, see vite stuff
+// at ./app/frontend
 
 
 // browse_everything https://github.com/samvera/browse-everything
@@ -24,6 +25,8 @@
 //= require jquery.treetable
 //= require browse_everything/behavior
 
+
+
 // Required by Blacklight
       // not currently using blacklight 'suggest' func which uses twitter typeahead.
       // twitter typeahead is a non maintained kind of mess, so we might try to
@@ -33,9 +36,13 @@
       // require twitter/typeahead
 //= require blacklight/blacklight
 
+
+
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
+
+
 
 //= require_tree .
 
