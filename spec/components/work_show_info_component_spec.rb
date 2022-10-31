b6b494436e144804d11932f9cf777192c2fb5dfd
Jonathan Rochkind <jonathan@dnil.net>
2022-10-31 15:50:31 -0400

    split exhibition out from collection on work show metadata

--- spec/components/work_show_info_component_spec.rb
+++ spec/components/work_show_info_component_spec.rb
@@ -14,4 +14,17 @@ describe WorkShowInfoComponent, type: :component do
       expect(page).to have_text(/Oral history number\s+0012/)
     end
   end
+
+  context "exhibitions and collections" do
+    let(:exhibition) { build(:collection, department: Collection::DEPARTMENT_EXHIBITION_VALUE) }
+    let(:collection) { build(:collection) }
+    let(:work) { create(:public_work, :with_complete_metadata, contained_by: [exhibition, collection])}
+
+    it "displays exhibition separately" do
+      rendered = render_inline WorkShowInfoComponent.new(work: work)
+
+      expect(page).to have_text(/Collection\s+#{collection.title}/)
+      expect(page).to have_text(/Exhibited in\s+#{exhibition.title}/)
+    end
+  end
 end
