616a57f3adaa1db6b7a57d216eaf409291f7b395
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-10-31 09:47:00 -0400

    new code from survey sparrow

--- app/frontend/javascript/survey_widget.js
+++ app/frontend/javascript/survey_widget.js
@@ -56,37 +56,8 @@ function surveyAlreadyShown() {
 }
 
 // This is taken from the survey sparrow site.
-function sparrowLaunch(opts) {
-  var e = "ss-widget",
-    t = "script",
-    a = document,
-    r = window,
-    l = localStorage;
-  var s, n, c, rm = a.getElementById('SS_SCRIPT');
-  r.SS_WIDGET_TOKEN = "tt-5zUYbALWMcKM1wqSzFDcLi";
-  r.SS_ACCOUNT = "sciencehistory.surveysparrow.com";
-  r.SS_SURVEY_NAME = "digitial-collections-survey";
-  if (!a.getElementById(e) && !l.getItem('removed-ss-widget-tt-5zUYbALWMcKM1wqSzFDcLi')) {
-    var S = function() {
-      S.update(arguments)
-    };
-    S.args = [];
-    S.update = function(e) {
-      S.args.push(e)
-    };
-    r.SparrowLauncher = S;
-    s = a.getElementsByTagName(t);
-    c = s[s.length - 1];
-    n = a.createElement(t);
-    n.type = "text/javascript";
-    n.async = !0;
-    n.id = e;
-    n.src = ["https://", "sciencehistory.surveysparrow.com/widget/", r.SS_WIDGET_TOKEN, "?", "customParams=", JSON.stringify(opts)].join("");
-    c.parentNode.insertBefore(n, c);
-    r.SS_VARIABLES = opts;
-    rm.parentNode.removeChild(rm);
-  }
-};
+function sparrowLaunch(opts){var e="ss-widget",t="script",a=document,r=window,l=localStorage;var s,n,c,rm=a.getElementById('SS_SCRIPT');r.SS_WIDGET_TOKEN="tt-1nbxxaSrT4E1QMjegveHmU";r.SS_ACCOUNT="sciencehistory.surveysparrow.com";r.SS_SURVEY_NAME="digitial-collections-survey";if(!a.getElementById(e) && !l.getItem('removed-ss-widget-tt-1nbxxaSrT4E1QMjegveHmU')){var S=function(){S.update(arguments)};S.args=[];S.update=function(e){S.args.push(e)};r.SparrowLauncher=S;s=a.getElementsByTagName(t);c=s[s.length-1];n=a.createElement(t);n.type="text/javascript";n.async=!0;n.id=e;n.src=["https://","sciencehistory.surveysparrow.com/widget/",r.SS_WIDGET_TOKEN,"?","customParams=",JSON.stringify(opts)].join("");c.parentNode.insertBefore(n,c);r.SS_VARIABLES=opts;rm.parentNode.removeChild(rm);}};
+
 
 // Useful for testing; just call this function
 // from the console and you can test repeatedly.
