de0a59e7b533a31b6060594ad2cac0745027374c
Jonathan Rochkind <jonathan@dnil.net>
2022-11-03 15:21:46 -0400

    set up vite resolve alias to our desired stylesheets dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import { brotliCompressSync } from "zlib";
 import gzipPlugin from "rollup-plugin-gzip";
+import { resolve } from 'path'
+
 
 // gzip and brotli plugin from https://github.com/ElMassimo/vite_ruby/discussions/101#discussioncomment-1019222
 // and https://vite-ruby.netlify.app/guide/deployment.html#compressing-assets-%F0%9F%93%A6
@@ -10,6 +12,7 @@ import gzipPlugin from "rollup-plugin-gzip";
 // in our heroku setup where the Rails app is directly serving assets (cached by CDN)
 //
 // https://github.com/ElMassimo/vite_ruby/discussions/281
+
 
 export default defineConfig({
   plugins: [
@@ -36,5 +39,10 @@ export default defineConfig({
     // Seems necessary to get sourceMaps in dev autoBuild, which are kind of
     // important for being able to debug. https://github.com/ElMassimo/vite_ruby/discussions/285
     sourcemap: true
+  },
+  resolve: {
+    alias: {
+      "@stylesheets": resolve(__dirname, 'app/frontend/stylesheets')
+    }
   }
 })
