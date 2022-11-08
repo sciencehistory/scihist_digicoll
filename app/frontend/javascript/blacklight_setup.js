aea62f50aba9700383e6348260929022af30406f
Jonathan Rochkind <jonathan@dnil.net>
2022-11-08 15:48:17 -0500

    avoid dart-sass deprecated division with '/'
    We were mostly dividing by `2` or occasionally other simple integer, we can convert to a multiplication of 1/n.
    
    "DEPRECATION WARNING: Using / for division outside of calc() is deprecated and will be removed in Dart Sass 2.0.0.
    
    More info and automated migrator: https://sass-lang.com/d/slash-div"

---
+++ app/frontend/javascript/blacklight_setup.js
@@ -0,0 +1,24 @@
+// The Blacklight docs at
+//
+// Suggest importing ALL of BL JS with:
+//      import 'blacklight-frontend/app/javascripts/blacklight/'
+//
+// We instead take a picking-and-choosing approach to import only
+// the BL JS we want.  This is mainluy to *avoid* importing `typeahead`,
+// which at least in BL 7.x is a LOT of code (it brings in some big dependencies)
+//
+// It's possible we're still including other JS we don't actually use; on the other
+// side, future versions of BL may include JS that we want but don't automatically
+// get included without us realizing and editing here.
+//
+// While this picking-and-choosing apporach is not documented, other BL
+// apps in the wild do it, eg:
+//
+//   * https://github.com/psu-libraries/psulib_blacklight/blob/451a8ab9e64eaed8b0000bda0300a4f28097f165/package.json
+//   * https://github.com/unt-libraries/discover/blob/3c02d22bda7ec3f9e1d2f71a49c3b7e662ef8758/app/webpacker/packs/base.js.erb
+
+import 'blacklight-frontend/app/javascript/blacklight/core';
+import 'blacklight-frontend/app/javascript/blacklight/modal';
+// import 'blacklight-frontend/app/javascript/blacklight/button_focus';
+import 'blacklight-frontend/app/javascript/blacklight/facet_load';
+//import 'blacklight-frontend/app/javascript/blacklight/search_context';
