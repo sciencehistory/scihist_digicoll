ed9aec92f6db481e5f95f6ab108f636dfb713bc6
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-11-22 15:55:40 -0500

    more edits to access policy

--- app/policies/access_policy.rb
+++ app/policies/access_policy.rb
@@ -8,8 +8,6 @@ class AccessPolicy
   include AccessGranted::Policy
 
   def configure
-    # The most important admin role, gets checked first
-
     role :admin, proc { |user| user&.admin? } do
       can :see_admin
       can :manage, Kithe::Model
@@ -17,21 +15,40 @@ class AccessPolicy
       can :destroy, Admin::QueueItemComment do |comment, user|
         comment.user_id == user.id
       end
+      can :manage, InterviewerProfile
+      can :manage, IntervieweeBiography
+      can :manage, Admin::DigitizationQueueItem
     end
 
     role :editor, proc {  user&.editor? } do
       can :see_admin
+
       can :read, Kithe::Model # whether published or not
       can :update, Kithe::Model
       can :publish, Kithe::Model
+      
+      can :manage, InterviewerProfile
+      can :manage, IntervieweeBiography
+      can :manage, Admin::DigitizationQueueItem
     end
 
     role :viewer, proc { |user| user&.viewer? } do
       can :see_admin
+      
       can :read, Kithe::Model # whether published or not
+      
+      can :read, InterviewerProfile
+      can :read, IntervieweeBiography
+      can :read, Admin::DigitizationQueueItem
     end
 
     role :public do
+      # TODO:
+      # Deleting this line has no effect
+      # on any user's permissions.
+      #
+      # Conversely, adding it to the role
+      # of the active user causes an error.
       can :read, Kithe::Model, { published: true }
     end
 
