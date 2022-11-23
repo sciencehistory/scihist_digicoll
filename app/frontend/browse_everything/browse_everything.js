f0d89d33ce8e5a21ccad159c241ac9844b2d80ad
Jonathan Rochkind <jonathan@dnil.net>
2022-11-23 15:19:17 -0500

    use more consistent path

--- app/frontend/browse_everything/browse_everything.js
+++ app/frontend/browse_everything/browse_everything.js
@@ -1,8 +1,8 @@
 // Note more than one `.` in filename messed up vite CSS import from JS, we
 // turned them into hyphens.
-import "~/browse_everything/stylesheets/jquery.treetable.css";
-import "~/browse_everything/stylesheets/jquery.treetable.theme.browse.css";
-import "~/browse_everything/stylesheets/browse_everything_bootstrap4.scss";
+import "./stylesheets/jquery.treetable.css";
+import "./stylesheets/jquery.treetable.theme.browse.css";
+import "./stylesheets/browse_everything_bootstrap4.scss";
 
 import "./javascript/jquery.treetable.js";
 import "./javascript/behavior.js";
