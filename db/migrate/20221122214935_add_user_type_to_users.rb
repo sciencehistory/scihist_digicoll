0f9fc19d4813680a2d3fbec9151611afda6fac4d
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-11-30 10:52:57 -0500

    adding migration comment

--- db/migrate/20221122214935_add_user_type_to_users.rb
+++ db/migrate/20221122214935_add_user_type_to_users.rb
@@ -1,4 +1,6 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[6.1]
+  # This user_type column will eventually allow us to have more than
+  # two user types. See https://github.com/sciencehistory/scihist_digicoll/issues/1948
   def change
     add_column :users, :user_type, :string, default: 'editor'
   end
