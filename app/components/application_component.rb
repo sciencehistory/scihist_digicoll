f9b86778a3bbccf67a9bda5f5c5a43641162f4b1
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-12-01 19:25:47 -0500

    no need for current_user here anymore

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,6 +1,3 @@
 class ApplicationComponent < ViewComponent::Base
-  delegate :current_user, :can?, to: :helpers
-  # def access_policy
-  #   @access_policy ||= AccessPolicy.new(current_user)
-  # end
+  delegate :can?, to: :helpers
 end
