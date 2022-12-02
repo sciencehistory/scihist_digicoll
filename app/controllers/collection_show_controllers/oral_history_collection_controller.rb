4dd488e4758e6d5bf93fba7fb526745821f5c235
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-12-02 14:26:21 -0500

    method already defined higher up in the class hierarchy

--- app/controllers/collection_show_controllers/oral_history_collection_controller.rb
+++ app/controllers/collection_show_controllers/oral_history_collection_controller.rb
@@ -33,14 +33,6 @@ module CollectionShowControllers
       end
     end
 
-    # Should be equivalent to:
-    # ->(controller, field) { controller.can?(:access_staff_functions) }
-    # but was not able to get this syntax to work.
-    def can_see_all_search_facets?
-      can? :access_staff_functions
-    end
-
-
     # Collections we want to list as "projects", ordered by same order as their ids in
     # PROJECT_COLLECTION_FRIENDLIER_IDS
     helper_method def project_list
