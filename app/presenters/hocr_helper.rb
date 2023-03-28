c21d02a72289ee39009e0632e4f1bf6bfdd66b5c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-03-28 14:12:00 -0400

    moving to app/presenters

--- app/presenters/hocr_helper.rb
+++ app/presenters/hocr_helper.rb
@@ -1,7 +1,7 @@
-module HocrHelper
-
+# Some static utility methods for printing friendly versions of HOCR .
+class HocrHelper
   # Just the text content of the body. No tags.
-  def text_content_as_string(raw_hocr)
+  def self.text_content_as_string(raw_hocr)
     doc = Nokogiri::HTML(raw_hocr) { |config| config.strict }
     doc.xpath('//@*').remove
     body =  doc.css('body')[0]
@@ -10,11 +10,10 @@ module HocrHelper
 
   # With tags, but with ids stripped out. Several of these
   # can be printed in HTML without causing colliding ids.
-  def html_body_without_ids(raw_hocr)
+  def self.html_body_without_ids(raw_hocr)
     doc = Nokogiri::HTML(raw_hocr) { |config| config.strict }
     body =  doc.css('body')[0]
     body.xpath('//@id').remove
-    body.children.to_html
+    body.children.to_html.html_safe
   end
-
 end
--- spec/presenters/hocr_helper_spec.rb
+++ spec/presenters/hocr_helper_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe HocrHelper, type: :helper do
+describe HocrHelper do
   describe "#strip_ids_and_html_headers" do
     let (:hocr) { File.read('spec/test_support/hocr_xml/hocr.xml')}
 
     it "can return the body as a friendly string." do
-      processed_hocr =  helper::text_content_as_string(hocr)
+      processed_hocr =  HocrHelper.text_content_as_string(hocr)
       expect(processed_hocr).to include "lower left rear corner of the Recorder"
     end
 
     it "can return the contents of the body with tags stripped of IDs." do
-      processed_hocr =  helper::html_body_without_ids(hocr)
+      processed_hocr =  HocrHelper.html_body_without_ids(hocr)
       expect(processed_hocr).to include '<span class="ocrx_word" title="bbox 1405 564 1556 606; x_wconf 96">Supply</span>'
       expect(processed_hocr).not_to include  'id="word_1_1"'
     end
