deb679d73964e8bccff23bdd8e544df00d1225dc
Jonathan Rochkind <jonathan@dnil.net>
2023-04-13 14:05:26 -0400

    get fade in/out menu animation to work, by reverse-engineering from main website source

--- app/frontend/javascript/main_nav_collapse_toggle.js
+++ app/frontend/javascript/main_nav_collapse_toggle.js
@@ -1,10 +1,27 @@
 // Some very simple custom JS to duplicate main site collapsed navbar toggle
+//
+// We have to do some tricks with adding/removing TWO classes with a slightly delay between,
+// to get the fade-in/out animation we want to work, with display:block/none too -- we reverse-engineered
+// that from the main website source.
 document.querySelector(".js-shi-nav-close")?.addEventListener("click", function(e) {
-  document.querySelector("#shi-masthead-from-main-website .header")?.classList.remove("header_menu-open");
+  var header = document.querySelector("#shi-masthead-from-main-website .header");
+
+  if(!header) {
+    return;
+  }
+
+  header.classList.remove("header_menu-open");
+  setTimeout((()=>header.classList.remove("header_menu-prepare")), 300);
 });
 
 document.querySelector(".js-header-sandwich-btn")?.addEventListener("click", function(e) {
-  document.querySelector("#shi-masthead-from-main-website .header")?.classList.add("header_menu-open");
+    var header = document.querySelector("#shi-masthead-from-main-website .header");
+
+    if(!header) {
+      return;
+    }
+    document.querySelector("#shi-masthead-from-main-website .header")?.classList.add("header_menu-prepare");
+    setTimeout((()=>header.classList.add("header_menu-open")), 0);
 });
 
 
