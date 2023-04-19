9838931c1a26532075d19e34666c055934345a2e
Jonathan Rochkind <jonathan@dnil.net>
2023-04-19 14:49:12 -0400

    better name and encapsulation

--- app/components/scihist_masthead.rb
+++ app/components/scihist_masthead.rb
@@ -1,3 +1,11 @@
 class ScihistMasthead < ApplicationComponent
+  attr_reader :suppress_product_subhead
+
+  # @param suppress_product_subhead [Boolean] (false) If true, omit the sub-head
+  # with "Digital Collections" and a search bar. Used on home page, or other pages that
+  # may provide this content and functionality in other ways?
+  def initialize(suppress_product_subhead: false)
+    @suppress_product_subhead = suppress_product_subhead
+  end
 
 end
