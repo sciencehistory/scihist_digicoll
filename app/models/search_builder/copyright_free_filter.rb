c8247cc8db89500c4956b6cfa450bd41bd911da4
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-05-11 12:45:16 -0400

    keep track of what we consider 'copyright free' in the yml file

--- app/models/search_builder/copyright_free_filter.rb
+++ app/models/search_builder/copyright_free_filter.rb
@@ -10,27 +10,16 @@ class SearchBuilder
     end
 
     def copyright_free_filter(solr_params)
-      # For the purposes of search, we consider the following to meet the definition of "Copyright Free"
-      # See https://github.com/sciencehistory/scihist_digicoll/issues/2125 for some context
-      # The terms are defined in config/data/rights_terms.yml
-      considered_copyright_free = [
-        # Public Domain Mark 1.0
-        "http://creativecommons.org/publicdomain/mark/1.0/",
-        # No Known Copyright
-        "http://rightsstatements.org/vocab/NKC/1.0/",
-        # No Copyright - United States
-        "http://rightsstatements.org/vocab/NoC-US/1.0/",
-        # No Copyright - Other Known Legal Restrictions
-        "http://rightsstatements.org/vocab/NoC-OKLR/1.0/"
-      ].join(",")
-
+      # We consider these URIs "free of copyright" for the purposes of our search checkbox:
+      uris_to_match = RightsTerm.copyright_free_filter_uris().join(",")
+      
       # look for the "Copyright Free" checkbox
       if SearchBuilder::CopyrightFreeFilter.filtered_copyright_free?(blacklight_params)
         solr_params[:fq] ||= []
         # SOLR note:
         # The terms query parser "takes in multiple values separated by commas and returns documents matching any of the specified values."
         # https://solr.apache.org/guide/solr/latest/query-guide/other-parsers.html#terms-query-parser
-        copyright_free_filter = "{!terms f=rights_facet}#{considered_copyright_free}"
+        copyright_free_filter = "{!terms f=rights_facet}#{uris_to_match}"
         solr_params[:fq] << copyright_free_filter
       end
     end
