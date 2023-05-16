bdb5f1cc6a0b64edaf9967508506360aed659e25
Jonathan Rochkind <jonathan@dnil.net>
2023-05-16 15:34:12 -0400

    try image action buttons in off-black

--- app/components/download_dropdown_component.rb
+++ app/components/download_dropdown_component.rb
@@ -169,7 +169,7 @@ class DownloadDropdownComponent < ApplicationComponent
       if viewer_template_mode?
         options[:class] = "btn btn-emphasis btn-lg dropdown-toggle"
       else
-        options[:class] = "btn btn-brand-main dropdown-toggle"
+        options[:class] = "btn btn-brand-dark dropdown-toggle"
       end
     end
     options
