4a07c5add6118bb6d9ca7662014b8e58005dd2c0
Jonathan Rochkind <jonathan@dnil.net>
2023-07-03 16:00:51 -0400

    fix blacklight-frontend JS import in a way that works for BL8

--- app/frontend/javascript/blacklight_setup.js
+++ app/frontend/javascript/blacklight_setup.js
@@ -1,31 +1,29 @@
-// The Blacklight docs at
-//    https://github.com/projectblacklight/blacklight/wiki/Using-Webpacker-to-compile-javascript-assets#installing-webpacker-in-blacklight
+// While not doc'd very well, this seems to be the way to import all blacklight Javascript.
+
+import 'blacklight-frontend';
+
+// That, I think, is a file created at release-time that collects ALL of the blacklight JS modules.
+
+
+// This could have also worked, same thing I think?
+// import 'blacklight-frontend/app/assets/javascripts/blacklight/blacklight';
+
+
+
+// We USED to be able to pick-and-choose just the ones we need -- we don't actually need all of them, because
+// we don't use all BL parts.
 //
-// Suggest importing ALL of BL JS with:
+// See some docs (not necessarily kept up to date) at https://github.com/projectblacklight/blacklight/wiki/Using-Webpacker-to-compile-javascript-assets
 //
-//      import 'blacklight-frontend/app/javascripts/blacklight/blacklight'
-//
-// BUT we instead take a picking-and-choosing approach to import only
-// the BL JS we want.  This is mainluy to *avoid* importing `typeahead`,
-// which at least in BL 7.x is a LOT of code (it brings in some big dependencies)
-//
-// It's possible we're still including other JS we don't actually use; on the other
-// side, future versions of BL may include JS that we want but don't automatically
-// get included without us realizing and editing here.
-//
-// While this picking-and-choosing apporach is not documented, other BL
-// apps in the wild do it, eg:
+// And also see these other apps selectively importing in Blacklight 7:
 //
 //   * https://github.com/psu-libraries/psulib_blacklight/blob/451a8ab9e64eaed8b0000bda0300a4f28097f165/app/javascript/psulib_blacklight/index.js
 //   * https://github.com/unt-libraries/discover/blob/3c02d22bda7ec3f9e1d2f71a49c3b7e662ef8758/app/webpacker/packs/base.js.erb
 //
-// From all sub-parts at Blacklight 7.31.0
+// As of Blacklight 8.0.1, that does not seem possible anymore. Should it be? Questions asked at:
 //
+// https://github.com/projectblacklight/blacklight/issues/3050
 
-import 'blacklight-frontend/app/javascript/blacklight/core';
-// import 'blacklight-frontend/app/javascript/blacklight/bookmark_toggle';
-// import 'blacklight-frontend/app/javascript/blacklight/button_focus';
-// import 'blacklight-frontend/app/javascript/blacklight/checkbox_submit';
-import 'blacklight-frontend/app/javascript/blacklight/facet_load';
-import 'blacklight-frontend/app/javascript/blacklight/modal';
-//import 'blacklight-frontend/app/javascript/blacklight/search_context';
+
+
+
