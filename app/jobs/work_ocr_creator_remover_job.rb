bad78761fc00da6513d3afa63b2dc11045c14beb
Jonathan Rochkind <jonathan@dnil.net>
2023-07-13 11:38:16 -0400

    WorkOcrCreatorRemover discard on deserialiation error, which includes record deleted before job performed

--- app/jobs/work_ocr_creator_remover_job.rb
+++ app/jobs/work_ocr_creator_remover_job.rb
@@ -2,6 +2,11 @@
 # with the Work#ocr_requested_attribute
 #
 class WorkOcrCreatorRemoverJob < ApplicationJob
+  # if the work has already been deleted before the job is run, just discard
+  # This is how ActiveJob docs suggest to do that (https://guides.rubyonrails.org/active_job_basics.html#deserialization),
+  # although actually this eats lots of other errors too, but we'll take it I guess?
+  discard_on ActiveJob::DeserializationError
+
   def perform(work)
     WorkOcrCreatorRemover.new(work).process
   end
