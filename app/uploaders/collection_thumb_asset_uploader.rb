d4d2d30ac8e2bb27fb72c87134c80082fbdd5c52
Jonathan Rochkind <jonathan@dnil.net>
2023-08-03 12:24:34 -0400

    Don't inherit graphiconly_pdf on collection thumbs

--- app/uploaders/collection_thumb_asset_uploader.rb
+++ app/uploaders/collection_thumb_asset_uploader.rb
@@ -4,8 +4,11 @@ class CollectionThumbAssetUploader < AssetUploader
   # remove inherited derivatives we don't need for collections. Kind of bad OO
   # design maybe we should have an abstract asset superclass with ordinary and collection
   # asset both as sub-classes we don't need to do this, but it works for now.
+  #
+  # Should we actually be removing ALL derivatives defined in parent, so we don't forget
+  # to add here when we add new ones to parent we don't want here? Do we want ANY of them?
   Attacher.remove_derivative_definition!(*Attacher.defined_derivative_keys.find_all { |key| key.start_with?("download_")})
-  Attacher.remove_derivative_definition!(:thumb_large, :thumb_large_2X)
+  Attacher.remove_derivative_definition!(:thumb_large, :thumb_large_2X, :graphiconly_pdf)
 
   Attacher.define_derivative("thumb_collection_page", content_type: "image") do |original_file|
     Kithe::VipsCliImageToJpeg.new(max_width: COLLECTION_PAGE_THUMB_SIZE, thumbnail_mode: true).call(original_file)
