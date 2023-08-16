d431ef93160ea199c28599444adb672194a0e252
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-08-16 15:07:06 -0400

    easier to read summary

--- spec/system/work/ocr_info_spec.rb
+++ spec/system/work/ocr_info_spec.rb
@@ -4,6 +4,11 @@ describe "Member list displays OCR info", logged_in_user: :editor do
   let(:image_asset_without_hocr)   { create(:asset_with_faked_file, title: "No OCR", position: 1) }
   let(:image_asset_with_null_hocr) { create(:asset_with_faked_file, title: "Nil OCR", hocr: nil, position: 2) }
   let(:image_asset_with_hocr)      { create(:asset_with_faked_file, title: "Has OCR", hocr: "This is the HOCR", position: 3) }
+  let(:image_asset_with_hocr)      { create(:asset_with_faked_file, title: "Has OCR", hocr: "This is the HOCR", position: 4) }
+  let(:image_asset_with_ocr_suppressed) { create(:asset_with_faked_file, :suppress_ocr, title: "Asset with OCR suppressed", position: 5) }
+
+        
+
   let(:sound_asset) { create(:asset_with_faked_file, :m4a, title: "Sound file", position: 4 ) }
   let(:child_work)  { create(:public_work, representative: create(:asset_with_faked_file), title: "Child work", position: 1) }
 
@@ -11,7 +16,7 @@ describe "Member list displays OCR info", logged_in_user: :editor do
     create(:public_work,
       language: 'en',
       ocr_requested: true,
-      members: [ image_asset_without_hocr, image_asset_with_hocr, image_asset_with_null_hocr, sound_asset, child_work]
+      members: [ image_asset_without_hocr, image_asset_with_hocr, image_asset_with_null_hocr, sound_asset, child_work, image_asset_with_ocr_suppressed]
     )
   end
 
