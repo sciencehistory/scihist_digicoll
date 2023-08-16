c565bfb3350ff77886bfeb5a2bdc90bc612e4b7a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-08-16 10:56:15 -0400

    ok two seconds

--- spec/system/work/file_ingest_spec.rb
+++ spec/system/work/file_ingest_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "ingest files to work", logged_in_user: :editor, type: :system, j
 
   it "can add a file with restricted derivative storage", queue_adapter: :inline do
     visit admin_asset_ingest_path(work)
-    sleep 1
+    sleep 2
     add_file_via_uppy_dashboard(input_name: "files[]", file_path: Rails.root.join("spec/test_support/images/20x20.png"))
     expect(page).to have_css(".attach-files-table td", text: /20x20\.png/)
 
