855cffa18e9a80dcbd391f23099a973d9bbf29f6
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-08-25 14:29:19 -0400

    display `suppress_ocr` on the asset list page and on the work show page

--- spec/system/work/ocr_info_spec.rb
+++ spec/system/work/ocr_info_spec.rb
@@ -4,6 +4,9 @@ describe "Member list displays OCR info", logged_in_user: :editor do
   let(:image_asset_without_hocr)   { create(:asset_with_faked_file, title: "No OCR", position: 1) }
   let(:image_asset_with_null_hocr) { create(:asset_with_faked_file, title: "Nil OCR", hocr: nil, position: 2) }
   let(:image_asset_with_hocr)      { create(:asset_with_faked_file, title: "Has OCR", hocr: "This is the HOCR", position: 3) }
+  let(:image_asset_with_hocr)      { create(:asset_with_faked_file, title: "Has OCR", hocr: "This is the HOCR", position: 4) }
+  let(:image_asset_with_ocr_suppressed) { create(:asset_with_faked_file, :suppress_ocr, title: "Asset with OCR suppressed", position: 5) }
+
   let(:sound_asset) { create(:asset_with_faked_file, :m4a, title: "Sound file", position: 4 ) }
   let(:child_work)  { create(:public_work, representative: create(:asset_with_faked_file), title: "Child work", position: 1) }
 
@@ -11,7 +14,7 @@ describe "Member list displays OCR info", logged_in_user: :editor do
     create(:public_work,
       language: 'en',
       ocr_requested: true,
-      members: [ image_asset_without_hocr, image_asset_with_hocr, image_asset_with_null_hocr, sound_asset, child_work]
+      members: [ image_asset_without_hocr, image_asset_with_hocr, image_asset_with_null_hocr, sound_asset, child_work, image_asset_with_ocr_suppressed]
     )
   end
 
@@ -21,7 +24,10 @@ describe "Member list displays OCR info", logged_in_user: :editor do
 
       click_on "OCR"
       expect(page).to have_text(/OCR enabled\:\s+YES/)
-      expect(page).to have_text("1 out of 4 assets currently have OCR.")
+      expect(page).to have_text("Out of 5 assets")
+      expect(page).to have_text("1 asset currently has OCR")
+      expect(page).to have_text("1 asset has OCR suppressed")
+      expect(page).to have_text("OCR enabled, but work does not include languages compatible with OCR.")
 
       click_on "Members"
       path_to_ocr = admin_asset_path(image_asset_with_hocr, anchor: "ocr")
