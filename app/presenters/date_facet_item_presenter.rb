9898b6e54cda58dd1b413f0e8d1124e4bf5f1535
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-09-08 17:32:37 -0400

    Subclass BlacklightRangeLimit::FacetItemPresenter so we can label undated dates "Undated"

---
+++ app/presenters/date_facet_item_presenter.rb
@@ -0,0 +1,15 @@
+# Allows us to customize various aspects of the date field (year_facet_isim) facet.
+class DateFacetItemPresenter < BlacklightRangeLimit::FacetItemPresenter
+
+  # Look up the label for the facet for items missing a date in blacklight.en.yml .
+  def label
+    if value == {:missing=>true}
+      # The path to the label is taken from
+      # https://github.com/projectblacklight/blacklight/blob/457f89f3dba758c0e642e881724cd818c1cc5f9e/lib/blacklight/solr/response/facets.rb#L199
+      I18n.t(:"blacklight.search.fields.facet.missing.#{@facet_config.field}", default: super)
+    else
+      super
+    end
+  end
+
+end
