d5e494a27d5d187c23f837ce0ce7f90afed1633d
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-09-19 14:49:31 -0400

    check that q is an actual string

--- spec/requests/bad_blacklight_requests_spec.rb
+++ spec/requests/bad_blacklight_requests_spec.rb
@@ -84,4 +84,12 @@ describe CatalogController do
       expect(response.body).to match(/illegal page query parameter/)
     end
   end
+
+  describe "attempt to inject in page param" do
+    it "responds with a 400" do
+      get "/catalog?page=1&q[]=1"
+      expect(response.code).to eq "400"
+      expect(response.body).to match(/illegal q query parameter/)
+    end
+  end
 end
