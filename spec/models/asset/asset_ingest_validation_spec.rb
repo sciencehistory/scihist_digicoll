c98242c06226df38b3a40a5305105f09c6441b03
Jonathan Rochkind <jonathan@dnil.net>
2023-10-03 14:26:49 -0400

    Test async separate, since there are different code paths for promotion

--- spec/models/asset/asset_ingest_validation_spec.rb
+++ spec/models/asset/asset_ingest_validation_spec.rb
@@ -4,21 +4,41 @@ describe "Asset ingest validation" do
   describe "corrupt TIFF" do
     let(:corrupt_tiff_path) { Rails.root + "spec/test_support/images/corrupt_bad.tiff" }
 
-    it "does not ingest" do
-      asset = create(:asset, :inline_promoted_file, file: File.open(corrupt_tiff_path))
+    describe "inline promotion" do
+      it "does not ingest" do
+        asset = create(:asset, :inline_promoted_file, file: File.open(corrupt_tiff_path))
 
-      expect(asset.file_attacher.cached?).to be true
-      expect(asset.stored?).to be false
+        expect(asset.file_attacher.cached?).to be true
+        expect(asset.stored?).to be false
 
-      # and has validation errors
-      expect(asset.reload.file_metadata["ingest_validation_errors"]).to include(
-        "Missing required TIFF IFD0 tag 0x0111 StripOffsets",
-        "Missing required TIFF IFD0 tag 0x0116 RowsPerStrip",
-        "Missing required TIFF IFD0 tag 0x0117 StripByteCounts",
-        "Missing required TIFF IFD0 tag 0x0106 PhotometricInterpretation",
-        "Missing required TIFF IFD0 tag 0x0100 ImageWidth",
-        "Missing required TIFF IFD0 tag 0x0101 ImageHeight"
-      )
+        # and has validation errors
+        expect(asset.reload.file_metadata["ingest_validation_errors"]).to include(
+          "Missing required TIFF IFD0 tag 0x0111 StripOffsets",
+          "Missing required TIFF IFD0 tag 0x0116 RowsPerStrip",
+          "Missing required TIFF IFD0 tag 0x0117 StripByteCounts",
+          "Missing required TIFF IFD0 tag 0x0106 PhotometricInterpretation",
+          "Missing required TIFF IFD0 tag 0x0100 ImageWidth",
+          "Missing required TIFF IFD0 tag 0x0101 ImageHeight"
+        )
+      end
+    end
+
+    describe "async promotion", queue_adapter: :async do
+      include ActiveJob::TestHelper
+
+      it "does not ingest" do
+        asset = nil
+        perform_enqueued_jobs do
+          asset = create(:asset, file: File.open(corrupt_tiff_path))
+        end
+
+        asset.reload
+
+        expect(asset.reload.file_metadata["ingest_validation_errors"]).to be_present
+
+        expect(asset.file_attacher.cached?).to be true
+        expect(asset.stored?).to be false
+      end
     end
   end
 end
