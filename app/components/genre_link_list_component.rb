14389ad4798d0db0770dd9be42daa0c8ec696e9f
Jonathan Rochkind <jonathan@dnil.net>
2023-12-13 16:13:25 -0500

    extract oral_histories_collection_path helper

--- app/components/genre_link_list_component.rb
+++ app/components/genre_link_list_component.rb
@@ -2,7 +2,7 @@
 class GenreLinkListComponent < ApplicationComponent
   attr_reader :genre_list
 
-  delegate :search_on_facet_path, to: :helpers
+  delegate :search_on_facet_path, :oral_histories_collection_path, to: :helpers
 
   def initialize(genre_list)
     unless genre_list.is_a?(Array) && genre_list.all? {|e| e.is_a?(String)}
@@ -23,7 +23,7 @@ class GenreLinkListComponent < ApplicationComponent
   # Oral histories gets a special collection link, instead of to search results for genre
   def link_for_genre(genre_str)
     if genre_str == "Oral histories"
-      collection_path(ScihistDigicoll::Env.lookup!(:oral_history_collection_id))
+      oral_histories_collection_path
     else
       search_on_facet_path(:genre_facet, genre_str)
     end
