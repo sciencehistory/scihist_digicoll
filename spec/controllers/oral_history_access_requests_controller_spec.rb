b2c9005482d7791edca2b543c2d4237df8bba1e2
Jonathan Rochkind <jonathan@dnil.net>
2023-12-19 15:31:43 -0500

    controller spec for auth for OH request showing

---
+++ spec/controllers/oral_history_access_requests_controller_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe OralHistoryAccessRequestsController, type: :controller do
+  describe "#index" do
+    it "rejects if no auth" do
+      get :index
+      expect(response).to redirect_to(new_oral_history_session_path)
+      expect(flash[:auto_link_message]).to eq "You must be authorized to access this page."
+    end
+  end
+
+  describe "#show" do
+    let(:oh_request) { create(:oral_history_access_request) }
+
+    it "rejects if no auth" do
+      get :show, params: { id: oh_request.id }
+      expect(response).to redirect_to(new_oral_history_session_path)
+      expect(flash[:auto_link_message]).to eq "You must be authorized to access this page."
+    end
+  end
+end
+
