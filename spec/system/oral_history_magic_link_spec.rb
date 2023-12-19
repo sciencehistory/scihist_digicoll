22f1603d677ccf690f625062c144dc67af382673
Jonathan Rochkind <jonathan@dnil.net>
2023-12-19 14:58:44 -0500

    system spec includes OH request show page

--- spec/system/oral_history_magic_link_spec.rb
+++ spec/system/oral_history_magic_link_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 describe "Login with Oral Histories magic link", queue_adapter: :inline do
-  let(:requester_email) { Admin::OralHistoryRequesterEmail.create(email: "example@example.com") }
+  let(:requester_email) { Admin::OralHistoryRequesterEmail.create!(email: "example@example.com") }
+  let(:oral_history_work) { create(:oral_history_work, :available_by_request) }
+  let!(:approved_request) {
+    Admin::OralHistoryAccessRequest.create!(
+        oral_history_requester_email: requester_email,
+        patron_name: "Jo B",
+        work: oral_history_work,
+        delivery_status: "approved",
+        intended_use: "Just for fun"
+    )
+  }
 
   it "can request a link which works to log in" do
     visit new_oral_history_session_path
@@ -22,5 +32,19 @@ describe "Login with Oral Histories magic link", queue_adapter: :inline do
     # requests dashboard
     expect(page).to have_selector("h2", text: "Oral History Requests")
     expect(page).to have_text(requester_email.email)
+
+    # Move to individual request page
+    click_on oral_history_work.title
+    expect(page).to have_selector("h2", text: oral_history_work.title)
+    # basic sanity check, has a link for each asset
+    (by_request, not_by_request) = oral_history_work.members.partition { |member| member.kind_of?(Asset) && !member.published? && member.oh_available_by_request? }
+
+    by_request.each do |asset|
+      expect(page).to have_selector("a", text: /#{DownloadFilenameHelper.filename_base_for_asset(asset)}/)
+    end
+
+    not_by_request.each do |asset|
+      expect(page).not_to have_selector("a", text: /#{DownloadFilenameHelper.filename_base_for_asset(asset)}/)
+    end
   end
 end
