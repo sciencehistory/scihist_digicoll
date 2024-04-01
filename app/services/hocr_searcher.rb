94acaad5374e9c4737210a8de557cef32aab437a
Jonathan Rochkind <jonathan@dnil.net>
2024-04-01 15:24:00 -0400

    Refactor HocrSearcher for OpenSeadragon-based homegrown viewer

--- app/services/hocr_searcher.rb
+++ app/services/hocr_searcher.rb
@@ -1,11 +1,13 @@
+# Takes a query, finds matches with coordinates and context using HOCR files
+# stored in Assets. HOCR is from Tesseract 4.x or 5.x.
+#
 # TODO:
 #      * multi-word "or"
 #      * normalize query
 #      * search in DB first to identify matches, instead of fetching all into memory
+#      * HTML escaped text
 #
 class HocrSearcher
-  include Rails.application.routes.url_helpers
-
   attr_reader :work, :show_unpublished
 
   def initialize(work, show_unpublished: false, query:)
@@ -14,45 +16,49 @@ class HocrSearcher
     @query = query.downcase
   end
 
-  def matches
-    result = []
-    work.members.each do |member|
-      next unless member.hocr
+  # @param hocr_nokogiri [Nokogiri::XML::Document] representing an HOCR from Tesserat
+  # @return [Array<Nokogiri::XML::Element>] array of Nokogiri::XML::Element representing matching
+  #    Tesserct <span class='ocrx_word'>
+  #
+  # ocrx_word may be vendor-specific to tesseract, but looks like this in Tesseract 4 or 5:
+  #
+  #     <span class='ocrx_word' id='word_1_25' title='bbox 36 194 91 218; x_wconf 96'>The</span>
+  #
+  def matching_ocrx_words_for(hocr_nokogiri)
+    hocr_nokogiri.search('//*[@class="ocrx_word"]').select {|w| w.text().downcase.include? @query }
+  end
+
+  # @return [Array<Hash>], where each hash has a "text" key with html text in context, and
+  #   a "osd_rect" key which is a hash with left, top, height, and width in OpenSeadragon units
+  #   proportional to image width.
+  #
+  # example
+  #
+  #       [{
+  #         "text"=>"All {{{units}}} must be connected as above",
+  #         "osd_rect"=>{
+  #           "left"=>0.38815,
+  #           "top"=>0.18576,
+  #           "height"=>0.01193,
+  #           "width"=>0.04409
+  #         }
+  #       }]
+  #
+  def results_for_osd_viewer
+    work.members.collect do |member|
+      asset = member.leaf_representative
+
+      next unless asset.hocr
       parsed_hocr = Nokogiri::XML(member.hocr) { |config| config.strict }
-      next unless parsed_hocr.css(".ocr_page").length == 1
+      next unless parsed_hocr.css(".ocr_page").length >= 1
 
-      words_on_this_page = parsed_hocr.search('//*[@class="ocrx_word"]')
-
-      matches_on_this_page = words_on_this_page.
-        select {|w| w.text().downcase.include? @query }
-
-      matches_on_this_page.each do |match|
-        coordinates = extract_coordinates(match.attributes['title'].value)
-        extra_margin = 30
-        result << {
-              "text": extract_context(match),
-              "par": [
-                  {
-                      "l":coordinates[:left] - extra_margin,
-                      "t":coordinates[:top] - extra_margin,
-                      "r":coordinates[:right] + extra_margin,
-                      "b":coordinates[:bottom] + extra_margin,
-                      "boxes": [
-                          {
-                              "l":coordinates[:left] - extra_margin,
-                              "t":coordinates[:top] - extra_margin,
-                              "r":coordinates[:right] + extra_margin,
-                              "b":coordinates[:bottom]  + extra_margin,
-                          },
-                      ],
-                      "page_width": member.width,
-                      "page_height": member.height
-                  }
-              ]
-          }
+      matching_ocrx_words_for(parsed_hocr).collect do |ocrx_word|
+        {
+          "text" => extract_context(ocrx_word),
+          "osd_rect" => extract_osd_rect(ocrx_word: ocrx_word, asset: asset)
+        }
       end
-    end
-    result
+    end.flatten
   end
 
   def extract_context(match)
@@ -62,28 +68,50 @@ class HocrSearcher
     end.join(' ')
   end
 
-  def extract_coordinates(coords)
-    #https://en.wikipedia.org/wiki/HOCR#bbox
-    #"bbox 1299 1809 1402 1837; x_wconf 91"
-    if /bbox (\d*) (\d*) (\d*) (\d*)\; x_wconf (\d*)/ =~ coords
-      return {
-        left:    $1.to_i,
-        top:     $2.to_i,
-        right:   $3.to_i,
-        bottom:  $4.to_i,
-        x_wconf: $5.to_i
+  # Take Tesseract-provided ocrx_word span with bbox, and convert
+  # to coordinates that OpenSeadragon.Rect needs for an overlay.
+  #
+  # * Tessract hocr bbox is *pixels* in original image, x1 (left), y1 (top), x2 (right), y2 (bottom)
+  #
+  # * OSD uses units based on UNIT 1 is the entire original WIDTH of image. So units are a _portion_ of
+  #   original width. And then expressed as top, left, height, width.
+  #
+  # TODO: Later maybe a rotation too. Tesseract rotation is stored in `ocr_line` span parent.
+  #
+  # https://en.wikipedia.org/wiki/HOCR#bbox
+  #
+  # https://kba.github.io/hocr-spec/1.2/#bbox
+  #
+  # @param ocrx_word [Nokogiri::XML::Element] representing a <span class="ocrx_word"> from Tesseract
+  #
+  # @param asset [Asset]
+  #
+  # @return [Hash] with keys :left, :top, :height, :width
+  #
+  def extract_osd_rect(ocrx_word:, asset:)
+    # <span class='ocrx_word' id='word_1_4' title='bbox 1226 515 1327 546; x_wconf 95'>must</span>
+
+    round_digits = 5
+
+    if /bbox (\d+) (\d+) (\d+) (\d+)/ =~ ocrx_word['title']
+      x0 = $1.to_f
+      y0 = $2.to_f
+      x1 = $3.to_f
+      y1 = $4.to_f
+
+      unit_value = asset.width.to_f
+
+      {
+        'left' =>    (x0 / unit_value).round(round_digits),
+        'top'  =>    (y0 / unit_value).round(round_digits),
+        'height' =>  ((y1 - y0) / unit_value ).round(round_digits),
+        'width' =>   ((x1 - x0) / unit_value ).round(round_digits),
       }
+    else
+      raise ArgumentError("Could not parse bbox from expected ocrx_word: `#{ocrx_word.to_xml}`")
     end
   end
 
-  def as_hash
-    {
-        ia: @work.friendlier_id,
-        q: @query,
-        indexed: true,
-        matches: matches
-    }
-  end
 
   private
 
--- spec/services/hocr_searcher_spec.rb
+++ spec/services/hocr_searcher_spec.rb
@@ -1,13 +1,27 @@
 require "rails_helper"
 
 describe HocrSearcher do
-  let(:work) { create(:public_work, members: [ create(:asset, :with_ocr)])}
+  # set height and width to match the faked HOCR
+  let(:asset) { create(:asset_with_faked_file, :with_ocr, faked_width: 2767, faked_height: 3558)}
+  let(:work) { create(:public_work, members: [ asset ])}
 
   it "produces matches" do
     searcher = HocrSearcher.new(work, query: "units")
 
-    results = searcher.matches
+    results = searcher.results_for_osd_viewer
     expect(results).to be_kind_of(Array)
     expect(results.length).to be 1
+
+    result = results.first
+    expect(result).to be_kind_of(Hash)
+    expect(result['text']).to be_kind_of(String)
+    expect(result['osd_rect']).to be_kind_of(Hash)
+
+    # sanity check
+    %w{left top height width}.each do |key|
+      expect(result['osd_rect'][key]).to be_kind_of(Float)
+      expect(result['osd_rect'][key]).not_to eq(Float::INFINITY)
+      expect(result['osd_rect'][key]).to be < (asset.width * 4)
+    end
   end
 end
