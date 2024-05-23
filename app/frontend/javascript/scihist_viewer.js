6ae00499bd365fa4ff8f4b6bc1bcd3ba986cfc19
Jonathan Rochkind <jonathan@dnil.net>
2024-05-23 15:27:13 -0400

    viewer, record results on thumbs with screen-reader accessible label too

--- app/frontend/javascript/scihist_viewer.js
+++ app/frontend/javascript/scihist_viewer.js
@@ -568,7 +568,8 @@ ScihistImageViewer.prototype.makeThumbnails = function(json) {
         ' data-index="' + index + '"' +
       '>' +
         '<img class="lazyload"' +
-              ' alt="Image ' + (index + 1) +
+              ' alt="Image ' + (index + 1) + '"' +
+              ' data-base-alt="Image ' + (index + 1) + '"' +
               ' data-src="' + config.thumbSrc + '"' +
               ' data-srcset="' +  (config.thumbSrcset || '') + '"' +
               // not totally sure if this forced height is really necessary currently, maybe for lazyload?
@@ -840,7 +841,11 @@ ScihistImageViewer.prototype.getSearchResults = async function(query) {
     // Highlight thumbs in thumblist with result count. Add data attribute,
     // CSS will take care of rest.
     for (const [memberId, results] of Object.entries(this.searchResults.allResultsByPageId())) {
-      document.querySelector(".viewer-thumb[data-member-id='" + memberId + "']").setAttribute("data-search-result-count", results.length);
+      const elt = document.querySelector(".viewer-thumb[data-member-id='" + memberId + "']");
+      const imgElt = elt.querySelector("img");
+
+      elt.setAttribute("data-search-result-count", results.length);
+      imgElt.setAttribute("alt", imgElt.getAttribute("data-base-alt") + " (" + results.length + " results)");
     }
   } catch (error) {
     console.log("scihist_viewer, error fetching search results: " + error.message);
@@ -884,7 +889,12 @@ ScihistImageViewer.prototype.clearSearchResults = function() {
 
   document.getElementById("searchNav").style.display = "none";
 
-  $(".viewer-thumb[data-search-result-count]").removeAttr("data-search-result-count");
+  document.querySelectorAll(".viewer-thumb[data-search-result-count]").forEach(function(el) {
+    el.removeAttribute("data-search-result-count");
+
+    const imgElt = el.querySelector("img");
+    imgElt.setAttribute("alt", imgElt.getAttribute("data-base-alt"));
+  });
 
   this.viewer.clearOverlays();
   this.removeQueryInUrl();
