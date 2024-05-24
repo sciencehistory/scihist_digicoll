487a6e9d5f139bfe50f4b8ea113f027c273d80b8
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-05-24 13:57:06 -0400

    better docs on test

--- spec/models/work/asset_files_spec.rb
+++ spec/models/work/asset_files_spec.rb
@@ -5,15 +5,15 @@ describe "work with corrupt file attached" do
   let(:asset) {create(:asset, :inline_promoted_file, file: File.open(corrupt_tiff_path))}
   let(:parent_work) { create(:work, :with_complete_metadata, published: false, members: [asset]) }
   before do
-    allow(Rails.logger).to receive(:error)
     allow(Rails.logger).to receive(:warn)
   end
 
-  describe "publish the work" do
-    it "can't publish the work" do
+  describe "attempt to publish" do
+    it "refuses to publish" do
       expect(asset.promotion_failed?).to be true
       # Work should be valid as long as it's not published
       expect(parent_work.reload.valid?).to be true
+
       expect { parent_work.update!(published: true) }.to raise_error(ActiveRecord::RecordInvalid)
       expect(Rails.logger).to have_received(:warn).with(/.*couldn't be published. Something was wrong with asset*/)
     end
