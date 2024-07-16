080f8513f0ec78f09fb2725eefadab6af4f41b4a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-07-16 15:13:36 -0400

    if all items are checked, the "check all items" checkbox should be checked too

--- app/components/cart_control_component_multiple.rb
+++ app/components/cart_control_component_multiple.rb
@@ -1,10 +1,12 @@
 # Display a checkbox to add or remove all works on the page to the current user's cart.
 # See also cart_control.js and cart_items_controller.rb
 class CartControlComponentMultiple < ApplicationComponent
-  attr_reader :work_friendlier_ids
+  attr_reader :work_friendlier_ids, :start_checked
 
-  def initialize(work_friendlier_ids)
+  def initialize(work_friendlier_ids, start_checked: false, label_sr_only: false)
     @work_friendlier_ids = work_friendlier_ids
+    @start_checked = start_checked
+    @label_sr_only = label_sr_only
   end
 
   def call
@@ -14,9 +16,13 @@ class CartControlComponentMultiple < ApplicationComponent
       safe_join([
         check_box_tag("toggle",
           "1",
-          data: { "multiple-cart-toggle-input" => true }
+          @start_checked,
+          id: 'check-or-uncheck-all-works',
+          data: { "multiple-cart-toggle-input" => true },
+          class: "cart-multiple-checkbox"
          ),
-        " "
+        " ",
+        label_tag('check-or-uncheck-all-works', "Check or uncheck all works on this page", class: ("sr-only" if @label_sr_only))
       ])
     end
   end
