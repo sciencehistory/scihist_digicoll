c9745eedbf352701e1afb0e63bb6498cb669a81a
Jonathan Rochkind <jonathan@dnil.net>
2024-07-18 13:15:47 -0400

    stop including lazysizes.js in pages, we don't use it anymore

--- app/frontend/entrypoints/lazysizes.js
+++
@@ -1,7 +0,0 @@
-// A SEPARATE JS entrypoint file with JUST lazysizes. While may load our other JS
-// as 'deferred' or at bottom of page, lazysizes recommends loading early.
-//
-// https://github.com/aFarkas/lazysizes#include-early
-
-import 'lazysizes';
-lazySizes.init();
