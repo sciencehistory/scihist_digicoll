5c3d7c12b58e0c8542aba32e2778a28c7402cf0a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-07-18 10:44:38 -0400

    JS cleanup; fixing function names; more comments; using less jquery

--- app/frontend/javascript/cart_control.js
+++ app/frontend/javascript/cart_control.js
@@ -1,28 +1,12 @@
-// We have a checkbox for admins for adding/removing Works from the 'cart'. The checkbox is provided by
-// CartControlComponent
+// For each work in the admin work list, we display a checkbox that allows an admin user to add it or remove it
+// from the user's cart'. The checkbox is provided by CartControlComponent.
+// We also provide a convenience checkbox at the bottom of the page that does the same for ALL works on the page.
 //
-// This JS turns it into an auto-submit form, and handles errors, and updating the cart item count.
+// This JS turns the checkboxes into an auto-submit form, and handles errors, and updates the cart item count.
 //
 // Uses JQuery.
 
 jQuery( document ).ready(function( $ ) {
-
-  function form_disable(form) {
-    form.find(".cart-checkbox, label").attr("disabled", "disabled");
-  }
-
-  function form_enable(form) {
-    form.find(".cart-checkbox, label").removeAttr("disabled");
-  }
-
-  function multiple_form_disable(form) {
-    form.find(".cart-multiple-checkbox, label").attr("disabled", "disabled");
-  }
-
-  function multiple_form_enable(form) {
-    form.find(".cart-multiple-checkbox, label").removeAttr("disabled");
-  }
-
 
   // SINGLE work cart checkbox:
   $(document).on("change", ".cart-checkbox", function(event) {
@@ -35,39 +19,37 @@ jQuery( document ).ready(function( $ ) {
         type: form.attr('method').toUpperCase(),
         data: form.serialize(),
         beforeSend: function() {
-          form_disable(form);
+          formDisable(form);
         },
         error: function(xhr, ajaxOptions, error) {
           console.error("cart item toggle error\r\n" + error + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
           // Revert the checkbox change
           checkbox.prop("checked", !checkbox.prop("checked"));
           alert("Error in cart item modify");
-          form_enable(form)
+          formEnable(form);
         },
         success: function(data, status, xhr) {
           //if app isn't running at all, xhr annoyingly
           //reports success with status 0, so non-0 we consider success.
           if (xhr.status != 0) {
             // update cart count
-            $("span[data-role=cart-counter]").text(data["cart_count"]);
-            form_enable(form);
+            updateCount(data["cart_count"]);
+            formEnable(form);
           } else {
             console.error("cart item toggle error\r\n" + error + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
             // Revert the checkbox change
             checkbox.prop("checked", !checkbox.prop("checked"));
             alert("Error in cart item modify");
-            form_enable(form)
+            formEnable(form);
           }
         }
     });
   });
 
   // MULTIPLE work cart checkbox:
-
   $(document).on("change", ".cart-multiple-checkbox", function(event) {
     var checkbox = $(this);
     var form = checkbox.closest("form");
-
 
     $.ajax({
         url: form.attr('action'),
@@ -76,35 +58,65 @@ jQuery( document ).ready(function( $ ) {
         data: form.serialize(),
 
         beforeSend: function() {
-          multiple_form_disable(form);
+          // Disable the checkbox until server confirms the change went through.
+          multipleFormDisable();
         },
         
         error: function(xhr, ajaxOptions, error) {
           console.error("Multiple cart item toggle error\r\n" + error + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
           // Revert the checkbox change
           checkbox.prop("checked", !checkbox.prop("checked"));
-          alert("Error in cart item modify");
-          multiple_form_enable(form)
+          alert("Unable to add or remove multiple works from your cart.");
+          multipleFormEnable();
         },
 
         success: function(data, status, xhr) {
-          //Set all the checkboxes to the same status as this one:
+          //Set all the "cart" checkboxes to the same status as this one:
           $('.cart-checkbox').each(function() {$(this).prop("checked", checkbox.prop("checked"))})
-
-          //if app isn't running at all, xhr annoyingly
-          //reports errors with status 0, so non-0 we consider success.
           if (xhr.status != 0) {
-            // update cart count
-            $("span[data-role=cart-counter]").text(data["cart_count"]);
-            multiple_form_enable(form);
+            // SUCCESS
+            updateCount(data["cart_count"]);
+            multipleFormEnable();
           } else {
-            console.error("cart item toggle error\r\n" + error + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            // FAILURE
+            console.error("Error adding or removing multiple works from the user's cart. Details:\r\n" + error + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
             // Revert the checkbox change
             checkbox.prop("checked", !checkbox.prop("checked"));
-            alert("Error in cart all");
-            multiple_form_enable(form)
+            alert("Unable to add or remove multiple works from your cart.");
+            multipleFormEnable();
           }
         }
     });
   });
+
+
+  // Prevent user from clicking the checkboxes while we wait for the server:
+  function formDisable(form, multiple=false) {
+    form.find(".cart-checkbox, label").attr("disabled", "disabled");
+  }
+
+  function formEnable(form, multiple=false) {
+    form.find(".cart-checkbox, label").removeAttr("disabled");
+  }
+
+  function multipleFormDisable() {
+    document.querySelector('.cart-multiple-checkbox').setAttribute("disabled", "disabled");
+    var other_checkboxes = document.querySelectorAll('.cart-checkbox');
+    for (var i=0; i < other_checkboxes.length; i++) {
+        other_checkboxes[i].setAttribute("disabled", "disabled");
+    }
+  }
+
+  function multipleFormEnable() {
+    document.querySelector('.cart-multiple-checkbox').removeAttribute("disabled");
+    var other_checkboxes = document.querySelectorAll('.cart-checkbox');
+    for (var i=0; i < other_checkboxes.length; i++) {
+        other_checkboxes[i].removeAttribute("disabled");
+    }
+  }
+
+  function updateCount(number) {
+    $("span[data-role=cart-counter]").text(number);
+  }
+
 });
