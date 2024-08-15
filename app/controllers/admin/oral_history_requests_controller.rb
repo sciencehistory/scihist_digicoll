cc7f6a92cfc2ef436f9ef6bc30b84c162a175bae
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-08-15 14:44:53 -0400

    adding helper method for sending the email

--- app/controllers/admin/oral_history_requests_controller.rb
+++ app/controllers/admin/oral_history_requests_controller.rb
@@ -28,36 +28,19 @@ class Admin::OralHistoryRequestsController < AdminController
     @oral_history_request = OralHistoryRequest.find(params[:id])
   end
 
-  # accept or reject
+  # accept or reject or dismiss
   # POST /admin/oral_history_requests/:id/respond
   def respond
     @oral_history_request = OralHistoryRequest.find(params[:id])
-
     disposition = params[:disposition]
     custom_message = params.dig(:oral_history_request_approval, :notes_from_staff)
 
-    # Update the request object
-    delivery_status = {
-      'approve' => 'approved',
-      'reject'  => 'rejected',
-      'dismiss' => 'dismissed' }[disposition]
-
-    if delivery_status.nil?
-      raise ArgumentError, "Unrecognized disposition: #{disposition}"
-    else
-      @oral_history_request.update!(delivery_status: delivery_status, notes_from_staff: custom_message)  
-    end
-
-    email_method= {
-      'approve' => :approved_with_session_link_email,
-      'reject' =>  :rejected_with_session_link_email
-    }[disposition]
-
-    if email_method.present?
-      message = OralHistoryDeliveryMailer.with(request: @oral_history_request, custom_message: custom_message).send(email_method)
-      message.deliver_later
-    end
-
+    delivery_status = {'approve'=>'approved', 'reject'=>'rejected', 'dismiss'=>'dismissed'}[disposition]
+    raise ArgumentError, "Unrecognized disposition: #{disposition}" if delivery_status.nil?    
+    @oral_history_request.update!(delivery_status: delivery_status, notes_from_staff: custom_message)    
+    
+    maybe_enqueue_email(disposition, custom_message)
+    
     notice = if disposition == "approve"
       "Approve email was sent to #{@oral_history_request.requester_email} for '#{@oral_history_request.work.title}'"
     elsif disposition == "reject"
@@ -65,8 +48,22 @@ class Admin::OralHistoryRequestsController < AdminController
     elsif disposition == "dismiss"
        "#{@oral_history_request.requester_email}'s request for '#{@oral_history_request.work.title}' has been dismissed. The request has been set aside and no email will be sent to the requester."      
     end
+    
     redirect_to admin_oral_history_requests_path, notice: notice
   end
+
+  def maybe_enqueue_email(disposition, msg)
+    mailer_method= {
+      'approve' => :approved_with_session_link_email,
+      'reject' =>  :rejected_with_session_link_email
+    }[disposition]
+    return if mailer_method.nil?
+    OralHistoryDeliveryMailer.
+      with(request: @oral_history_request, custom_message: msg).
+      send(mailer_method).deliver_later
+  end
+  helper_method :maybe_enqueue_email
+
 
   def report
     scope = OralHistoryRequest
