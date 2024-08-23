aba44d0763d653569c1591aa41ce9a2c1916333f
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-08-23 10:18:44 -0400

    fix bug by replacing target with currentTarget

--- app/frontend/javascript/custom_google_analytics_4_events.js
+++ app/frontend/javascript/custom_google_analytics_4_events.js
@@ -19,24 +19,24 @@ $(document).on('click', '*[data-analytics-category]', function(e) {
 
     // If we find a selector in data-analytics-value-css, we can look up the current value of the text input 
     // using that selector, and then pass that string to GA as event_value.
-    var elementToLookUp =  this.getAttribute("data-analytics-value-css");
+    var elementToLookUp =  event.currentTarget.getAttribute("data-analytics-value-css");
     var eventValue = elementToLookUp ?  $(elementToLookUp)[0].value.replace(/[^a-zA-Z 0-9]+/g, '') : null;
 
     gtag( 'event',
 
       // A string describing what the user did,
       // e.g. "download" or "transcription_pdf" or "english_translation_pdf" or "download_original" or "search_inside"
-      this.getAttribute("data-analytics-action"),
+      event.currentTarget.getAttribute("data-analytics-action"),
 
       {
 
-        // This is always the string "work".
-        'event_category': e.target.getAttribute("data-analytics-category"),
+        // Always the string "work".
+        'event_category': event.currentTarget.getAttribute("data-analytics-category"),
 
-        // This is always the work's friendlier_id.
-        'event_label': this.getAttribute("data-analytics-label"),
+        // Always the work's friendlier_id.
+        'event_label': event.currentTarget.getAttribute("data-analytics-label"),
 
-        // This is only used to send a search phrase (or, more frequently, null).
+        // Only used to send a search phrase (or, more frequently, null).
         'event_value': eventValue
 
       }
