cf76fdd2461640cf9878a7e2b0308f2be4cdcb00
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-09-03 13:18:42 -0400

    report search_inside_book to Google Analytics

--- app/frontend/javascript/custom_google_analytics_4_events.js
+++ app/frontend/javascript/custom_google_analytics_4_events.js
@@ -8,21 +8,23 @@ $(document).on('click', '*[data-analytics-category]', function(e) {
   // Do not call `gtag` unless the function is defined
   // in app/views/layouts/_google_analytics_4.html.erb .
   // (This in turn is controlled by ScihistDigicoll::Env.lookup(:google_analytics_4_tag_id).
+
   if (typeof gtag === 'function') {
 
-    gtag( 'event',
+    var data_to_send = {
+      // Always the string "work".
+      'event_category': e.currentTarget.getAttribute("data-analytics-category"),
 
-      // param eventName
+      // Always the work's friendlier_id.
+      'event_label': e.currentTarget.getAttribute("data-analytics-label"),
+    }
+
+    gtag( 'event',
       // A string describing what the user did,
       // e.g. "download" or "transcription_pdf" or "english_translation_pdf" or "download_original"
       e.currentTarget.getAttribute("data-analytics-action"),
-      {
-        // As of early 2023, this is always the string "work".
-        'event_category': e.currentTarget.getAttribute("data-analytics-category"),
-        // As of early 2023, this is always the work's friendlier_id.
-        'event_label':    e.currentTarget.getAttribute("data-analytics-label"),
-      }
-
+      data_to_send
     );
   }
 });
+
