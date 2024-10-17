e3054e3e495fe7add66492ec948b78d9ea37a4ff
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-10-17 07:14:00 -0400

    cleaned up and better-tested auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,11 @@
 class AuthController < Devise::OmniauthCallbacksController
 
-
+  # This method signs a user in after they authenticate with Microsoft Azure.
   def azure_activedirectory_v2
     final_sign_in request.env['omniauth.auth']['info']['email']
   end
 
+  # Allows a developer to sign on without using OAuth.
   def dev_login
     if ScihistDigicoll::Env.staging? || ScihistDigicoll::Env.production?
       flash[:alert] = "Can't log you in this way."
@@ -12,27 +13,39 @@ class AuthController < Devise::OmniauthCallbacksController
       return
     end
     dev_login = ScihistDigicoll::Env.lookup(:dev_login)
-    if dev_login.nil?
-      flash[:alert] = "To log in this way, please make sure you have the ENV variable DEV_LOGIN set to your email address."
+    unless dev_login =~URI::MailTo::EMAIL_REGEXP
+      flash[:alert] = "Please set DEV_LOGIN to a valid email address."
       redirect_back(fallback_location: root_path)
       return
     end
     final_sign_in dev_login
   end
 
+  private
 
+  # We need to provide a default path for newly signed-in users.
+  # Usual login paths do not call this method, but when
+  # the SSO setup is misconfigured
+  # this method does sometimes get called, resulting in a 500 error.
+  # Instead, we send users to the root path.
   def new_session_path  *args
     flash[:notice] = "This URL is not meant for regular users."
     root_path
   end
 
-  private
-
+  # Look up a user. If they're not locked out, sign them in.
   def final_sign_in(email)
     @user = User.where('email ILIKE ?', "%#{ User.sanitize_sql_like(email) }%").first
     if @user&.persisted?
-      flash[:notice] = 'Signed in successfully.'
-      sign_in_and_redirect @user, event: :authentication
+      if @user.locked_out?
+        flash[:alert] = "Sorry, this user is not allowed to log in."
+        redirect_back(fallback_location: root_path)
+        return
+      else
+        flash[:notice] = 'Signed in successfully.'
+        sign_in_and_redirect @user, event: :authentication
+        return
+      end
     else
       flash[:alert] = "We couldn't find an account for you. Please contact an administrator."
       redirect_back(fallback_location: root_path)
--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
-
 RSpec.describe "Logins", type: :system do
-  let(:user) { FactoryBot.create(:admin_user, email: 'the_user@sciencehistory.org') }
+
+  # An authenticated email. This email address belongs to a person who has gotten authenticated.
+  let(:incoming_email) { 'the_user@sciencehistory.org' }
+
+  # This is the user that gets looked up in the DB:
+  let!(:user) { FactoryBot.create(:admin_user, email: 'the_user@sciencehistory.org') }
 
   before do
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:azure_activedirectory_v2] = OmniAuth::AuthHash.new({
       :provider => 'azure_activedirectory_v2',
-      :uid => '12345', :email => user.email,
-      :info => OmniAuth::AuthHash::InfoHash.new({ email: user.email })
+      :uid => '12345', :email => incoming_email,
+      :info => OmniAuth::AuthHash::InfoHash.new({ email: incoming_email })
     })
   end
-
   after do
     OmniAuth.config.test_mode = false
     OmniAuth.config.mock_auth[:azure_activedirectory_v2] = nil
   end
-
   context "dev" do
     context "admin user" do
       let(:work) { FactoryBot.create(:public_work, title: "Redirect to me")}
@@ -31,15 +33,13 @@ RSpec.describe "Logins", type: :system do
         click_on "Logout"
         expect(page).to have_text("Signed out successfully")
       end
-
       it "does not allow log in unless you have a login defined" do
         allow(ScihistDigicoll::Env).to receive(:lookup).and_call_original
         allow(ScihistDigicoll::Env).to receive(:lookup).with(:dev_login).and_return 'goat'
         visit root_path
         click_on "Dev log in"
-        expect(page).to have_text("couldn't find an account for you")
+        expect(page).to have_text("Please set DEV_LOGIN to a valid email address")
       end
-
       it "Can't log in unless you have a login defined" do
         allow(ScihistDigicoll::Env).to receive(:lookup).and_call_original
         allow(ScihistDigicoll::Env).to receive(:lookup).with(:dev_login).and_return nil
@@ -48,12 +48,10 @@ RSpec.describe "Logins", type: :system do
       end
     end
   end
-
-  context "staging and prod" do
+  context "staging or prod" do
     before do
       allow(ScihistDigicoll::Env).to receive(:staging?).and_return('true')
     end
-
     context "admin user" do
       let(:work) { FactoryBot.create(:public_work, title: "Redirect to me")}
       it "can login and log out" do
@@ -65,7 +63,6 @@ RSpec.describe "Logins", type: :system do
         click_on "Logout"
         expect(page).to have_text("Signed out successfully")
       end
-
       it "redirects after login to where you were before" do
         visit work_path(work)
         expect(page).to have_text("Redirect to me")
@@ -73,20 +70,26 @@ RSpec.describe "Logins", type: :system do
         expect(page).to have_text("Signed in successfully")
         expect(page).to have_text("Redirect to me")
       end
-
       it "doesn't allow dev login" do
         visit dev_login_path
         expect(page).to have_text("Can't log you in this way.")
       end
-
     end
-
-    context "locked out user" do
-      let(:user) { FactoryBot.create(:admin_user, email: 'the_user@sciencehistory.org', locked_out: true) }
+    context "SSO succeeds, but no account by that name in our DB" do
+      let(:incoming_email) { 'some_other_user@sciencehistory.org' }
       it "can't log in" do
         visit root_path
         click_on "Log in"
-        expect(page).to have_text("your account is disabled")
+        expect(page).to have_text("couldn't find an account")
+      end
+    end
+
+    context "locked out user" do
+      let!(:user) { FactoryBot.create(:admin_user, email: 'the_user@sciencehistory.org', locked_out: true) }
+      it "can't log in" do
+        visit root_path
+        click_on "Log in"
+        expect(page).to have_text("Sorry, this user is not allowed to log in.")
       end
     end
 
@@ -103,7 +106,6 @@ RSpec.describe "Logins", type: :system do
         expect(page).to have_text("LOG IN")
       end
     end
-
     context "global lock-out" do
       it "can't log in" do
         allow(ScihistDigicoll::Env).to receive(:lookup).and_call_original
