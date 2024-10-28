27a06b4426e913f248d012efae826fd0d23196fc
Eddie Rubeiz <erubeiz@sciencehistory.org>
2024-10-28 10:33:13 -0400

    removing commented-out method

--- app/components/scihist_footer_component.rb
+++ app/components/scihist_footer_component.rb
@@ -3,8 +3,4 @@
 # The standard SHI footer from main site
 class ScihistFooterComponent < ApplicationComponent
 
-  # def is_dev?
-# 	 ScihistDigicoll::Env.lookup(:service_level) == 'development'
-  # end
-  # helper_method is_dev?
 end
