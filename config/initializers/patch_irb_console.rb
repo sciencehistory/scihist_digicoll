6c6b6b9a74e4524ae13faada4feb4cda577d36a5
Jonathan Rochkind <jonathan@dnil.net>
2024-12-12 11:30:29 -0500

    Remove require that is no longer required in Rails 8 to patch IRB how we like
    It resulted in deprecation warning:
    
    ```
    DEPRECATION WARNING: `rails/console/methods` has been deprecated and will be removed in Rails 8.1.
    Please directly use IRB's extension API to add new commands or helpers to the console.
    For more details, please visit: https://github.com/ruby/irb/blob/master/EXTEND_IRB.md
    ```
    
    But we didn't need the require at all in Rails 8 to patch what we wanted to patch -- not sure if extension API would let us or be appropriate to patch what we want, but this seems to be working.

--- config/initializers/patch_irb_console.rb
+++ config/initializers/patch_irb_console.rb
@@ -1,6 +1,4 @@
- require "rails/console/methods"
- require "rails/commands/console/irb_console"
-
+require "rails/commands/console/irb_console"
 
 # Over-ride rails method that puts the env in console prompt,
 # so console prompt in staging says 'staging'.
@@ -8,7 +6,6 @@
 # even though our staging uses "production" env, we want it to
 # show up differently in console, so prompt can serve it's cautionary function to
 # distinguish production
-
 
 module OverrideIrbConsoleColorizedEnv
   def colorized_env
