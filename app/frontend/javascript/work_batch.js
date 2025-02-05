28fb73909172eab17753e8587c80f819a640993e
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-02-05 12:32:28 -0500

    get the friendlierID from the  URL

--- app/frontend/javascript/work_batch.js
+++ app/frontend/javascript/work_batch.js
@@ -2,30 +2,44 @@ let WorkBatch = (function () {
   let privateVariable = "I am private";
 
 
+  function getFriendlierId() {
+    var urlMatches = window.location.href.match(/\/works\/([^\/]*)/);
+    if (urlMatches === null) {
+      return;
+    }
+    return urlMatches[1];
+  }
+
+  function getMembers() {
+    var friendlierId = getFriendlierId();
+    if (friendlierId !== null) {
+      var url = "http://localhost:3000/works/" + friendlierId + "/work_batch";
+      getWorks(url);
+    }
+  }
+
   async function getWorks(url) {
     try {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
       const html = await response.text();
 
-      document.querySelector('div.show-member-list-items').innerHTML = html;
+      var tagToReplace = document.querySelector('div.show-member-list-items')
+      if (tagToReplace !== null) {
+        tagToReplace.innerHTML = html;
+      }
     } catch (error) {
       console.error('Error fetching or inserting HTML:', error);
     }
   }
 
-
-  function getMembers() {
-    getWorks("http://localhost:3000/works/qbtvy9p/work_batch");
-  }
-
+  // return for the class:
   return {
     getMembers: getMembers,
   };
 
 })();
 
-//WorkBatch.getMembers();
+WorkBatch.getMembers();
