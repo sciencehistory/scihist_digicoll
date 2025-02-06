5f1c5071df206c6c50bf17a0416ca0f7a1ce5821
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-02-06 15:54:01 -0500

    adding link to add one batch at a time

--- app/frontend/javascript/work_batch.js
+++ app/frontend/javascript/work_batch.js
@@ -1,18 +1,29 @@
 let WorkBatch = (function () {
   let privateVariable = "I am private";
 
+  document.querySelector(".work-batch-link")?.addEventListener("click", getMembers);
 
   function getFriendlierId() {
     var urlMatches = window.location.href.match(/\/works\/([^\/]*)/);
     return (urlMatches === null) ? null : urlMatches[1];
   }
 
-  function getMembers() {
+  function getPage() { 
+    return parseInt(document.querySelector('.last-loaded-page').innerHTML) || 0;
+  }
+
+  function incrementPage() {
+    document.querySelector('.last-loaded-page').innerHTML = (getPage() + 1);
+  }
+
+
+  function getMembers(event) {
     var friendlierId = getFriendlierId();
     if (friendlierId !== null) {
-      var url = "http://localhost:3000/works/" + friendlierId + "/work_batch";
+      var url = "http://localhost:3000/works/" + friendlierId + "/work_batch" + "?page=" + (getPage() + 1);
       getWorks(url);
     }
+    event.preventDefault();
   }
 
   async function getWorks(url) {
@@ -23,9 +34,10 @@ let WorkBatch = (function () {
       }
       const html = await response.text();
 
-      var tagToReplace = document.querySelector('div.show-member-list-items')
+      var tagToReplace = document.querySelector('div.show-member-list-items');
       if (tagToReplace !== null) {
-        tagToReplace.innerHTML = html;
+        tagToReplace.insertAdjacentHTML('beforeend', html);
+        incrementPage();
       }
     } catch (error) {
       console.error('Error fetching or inserting HTML:', error);
@@ -39,4 +51,3 @@ let WorkBatch = (function () {
 
 })();
 
-WorkBatch.getMembers();
