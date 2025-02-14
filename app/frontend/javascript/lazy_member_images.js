f3e64ded43176597f2488a102a1c69fbb304ed96
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-02-14 16:21:54 -0500

    hide the link if we see a no_more_images tag

--- app/frontend/javascript/lazy_member_images.js
+++ app/frontend/javascript/lazy_member_images.js
@@ -3,19 +3,17 @@ document.querySelector(".lazy-member-images-link")?.addEventListener("click", ge
 
 function getMoreMemberImages(event) {
   var friendlierId = getFriendlierId();
+  var startIndex = document.querySelector(".lazy-member-images-link").dataset.startIndex;
+  var imagesPerPage = document.querySelector(".lazy-member-images-link").dataset.imagesPerPage;
   if (friendlierId !== null) {
-    var url = "http://localhost:3000/works/" + friendlierId + "/lazy_member_images?" +
-      "start_index=" + document.querySelector(".lazy-member-images-link").dataset.startIndex +
-      '&images_per_page=' + document.querySelector(".lazy-member-images-link").dataset.imagesPerPage ;
+    var url = window.location.origin + "/works/" + friendlierId + "/lazy_member_images?" +
+      "start_index=" + startIndex + '&images_per_page=' + imagesPerPage;
     console.log("Getting url" + url);
     getWorks(url);
-
   }
-
   event.preventDefault();
 }
 
-// todo: get the entire url from the template
 function getFriendlierId() {
   var urlMatches = window.location.href.match(/\/works\/([^\/]*)/);
   return (urlMatches === null) ? null : urlMatches[1];
@@ -23,8 +21,8 @@ function getFriendlierId() {
 
 // todo: get the entire url from the template
 async function getWorks(url) {
-    var tagToReplace = document.querySelector('.member-divs');
-    if (tagToReplace === null) {
+    var tagForImages = document.querySelector('.member-divs');
+    if (tagForImages === null) {
       return;
     }
 
@@ -34,14 +32,16 @@ async function getWorks(url) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const html = await response.text();
-    tagToReplace.insertAdjacentHTML('beforeend', html);
+    tagForImages.insertAdjacentHTML('beforeend', html);
 
-
-    // update the link so it works for the next batch:
-    document.querySelector(".lazy-member-images-link").dataset.startIndex  = Array.from(document.querySelectorAll('.next-start-index')).pop().innerHTML
-
-    if (allMembersLoaded()) {
+    // if there are no more images to fetch
+    if (document.querySelector(".no-more-images") !== null) {
+      // hide the now-useless link
       document.querySelector(".lazy-member-images-link").style.display = "none";
+    }
+    else {
+      // update the link so it knows about the next batch:
+      document.querySelector(".lazy-member-images-link").dataset.startIndex  = parseInt(Array.from(document.querySelectorAll('.next-start-index')).pop().innerHTML);
     }
 
   } catch (error) {
