0f545206f2fb18084e0695bc51bfe8ba54e568ae
Jonathan Rochkind <jonathan@dnil.net>
2025-02-19 15:34:41 -0500

    fix to work for multiple links on page, with delegation

--- app/frontend/javascript/turnstile_protected_link.js
+++ app/frontend/javascript/turnstile_protected_link.js
@@ -11,50 +11,65 @@ import * as bootstrap from 'bootstrap';
 domready(function() {
   var loadedWidgetId;
 
-  document.querySelector("a[data-turnstile-protection=true],a[data-analytics-action=download_original]")?.addEventListener("click", function(event) {
+  document.addEventListener("click", function(event) {
+    const link = event.target.closest("a[data-turnstile-protection=true],a[data-analytics-action=download_original]");
+
+    if (!link) {
+      // not the droids we are looking for
+      return;
+    }
+
     // Hacky exempt if it's a PDF download, because we want google to crawl PDFs
     // We can tell only by href path sorry
-    if (event.currentTarget.getAttribute("href")?.includes('downloads/orig/pdf')) {
+    if (link.getAttribute("href")?.includes('downloads/orig/pdf')) {
       return;
     }
 
     event.preventDefault();
-    const origHref = event.currentTarget.getAttribute("href");
 
     addTurnstileJsIfNecessary().then(() => {
-      const modal = bootstrap.Modal.getOrCreateInstance(document.getElementById("turnstile-protected-link-modal"));
+      const modalDom = document.getElementById("turnstile-protected-link-modal");
+      const modal = bootstrap.Modal.getOrCreateInstance(modalDom);
+      var origHref = link.getAttribute("href");
+
       modal.show();
 
       const widgetWrapper = document.getElementById('turnstile-protected-link-widget');
       const siteKey = widgetWrapper.getAttribute("data-sitekey");
 
       if (loadedWidgetId) {
-        window.turnstile.reset(loadedWidgetId);
-      } else {
-        loadedWidgetId = window.turnstile.render(widgetWrapper, {
-          sitekey: siteKey,
-          callback: function(token) {
-            // We're going to redirect with token visible in URL, but since
-            // we plan to use this right now only for URL that's going to immediately
-            // redirect anyway, no problem.
-
-            var newUrl;
-            if (origHref.indexOf('?') === -1) {
-              newUrl = origHref + "?cf_turnstile_response=" + encodeURIComponent(token);
-            } else {
-              url += '&' + "cf_turnstile_response=" + encodeURIComponent(token);
-            }
-
-            // likely media that will download and not actualy change browser
-            window.location.assign(newUrl);
-            modal.hide();
-          }
-        });
+        // Remove turnstile so we can re-init with callback with correct values
+        // for current link.
+        window.turnstile.remove(loadedWidgetId);
       }
 
+      loadedWidgetId = window.turnstile.render(widgetWrapper, {
+        sitekey: siteKey,
+        callback: createTurnstileCallback(siteKey, modal, origHref)
+      });
     });
   });
 });
+
+// Return the closure that will do the right thing for current origHref!
+function createTurnstileCallback(siteKey, modal, origHref) {
+  return (token) => {
+    // We're going to redirect with token visible in URL, but since
+    // we plan to use this right now only for URL that's going to immediately
+    // redirect anyway, no problem.
+
+    var newUrl;
+    if (origHref.indexOf('?') === -1) {
+      newUrl = origHref + "?cf_turnstile_response=" + encodeURIComponent(token);
+    } else {
+      url += '&' + "cf_turnstile_response=" + encodeURIComponent(token);
+    }
+
+    // likely media that will download and not actualy change browser
+    modal.hide();
+    window.location.assign(newUrl);
+  }
+}
 
 
 function addTurnstileJsIfNecessary() {
