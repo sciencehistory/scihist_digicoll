6140d87568f6d26fa9c65b85f24406908198762a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-02-27 09:02:54 -0500

    adding an invisible span component

---
+++ app/components/invisible_span_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# A component that displays an invisible span designed to be read by Javascript code in the browser and hidden from all users.
+#   <%= InvisibleSpanComponent.new(klass:"no-more-images", contents: "No more images").display %>
+#   should render as: <span class="no-more-images" style="display:none">No more images!</span>
+#
+class InvisibleSpanComponent < ApplicationComponent
+  attr_accessor :klass, :contents
+
+  def initialize(klass:, contents:"")
+    @klass = klass
+    @contents = contents # also a reserved word apparently
+  end
+
+  def call
+    tag.span @contents, class: @klass, style: "display:none"
+  end
+end
