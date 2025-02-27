db81abe5a07315750f3ce2fa10796a9533309d56
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-02-27 16:38:23 -0500

    better testing for image lazy loading

---
+++ spec/components/work_image_show_component_lazy_load_images_spec.rb
@@ -0,0 +1,111 @@
+require 'rails_helper'
+
+describe WorkImageShowComponent, type: :component do
+  context "thumbnails" do
+    let(:asset1) {
+      create(:asset_with_faked_file, faked_derivatives: {}, position: 0)
+    }
+    let(:asset2) {
+      create(:asset_with_faked_file, faked_derivatives: {},  position: 1)
+    }
+    let(:asset3) {
+      create(:asset_with_faked_file, faked_derivatives: {},  position: 2)
+    }
+    let(:asset4) {
+      create(:asset_with_faked_file, faked_derivatives: {},  position: 3)
+    }
+    let(:asset5) {
+      create(:asset_with_faked_file, faked_derivatives: {},  position: 4)
+    }
+    let(:members) {[asset1, asset2, asset3, asset4, asset5]}
+    let(:work) {
+      create(:work, :published, members: members, representative: asset1)
+    }
+
+    let(:hero_friendlier_id) do 
+      hero = page.first(".show-hero .member-image-presentation")
+      hero.first("a.thumb")["data-member-id"]      
+    end
+
+    let(:images_per_page) do
+      page.first(".images-per-page", visible: :all).text.strip
+    end
+    
+    let(:other_thumbs_friendlier_id_list) do
+      page.all(".show-member-list-item .member-image-presentation a.thumb").map { |thumb| thumb["data-member-id"] }
+    end
+    context "published work" do
+      context "first image is representative" do
+        it "shows the representative at the top, two more images underneath" do
+          render_inline described_class.new(work, images_per_page: 3)
+          expect(images_per_page).to eq "3"
+          expect(hero_friendlier_id).to eq asset1.friendlier_id
+          thumbs = page.all(".show-member-list-item .member-image-presentation")
+          expect(thumbs.length).to be(2)
+          expect(other_thumbs_friendlier_id_list).to eq [ asset2.friendlier_id, asset3.friendlier_id ]
+          # JS code will fetch more more thumbnails on request, starting with the fourth thumbnail
+          # (next-start-index starts at zero)
+          expect(page.first(".next-start-index", visible: :all).text.strip).to eq "3"
+        end
+      end
+
+      context "non-first image is representative" do
+        let(:work) {
+          create(:work, :published, :with_complete_metadata, members: members, representative: asset2)
+        }
+        it "displays all three thumbnails in small thumb list" do
+          render_inline described_class.new(work, images_per_page: 3)
+          expect(hero_friendlier_id).to eq(asset2.friendlier_id)
+          expect(other_thumbs_friendlier_id_list).to eq [ asset1.friendlier_id, asset2.friendlier_id, asset3.friendlier_id ]
+          expect(page.first(".next-start-index", visible: :all).text.strip).to eq "3"
+        end
+      end
+    end
+
+    context "work with unpublished assets" do
+      let(:asset2) {
+        create(:asset_with_faked_file, published: false, faked_derivatives: {}, position: 1)
+      }
+      let(:asset3) {
+        create(:asset_with_faked_file, published: false, faked_derivatives: {}, position: 2)
+      }
+      let(:asset4) {
+        create(:asset_with_faked_file, published: false, faked_derivatives: {}, position: 3)
+      }
+
+      it "unpublished assets are counted towards the images-per-page total" do
+        render_inline described_class.new(work, images_per_page: 4)
+        expect(hero_friendlier_id).to eq(asset1.friendlier_id)
+        # Assets 2, 3, and 4 are hidden.
+        expect(other_thumbs_friendlier_id_list).to eq [ asset5.friendlier_id ]
+        # The next batch of assets fetched will start with the fifth asset:
+        expect(page.first(".next-start-index", visible: :all).text.strip).to eq "4"
+      end
+
+      it "does show unpublished assets to a logged-in user", logged_in_user: :admin do
+        render_inline described_class.new(work, images_per_page: 3)
+        expect(hero_friendlier_id).to eq(asset1.friendlier_id)
+        expect(other_thumbs_friendlier_id_list).to eq [ asset2.friendlier_id, asset3.friendlier_id ]
+        expect(page.first(".next-start-index", visible: :all).text.strip).to eq "3"
+      end
+    end
+
+    context "work with a child work" do
+      #replace asset2, in position 1, with a child work:
+      let(:asset2) { nil }
+      let(:child_work) { create(:work, :published, position: 1) }
+      let(:members) {[asset1, child_work, asset3, asset4, asset5]}
+      it "unpublished assets are counted towards the images-per-page total" do
+        render_inline described_class.new(work, images_per_page: 4)
+        expect(hero_friendlier_id).to eq(asset1.friendlier_id)
+        expect(other_thumbs_friendlier_id_list).to eq [
+          child_work.friendlier_id,
+          asset3.friendlier_id,
+          asset4.friendlier_id
+        ]
+        # The next batch of assets fetched will start with the fifth asset:
+        expect(page.first(".next-start-index", visible: :all).text.strip).to eq "4"
+      end
+    end
+  end
+end
