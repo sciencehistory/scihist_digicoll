422e52564a1bf614298168b5849cb5b9076342a1
Jonathan Rochkind <jonathan@dnil.net>
2025-03-06 17:23:28 -0500

    spruce up load more pages link to be more accurate and include remaining count

--- app/components/fetch_more_pages_link_component.rb
+++ app/components/fetch_more_pages_link_component.rb
@@ -2,11 +2,16 @@ class FetchMorePagesLinkComponent < ApplicationComponent
   def initialize(start_index:, images_per_page:, total_count:)
     @start_index = start_index
     @images_per_page = images_per_page
+    @total_count = total_count
   end
 
   def call
     link_to("#", class:"lazy-member-images-link show-member-list-item", data: { trigger: "lazy-member-images", start_index: @start_index, images_per_page: @images_per_page }) do
-      content_tag("span", "Fetch more works")
+      content_tag("span", "Load #{remaining_items_count} more #{'item'.pluralize(remaining_items_count)}")
     end
+  end
+
+  def remaining_items_count
+    @total_count - @start_index
   end
 end
