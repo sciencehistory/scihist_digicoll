36b15ece544fc03d9db15e698e6d05641b8c22c2
Jonathan Rochkind <jonathan@dnil.net>
2025-03-06 16:58:46 -0500

    make load-more-pages link styled better, and fit in as just another show-member-list-item in grid

--- app/components/fetch_more_pages_link_component.rb
+++ app/components/fetch_more_pages_link_component.rb
@@ -5,6 +5,8 @@ class FetchMorePagesLinkComponent < ApplicationComponent
   end
 
   def call
-    link_to("Fetch more works", "", class:"lazy-member-images-link", data: { trigger: "lazy-member-images", start_index: @start_index, images_per_page: @images_per_page })
+    link_to("#", class:"lazy-member-images-link show-member-list-item", data: { trigger: "lazy-member-images", start_index: @start_index, images_per_page: @images_per_page }) do
+      content_tag("span", "Fetch more works")
+    end
   end
 end
