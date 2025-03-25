0eb3b3744d30b1667addc9de75074f83713ede51
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-03-25 15:46:40 -0400

    use upsert

--- app/controllers/all_search_result_ids_controller.rb
+++ app/controllers/all_search_result_ids_controller.rb
@@ -34,26 +34,30 @@ class AllSearchResultIdsController < CatalogController
     search_service.search_results['response']['docs'].map { |doc| doc['id'] }
   end
 
+
+  # The "on conflict do nothing" part will only work with PostGres.
+  # You could also use a subquery like:
+  #         AND    id NOT IN
+  #         (
+  #           SELECT work_id
+  #           FROM   cart_items
+  #           WHERE  user_id = #{ user_id }
+  #         )
   def add_friendlier_ids_to_cart_sql(friendlier_ids)
     friendlier_id_list = friendlier_ids.map {|id| "'#{id}'"}.join(",")
     """
     INSERT INTO cart_items
-      (
-        user_id, work_id, created_at, updated_at
-      )
+    (
+      user_id, work_id, created_at, updated_at
+    )
     SELECT #{ user_id }, cart_work_friendlier_id, now(), now()
     FROM   (
       SELECT kithe_models.id AS cart_work_friendlier_id
       FROM   kithe_models
       WHERE  friendlier_id IN ( #{ friendlier_id_list } )
       AND    type = 'Work'
-      AND    id NOT IN
-        (
-          SELECT work_id
-          FROM   cart_items
-          WHERE  user_id = #{ user_id }
-        )
     ) AS foo
+    ON CONFLICT (user_id, work_id) DO NOTHING
     """
   end
 
