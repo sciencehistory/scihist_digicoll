d8d32be1800d6cffaab33a761819bc494fea81c8
Jonathan Rochkind <jonathan@dnil.net>
2025-04-14 14:09:12 -0400

    Don't allow sequencing without up to date combined audio

--- app/components/oral_history/admin/sequence_timestamps_component.rb
+++ app/components/oral_history/admin/sequence_timestamps_component.rb
@@ -8,6 +8,23 @@ module OralHistory
       def initialize(work:)
         @work = work
       end
+
+
+      # These methods copied from combined audio derivatives component, sorry not DRY
+      def combined_audio_fingerprint
+        return nil unless work.is_oral_history?
+        work.oral_history_content&.combined_audio_fingerprint
+      end
+
+      def current_required_fingerprint
+        @current_required_fingerprint ||= CombinedAudioDerivativeCreator.new(work).fingerprint
+      end
+
+      def has_good_combined_audio?
+        work&.oral_history_content&.combined_audio_m4a_data.present? &&
+        (current_required_fingerprint == combined_audio_fingerprint)
+      end
+
     end
   end
 end
