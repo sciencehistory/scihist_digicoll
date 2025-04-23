d95982fa86698303a346d9992a014f0d257a1aca
Jonathan Rochkind <jonathan@dnil.net>
2025-04-23 13:19:12 -0400

    fixup! add model BotChallengedRequests for logging them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_195338) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_154128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
