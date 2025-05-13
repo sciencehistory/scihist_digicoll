15181372029f7e18edc68fd4d46ffa45c610cdbc
Jonathan Rochkind <jonathan@dnil.net>
2025-05-13 15:35:30 -0400

    OpenaiAudioTranscribe#get_vtt

---
+++ app/services/openai_audio_transcribe.rb
@@ -0,0 +1,46 @@
+# Use OpenAI audio transcribe API with whisper to transcribe
+#
+class OpenaiAudioTranscribe
+  class Error < StandardError ; end
+
+
+
+
+  def get_transcription_for_asset(asset)
+    lofi_opus = FfmpegExtractLofiSpeechOgg.new.call(asset.file)
+
+    webvtt = get_vtt(lofi_opus)
+
+
+  ensure
+    lofi_opus.unlink if lofi_opus
+  end
+
+  # @param audio_file [File] pointing to an audio file, needs to be under 25 meg or
+  # you'll get an error from Whisper. May need to be an actual File with #path for
+  # the openai ruby client, even though it's not supposed to be required.
+  #
+  # @param lang_code [String] ISO-279 two-letter language code, optional, Whisper
+  #   can only take one, the 'primary' one of the audio. Without it,  whisper will guess.
+  def get_vtt(audio_file, lang_code: nil)
+      response = client.audio.transcribe(
+        parameters: {
+          model: "whisper-1",
+          #file: audio_file,
+          response_format: "vtt",
+          language: lang_code, # Optional
+        }
+      )
+  rescue Faraday::Error => e
+    raise Error.new("OpenAI API error: #{e.response[:status]}: #{e.response[:body]}")
+  end
+
+  def client
+    @client ||= OpenAI::Client.new(
+      access_token: ScihistDigicoll::Env.lookup("openai_api_key"),
+      # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
+      log_errors: Rails.env.development?
+    )
+  end
+
+end
---
+++ spec/services/openai_audio_transcribe_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe OpenaiAudioTranscribe do
+  let(:opus_path) { (Rails.root + "spec/test_support/audio/short.opus").to_s}
+  let(:service) { described_class.new }
+
+  describe "#get_vtt" do
+    it "submits API request" do
+      expect(service.client.audio).to receive(:transcribe) do |args|
+        expect(args.keys).to eq [:parameters]
+        parameters = args[:parameters]
+
+        expect(parameters.slice(:model, :response_format)).to eq({:model=>"whisper-1", :response_format=>"vtt"})
+
+        expect(parameters[:file]).to be_kind_of(File)
+        expect(parameters[:file].path).to eq opus_path
+      end.and_return(sample_webvtt)
+
+      file = File.open(opus_path)
+      output = service.get_vtt(file)
+
+      expect(output).to be_kind_of(String)
+      expect(output).to start_with("WEBVTT")
+    ensure
+      file.close if file
+    end
+
+    it "raises for error" do
+      expect(service.client.audio).to receive(:transcribe).and_raise(
+        Faraday::BadRequestError.new(status: 400, body: "{ fake: 'body' }")
+      )
+
+      file = File.open(opus_path)
+      expect {
+        output = service.get_vtt(file)
+      }.to raise_error(described_class::Error)
+    ensure
+      file.close if file
+    end
+  end
+
+  let(:sample_webvtt) do
+    <<~EOS
+    WEBVTT
+
+    00:00.000 --> 00:01.500
+    Hello, welcome to the podcast.
+
+    00:01.500 --> 00:03.000
+    Today we're going to talk about AI.
+
+    00:03.000 --> 00:05.000
+    Let's get started.
+    EOS
+  end
+
+end
