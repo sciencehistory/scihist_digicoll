4fe1798cb01f3ce9f221dd5ebf18096748dedeb5
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-06-11 10:47:05 -0400

    remove namespace; add key prefix

--- app/services/more_like_this_getter.rb
+++ app/services/more_like_this_getter.rb
@@ -28,7 +28,6 @@ class MoreLikeThisGetter
   TIMEOUT=1
   OPEN_TIMEOUT=1
   HOW_LONG_TO_CACHE = 7.days
-  CACHE_NAMESPACE = :scihist_digicoll_more_like_this
 
   # @param work [Work] Work
   # @param max_number_of_works: if specified,
@@ -36,8 +35,6 @@ class MoreLikeThisGetter
   def initialize(work, max_number_of_works: nil)
     @work = work
     @max_number_of_works = max_number_of_works
-    # Defaults to false.
-    @use_cache = ScihistDigicoll::Env.lookup(:cache_more_like_this)
   end
 
   # Returns an array of up to @max_number_of_works
@@ -45,6 +42,65 @@ class MoreLikeThisGetter
   def works
     return [] if @work&.friendlier_id.nil?
     friendlier_ids.map {|id| works_in_arbitrary_order[id] }.compact
+  end
+
+  # Returns the friendlier_ids of the similar works, most similar first.
+  # Can be cached.
+  def friendlier_ids
+    @friendlier_ids ||= use_cache? ? cached_friendlier_ids : uncached_friendlier_ids
+  end
+
+  # NOTE:
+  # helpful SOLR console debugging URL:
+  #    "#{ScihistDigicoll::Env.lookup!(:solr_url)}/mlt?wt=json&q=id:#{work_to_match.friendlier_id}" +
+  #    "&mlt.fl=more_like_this_keywords_tsimv&mlt.mintf=0&mlt.mindf=0&rows=7"
+  #
+  def more_like_this_doc_set
+    @more_like_this_doc_set ||= begin
+      solr_connection&.mlt(:params => mlt_params)&.dig("response", "docs") || []
+    rescue RSolr::Error::ConnectionRefused,
+      RSolr::Error::Http,
+      RSolr::Error::InvalidResponse,
+      RSolr::Error::Timeout,
+      RSolr::Error::InvalidJsonResponse,
+      RSolr::Error::InvalidRubyResponse => e
+      Rails.logger.error("Encountered #{e.class.name} while trying to fetch more-like-this works for work #{@work.friendlier_id}")
+      []
+    end
+  end
+
+  # see https://solr.apache.org/guide/solr/latest/query-guide/morelikethis.html
+  def mlt_params
+    @mlt_params ||= begin
+      parameters = {
+        "q"         => "id:#{@work.friendlier_id}",
+        "fq"        => "{!term f=published_bsi}true",
+        "mlt.fl"    => 'more_like_this_keywords_tsimv',
+      }
+      parameters["rows"] = @max_number_of_works unless @max_number_of_works.nil?
+      parameters
+    end
+  end
+
+  private
+
+  # Returns an RSolr::Client configured with our
+  # standard blacklight config, except it won't retry failed queries.
+  def solr_connection
+    # If we don't get a response from SOLR right away,
+    # we just want to show the page without the more_like_this content. 
+    #
+    # Note the existence of Scihist::BlacklightSolrRepository, which we are
+    # consciously not using as the solr repo in this method. Its only purpose
+    # is to provide two successive retries on failure, which we don't want here.
+    @solr_connection ||= begin
+      Blacklight::Solr::Repository.new(CatalogController.blacklight_config).connection.tap do |conn|
+        conn.connection.params = {
+          :timeout => TIMEOUT,
+          :open_timeout => OPEN_TIMEOUT
+        }
+      end
+    end
   end
 
   # Note that we check one last time here using fresh data from the DB
@@ -67,70 +123,23 @@ class MoreLikeThisGetter
     end
   end
 
-  # Returns an RSolr::Client configured with our
-  # standard blacklight config, except it won't retry failed queries.
-  def solr_connection
-    # If we don't get a response from SOLR right away,
-    # we just want to show the page without the more_like_this content. 
-    #
-    # Note the existence of Scihist::BlacklightSolrRepository, which we are
-    # consciously not using as the solr repo in this method. Its only purpose
-    # is to provide two successive retries on failure, which we don't want here.
-    @solr_connection ||= begin
-      Blacklight::Solr::Repository.new(CatalogController.blacklight_config).connection.tap do |conn|
-        conn.connection.params = {
-          :timeout => TIMEOUT,
-          :open_timeout => OPEN_TIMEOUT
-        }
-      end
-    end
+  # Defaults to false.
+  def use_cache?
+    @use_cache ||= ScihistDigicoll::Env.lookup(:cache_more_like_this)
   end
 
-  def more_like_this_doc_set
-    @more_like_this_doc_set ||= begin
-      solr_connection&.mlt(:params => mlt_params)&.dig("response", "docs") || []
-    rescue RSolr::Error::ConnectionRefused,
-      RSolr::Error::Http,
-      RSolr::Error::InvalidResponse,
-      RSolr::Error::Timeout,
-      RSolr::Error::InvalidJsonResponse,
-      RSolr::Error::InvalidRubyResponse => e
-      Rails.logger.error("Encountered #{e.class.name} while trying to fetch more-like-this works for work #{@work.friendlier_id}")
-      []
-    end
-  end
-
-  # Returns the friendlier_ids of the similar works, most similar first.
-  # Can be cached.
-  def friendlier_ids
-    @friendlier_ids ||= @use_cache ? cached_friendlier_ids : uncached_friendlier_ids
+  def cache_key
+    "more_like_this/#{@work.friendlier_id}"
   end
 
   # Caching these should save trips to our flaky solr provider.
   def cached_friendlier_ids
-    Rails.cache.fetch(@work.friendlier_id, expires_in: HOW_LONG_TO_CACHE, namespace: CACHE_NAMESPACE) do
-      uncached_friendlier_ids
-    end
+    Rails.cache.fetch(cache_key, expires_in: HOW_LONG_TO_CACHE) { uncached_friendlier_ids }
   end
 
   def uncached_friendlier_ids
     more_like_this_doc_set&.map { |d| d['id'] } || []
   end
-
-  # see https://solr.apache.org/guide/solr/latest/query-guide/morelikethis.html
-  def mlt_params
-    @mlt_params ||= begin
-      parameters = {
-        "q"         => "id:#{@work.friendlier_id}",
-        "fq"        => "{!term f=published_bsi}true",
-        "mlt.fl"    => 'more_like_this_keywords_tsimv',
-      }
-      parameters["rows"] = @max_number_of_works unless @max_number_of_works.nil?
-      parameters
-    end
-  end
-
-  private
 
   def solr_url
     ScihistDigicoll::Env.lookup!(:solr_url)
