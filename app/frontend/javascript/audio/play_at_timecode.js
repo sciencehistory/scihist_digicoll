c2a0a63cf10e247d45ffd40c97c2e9e44a7dcb26
Jonathan Rochkind <jonathan@dnil.net>
2025-06-25 10:08:36 -0400

    play_at_timecode.js no-ops if no player on page

--- app/frontend/javascript/audio/play_at_timecode.js
+++ app/frontend/javascript/audio/play_at_timecode.js
@@ -9,13 +9,14 @@
 // container `data-role="now-playing-container"`
 
 $(document).on("click", "*[data-ohms-timestamp-s]", function(event) {
-  event.preventDefault();
-
   var seconds = this.dataset.ohmsTimestampS;
 
   // OH audio player, or our video player
   var html5Media = $("*[data-role=now-playing-container] audio, .show-video video").get(0);
 
-  html5Media.currentTime = seconds;
-  html5Media.play();
+  if (seconds && html5Media) {
+    event.preventDefault();
+    html5Media.currentTime = seconds;
+    html5Media.play();
+  }
 });
