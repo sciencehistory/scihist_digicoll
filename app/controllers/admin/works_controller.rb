25584edcf9b3c6bd5c3ef2de30b0dc9519dad772
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-07-17 16:52:38 -0400

    simpler syntax

--- app/controllers/admin/works_controller.rb
+++ app/controllers/admin/works_controller.rb
@@ -718,7 +718,7 @@ class Admin::WorksController < AdminController
           {
             match_ilike: Work.sanitize_sql_like(params[:title_or_id]),
             match_uuid:  Work.type_for_attribute(:id).cast(params[:title_or_id]),
-            match_any_external_id: [{ "value" => params[:title_or_id] }].to_json
+            match_any_external_id: [{ value: params[:title_or_id] }].to_json
           }
         )
       end
