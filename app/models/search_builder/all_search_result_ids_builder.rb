70b98dcb3c42cdf545b7e755eda706c27b9b9786
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-09-12 12:43:38 -0400

    simplify /all_search_result_ids_builder.rb in hopes of speeding it up

--- app/models/search_builder/all_search_result_ids_builder.rb
+++ app/models/search_builder/all_search_result_ids_builder.rb
@@ -7,40 +7,20 @@ class SearchBuilder
       self.default_processor_chain += [:all_search_result_ids_processor]
     end
 
-    # Return only friendlier_ids, and return up to ten million of them.
-    # We take some pains to request as little as possible from SOLR other than the actual friendlier_ids.
+    # Return only primary keys, and return up to ten million of them.
+    # We take some pains to request as little as possible from SOLR other than the actual primary keys.
     def all_search_result_ids_processor(solr_parameters)
 
       # This method runs on all searches, but we exit early unless we actually want this.
       return unless scope.context[:all_search_result_ids] == 'true'
-
-      solr_parameters.except!(*unneeded_keys)
-
-      # remove all facet limit info as well:
-      facet_limit_keys = solr_parameters.keys.select  {|k| k.include? 'facet.limit'}
-      solr_parameters.except!(*facet_limit_keys)
   
-      solr_parameters.merge!({fl:"model_pk_ssi", rows: '10000000'})
-    end
+      solr_parameters.delete_if do |k, v|
+        k.start_with?('facet')   || k.end_with?("facet.limit") ||
+        k.start_with?("hl")      || 
+        k.start_with?("stats")  
+      end
 
-    def unneeded_keys
-      @unneeded_keys ||= [
-        # Keep the sort for now
-        # ['sort']
-
-        # don't need stats
-        ["stats", "stats.field"],
-
-        # don't need highlighting
-        [
-          "hl", "hl.method", "hl.fl", "hl.usePhraseHighlighter", "hl.snippets",
-          "hl.encoder", "hl.maxAnalyzedChars", "hl.bs.type",
-          "hl.fragsize", "hl.fragsizeIsMinimum"
-        ],
-
-        # Also think we don't need these:
-        ["facet", "facet.field", "facet.query"],
-      ].flatten!
+      solr_parameters.merge!({fl:"model_pk_ssi", rows: '10000000', hl: 'false'})
     end
   end
 end
