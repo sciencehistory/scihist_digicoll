ec766c6e5a86b2eba0cf48576594072fb319d53e
Eddie Rubeiz <eddierubeiz@users.noreply.github.com>
2025-09-29 16:49:21 -0400

    Revert "Add docValues to Solr id and model pk fields"

--- app/models/search_builder/all_search_result_ids_builder.rb
+++ app/models/search_builder/all_search_result_ids_builder.rb
@@ -13,16 +13,13 @@ class SearchBuilder
 
       # This method runs on all searches, but we exit early unless we actually want this.
       return unless scope.context[:all_search_result_ids] == 'true'
-
+  
       solr_parameters.delete_if do |k, v|
         k.start_with?('facet')   || k.end_with?("facet.limit") ||
-        k.start_with?("hl")      ||
-        k.start_with?("stats")
+        k.start_with?("hl")      || 
+        k.start_with?("stats")  
       end
 
-      # It's important for performance that ALL fields in `fl` (in this case just one)
-      # have `docValues=true` in solr schema, to avoid major stored field performance hit
-      # in this query that could have thousands of results.
       solr_parameters.merge!({fl:"model_pk_ssi", rows: '10000000', hl: 'false', facet: 'false'})
     end
   end
