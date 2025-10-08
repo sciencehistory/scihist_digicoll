94337b267c88a576058f06f8a3c885005e7e7407
Jonathan Rochkind <jonathan@dnil.net>
2025-10-08 10:07:56 -0400

    consistent spacing in spec for legibility

--- spec/services/more_like_this_getter_spec.rb
+++ spec/services/more_like_this_getter_spec.rb
@@ -38,36 +38,42 @@ describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapt
       expect(getter.works.all? {|w| w.published?}).to be true
       expect(getter.works.include? work_to_match).to be false
     end
-
   end
 
   context "caching" do
     let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+
     before do
       allow(Rails).to receive(:cache).and_return(memory_store)
       Rails.cache.clear
     end
+
     it "doesn't cache by default" do
       expect(Rails.cache.read(work_to_match_cache_key)).to eq nil
       getter.works
       expect(Rails.cache.read(work_to_match_cache_key)).to eq nil
     end
+
     context "setting turned on" do
       before do
         allow(ScihistDigicoll::Env).to receive(:lookup).with(:cache_more_like_this).and_return(true)
       end
+
       after do
         allow(ScihistDigicoll::Env).to receive(:lookup).and_call_original
       end
+
       it "writes to the cache" do
         expect(Rails.cache.read(work_to_match.friendlier_id)).to eq nil
         expect(getter.works.map {|w| w.friendlier_id}).to eq Rails.cache.read(work_to_match_cache_key)
       end
+
       it "reads from the cache" do
         expect(Rails.cache.read(work_to_match.friendlier_id)).to eq nil
         Rails.cache.write(work_to_match_cache_key, ['a', 'b', 'c'])
         expect(getter.friendlier_ids).to eq ['a', 'b', 'c']
       end
+
       context "a work was unpublished after being cached" do
         it "only returns public works, even if the cache contains private works" do
           Rails.cache.write(work_to_match_cache_key, five_private_works.map(&:friendlier_id))
@@ -76,15 +82,18 @@ describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapt
       end
     end
   end
+
   it "delivers works in the same order their ids arrived from solr" do
     ids_in_order_of_similarity = five_public_works.map(&:friendlier_id)
     allow(getter).to receive(:friendlier_ids).and_return ids_in_order_of_similarity
     expect(getter.works).to eq five_public_works
   end
+
   it "fails gracefully if the solr connection isn't available" do
     allow(getter).to receive(:solr_connection).and_return(nil)
     expect(getter.more_like_this_doc_set).to eq []
   end
+
   it "recovers from a solr error and logs the error" do
     expect(Rails.logger).to receive(:error).with(/RSolr::Error::Http .* #{work_to_match.friendlier_id}/)
     allow(getter).to receive(:solr_connection).and_raise(RSolr::Error::Http.new({},nil))
