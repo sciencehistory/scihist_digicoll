f780114daa277a42c143c1bc41d786b235b5dea9
Jonathan Rochkind <jonathan@dnil.net>
2025-10-08 10:19:33 -0400

    override max_number_of_works explicitly for clearer spec, instead of global objects

--- spec/services/more_like_this_getter_spec.rb
+++ spec/services/more_like_this_getter_spec.rb
@@ -5,32 +5,39 @@ require 'rails_helper'
 #    "&mlt.fl=more_like_this_keywords_tsimv&mlt.mintf=0&mlt.mindf=0&rows=7"
 #
 describe MoreLikeThisGetter,  solr: true, indexable_callbacks: true, queue_adapter: :inline do
-  let(:getter) {MoreLikeThisGetter.new(work_to_match)}
-  let(:getter_of_two_works) {MoreLikeThisGetter.new(work_to_match, max_number_of_works: 2)}
-  let(:work_to_match)   { create(:public_work, subject: "aaa", description: "aaa")  }
+  let(:shared_subject)  { "aaa" }
+  let(:shared_description) { "aaa" }
+
+  let(:max_number_of_works) { nil }
+  let(:getter) {MoreLikeThisGetter.new(work_to_match, max_number_of_works: max_number_of_works)}
+  let(:work_to_match)   { create(:public_work, subject: shared_subject, description: shared_description)  }
   let(:work_to_match_cache_key) {"scihist:more_like_this:#{work_to_match.friendlier_id}"}
 
   let(:five_public_works) { [
-      create(:public_work, subject: "aaa", description: "aaa"),
-      create(:public_work, subject: "aaa", description: "aaa"),
-      create(:public_work, subject: "aaa", description: "aaa"),
-      create(:public_work, subject: "aaa", description: "aaa"),
-      create(:public_work, subject: "aaa", description: "aaa"),
+      create(:public_work, subject: shared_subject, description: shared_description),
+      create(:public_work, subject: shared_subject, description: shared_description),
+      create(:public_work, subject: shared_subject, description: shared_description),
+      create(:public_work, subject: shared_subject, description: shared_description),
+      create(:public_work, subject: shared_subject, description: shared_description)
     ]
   }
   let(:five_private_works) { [
-      create(:private_work, subject: "aaa", description: "aaa"),
-      create(:private_work, subject: "aaa", description: "aaa"),
-      create(:private_work, subject: "aaa", description: "aaa"),
-      create(:private_work, subject: "aaa", description: "aaa"),
-      create(:private_work, subject: "aaa", description: "aaa"),
+      create(:private_work, subject: shared_subject, description: shared_description),
+      create(:private_work, subject: shared_subject, description: shared_description),
+      create(:private_work, subject: shared_subject, description: shared_description),
+      create(:private_work, subject: shared_subject, description: shared_description),
+      create(:private_work, subject: shared_subject, description: shared_description),
     ]
   }
   let! (:indexed_works) { [work_to_match] + five_public_works + five_private_works }
 
   context "calls to test solr" do
-    it "can limit the number of works returned" do
-      expect(getter_of_two_works.works).to eq five_public_works[0..1]
+    context "with limit" do
+      let(:max_number_of_works) { 2 }
+
+      it "can limit the number of works returned" do
+        expect(getter.works).to eq five_public_works[0..(max_number_of_works - 1)]
+      end
     end
 
     it "returns only public works" do
