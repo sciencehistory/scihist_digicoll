b164d5806aca99a2df08c760e98f938a491c3e17
Jonathan Rochkind <jonathan@dnil.net>
2025-11-20 09:20:36 -0500

    add a fake embedding value that can be used in tests and such

--- app/models/oral_history_chunk.rb
+++ app/models/oral_history_chunk.rb
@@ -1,5 +1,9 @@
 # Chunks of text for LLM RAG research
 class OralHistoryChunk < ApplicationRecord
+  # For testing or what have you, since `embedding` attribute is required.
+  # 3072 is size of OpenAI text-embedding-3-large we use
+  FAKE_EMBEDDING = [0.0] * 3072
+
   # from neighbors gem for pg_vector embeddings
   has_neighbors :embedding
 
