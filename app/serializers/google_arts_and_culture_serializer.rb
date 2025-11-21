4015645ddf5b814f3cdc18da6806c9719a7b7f28
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-11-21 11:24:26 -0500

    better method names. column is not the same as attribute.

--- app/serializers/google_arts_and_culture_serializer.rb
+++ app/serializers/google_arts_and_culture_serializer.rb
@@ -4,10 +4,10 @@ class GoogleArtsAndCultureSerializer
 
   def initialize(scope, columns: nil)
     @scope = scope
-    @column_keys = if columns.nil?
-      all_columns.keys
+    @attribute_keys = if columns.nil?
+      all_attributes.keys
     else
-      columns.select { |c| all_columns.keys.include? c }
+      columns.select { |c| all_attributes.keys.include? c }
     end
   end
 
@@ -26,49 +26,29 @@ class GoogleArtsAndCultureSerializer
     @scope.includes(:leaf_representative).find_each do |work|
       assets = GoogleArtsAndCultureZipCreator.members_to_include(work)
       data << work_row(work)
-      # assets.each do |asset|
-      #    data << asset_row(asset)
-      # end
+      assets.each do |asset|
+         data << asset_row(asset)
+      end
     end
     data
   end
 
-  # number of columns we need for each array column.
-  def column_counts
-    @column_counts ||= Hash[
-        array_columns.zip(
-        @scope.pluck(
-            *array_columns.map { |c| column_max_arel c }
-        ).first
-      )
-    ]
-  end
-
-  def column_max_arel(column_name)
-    Arel.sql("max(jsonb_array_length(kithe_models.json_attributes -> '#{column_name}'))" )
-  end
-
   def title_row
-    @column_keys.map do |k|
-      if array_columns.include? k.to_s
-        start_index = if (k == :additional_title)
-          1
-        else
-           0
-        end
+    @attribute_keys.map do |k|
+      if array_attributes.include? k.to_s
         (0..(column_counts[k.to_s] - 1)).map do |i|
-          "#{all_columns[k]}##{start_index + i}"
+          "#{ all_attributes[k ]}##{ i }"
          end
       else
-        all_columns[k]
+        all_attributes[k]
       end
     end.flatten
   end
 
   def work_row(work)
-    @column_keys.map do |k|
+    @attribute_keys.map do |k|
       scalar_or_array(
-        column_methods[k].call(work), column_counts.dig(k.to_s)
+        attribute_methods[k].call(work), column_counts.dig(k.to_s)
       )
     end.flatten
   end
@@ -89,7 +69,7 @@ class GoogleArtsAndCultureSerializer
       filespec:       filename,
       filetype:       asset_filetype(asset)
     }
-    @column_keys.map do |k|
+    @attribute_keys.map do |k|
       count = column_counts.dig(k.to_s)
       if count.nil?
         vals.fetch(k, not_applicable)
@@ -97,59 +77,69 @@ class GoogleArtsAndCultureSerializer
         Array.new(count, not_applicable)
       end
     end.flatten
+  end
 
+
+  # number of columns we need for each array attribute.
+  def column_counts
+    @column_counts ||= Hash[
+        array_attributes.zip(
+        @scope.pluck(
+            *array_attributes.map { |c| column_max_arel c }
+        ).first
+      )
+    ]
+  end
+
+  # sql to determine the maximum number of columns needed for a multiple-column attribute
+  def column_max_arel(attribute_name)
+    Arel.sql("max(jsonb_array_length(kithe_models.json_attributes -> '#{attribute_name}'))" )
   end
 
   # A hash of possible columns (and their titles)
   # you can use in the report.
   # By default, the report contains all these columns,
   # but you can pass `columns` to return fewer.
-  def all_columns
-    @all_columns ||= {
+  def all_attributes
+    @all_attributes ||= {
       friendlier_id:            'itemid',       # friendlier_id of works
       subitem_id:               'subitemid',    # friendlier_id of assets
       order_id:                 'orderid',      # order
 
 
-      title:                    'title#0',
-      additional_title:         'title',
+      title:                    'title',
+      additional_title:         'customtext:additional_title',
+
+      filespec:                 'filespec',
+      filetype:                 'filetype',
+      url_text:                 'relation:text',
+      url:                      'relation:url',
+
+      creator:                  'creator',
+      publisher:                'publisher',
 
 
+      min_date:                 'dateCreated:start',
+      max_date:                 'dateCreated:end',
+      date_of_work:             'dateCreated:display',
 
-      # filespec:                 'filespec',
-      # filetype:                 'filetype',
-      # url_text:                 'relation:text',
-      # url:                      'relation:url',
-
-      # creator:                  'creator',
-      # publisher:                'publisher',
-
-
-      # min_date:                 'dateCreated:start',
-      # max_date:                 'dateCreated:end',
-      # date_of_work:             'dateCreated:display',
-
-      # place:                    'locationCreated:placename',
-      
-
+      place:                    'locationCreated:placename',
       medium:                   'medium',
-      # # Gotcha: 'format' is actually used to store our 'extent' metadata in GAC.
+      
+      genre:                    'art=genre',
+      description:              'description',
+      subject:                    'subject',
+      rights_holder:            'rights',
+
+      # GAC's 'format' is used for our 'extent' metadata.
       extent:                   'format',
-      # genre:                    'art=genre',
-      # description:              'description',
-      # subject:                    'subject',
-      # rights_holder:            'rights',
-
-      # # PROBLEM CHILDREN:
-      # # GAC doesn't seem to have a field for what we call "format"
-      # # format:                   '???',
-
+      # Meanwhile, GAC doesn't seem to have a field for what we call "format"
+      # format:                   '???',
 
     }
   end
 
-
-  def array_columns
+  def array_attributes
     [
       'subject',
       'external_id',
@@ -165,7 +155,7 @@ class GoogleArtsAndCultureSerializer
 
 
   # Returns a hash.
-  # The keys of the hash are the same as @column_keys .
+  # The keys of the hash are the same as @attribute_keys .
   # The values of the hash are procs.
   #
   # Each proc
@@ -173,18 +163,18 @@ class GoogleArtsAndCultureSerializer
   #   returns the metadata we want.
   #
   # { :title => method(work), :additional_title => method(work), ... }
-  def column_methods
-    @column_methods ||= @column_keys.map do |column_label|
-      new_proc = if self.respond_to? column_label
+  def attribute_methods
+    @attribute_methods ||= @attribute_keys.map do |attribute_label|
+      new_proc = if self.respond_to? attribute_label
         # If k is defined in this class, use that (e.g. :created)
-        Proc.new { |some_work| self.send column_label, some_work }
-      elsif Work.method_defined? column_label
+        Proc.new { |some_work| self.send attribute_label, some_work }
+      elsif Work.method_defined? attribute_label
         # Or, if k is defined as a method on work, use that (e.g. :title)
-        Proc.new { |some_work| some_work.send column_label }
+        Proc.new { |some_work| some_work.send attribute_label }
       else
-        raise "Unknown column: #{column_label}"
+        raise "Unknown column: #{attribute_label}"
       end
-      [column_label, new_proc]
+      [attribute_label, new_proc]
     end.to_h
   end
 
