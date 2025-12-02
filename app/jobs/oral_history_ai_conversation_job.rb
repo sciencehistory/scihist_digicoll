74e0e768617ec8be9f0c7dc1fe77ae5117a0788f
Jonathan Rochkind <jonathan@dnil.net>
2025-12-02 13:57:55 -0500

    make job idempotent by having it no-op on already run job

--- app/jobs/oral_history_ai_conversation_job.rb
+++ app/jobs/oral_history_ai_conversation_job.rb
@@ -12,6 +12,14 @@ class OralHistoryAiConversationJob < ApplicationJob
       raise ArgumentError("must be an OralHistory::AiConversation not #{ai_conversation.class}")
     end
 
+    # Error we can re-run, otherwise it's not in runnable state.
+    # TODO: if it's stuck after some timeout, we might be willing to re-start?
+    # Or that might not be the right flow.
+    unless ai_conversation.queued? || ai_conversation.error?
+      Rails.info.log("#{self.class.name}: #{ai_conversation.class.name} #{ai_conversation.id}: Can't exec conversation in status #{ai_conversation.status}")
+      return
+    end
+
     ai_conversation.exec_and_record_interaction
   rescue StandardError => e
     # most errors should have been caught inside exec_and_record_interaction, but just in case
