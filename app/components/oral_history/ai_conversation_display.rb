c49c8dffcff6806689e3250c68e33cc485a82d32
Jonathan Rochkind <jonathan@dnil.net>
2025-12-02 17:41:10 -0500

    links from AI answer to OHMS oral histories

--- app/components/oral_history/ai_conversation_display.rb
+++ app/components/oral_history/ai_conversation_display.rb
@@ -46,7 +46,7 @@ module OralHistory
 
         <<~EOS
            <a href="##{footnote_item_data.anchor}"><span class="badge bg-primary rounded-pill">#{footnote_item_data.number}</span></a>
-          <a href="#" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="“#{footnote_item_data.quote}”">
+          <a target="_blank" href="#{work_path(footnote_item_data.work.friendlier_id)}" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="“#{footnote_item_data.quote}”">
             <span class="badge bg-secondary rounded-pill">#{footnote_item_data.short_citation_title} ~ #{footnote_item_data.nearest_timecode_formatted}</span>
           </a>
         EOS
