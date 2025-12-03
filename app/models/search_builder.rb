37b8d888b54a63bbf1ef261f43d48753e30f9f4c
Jonathan Rochkind <jonathan@dnil.net>
2025-12-03 15:28:27 -0500

    Revert "Refactor SearchBuilder search logic customizations for Blacklight 9.0.0.beta8"
    This reverts commit 0866ffaec685713276243373c5c7c87428a9c776.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 #
-# Blacklight 9 requires all custom search builder logic to be duplicated in a FacetSearchBuilder,
-# so please do all customization in the `SearchBuilderBehavior` module so it can be kept DRY.
-# See https://github.com/projectblacklight/blacklight/pull/3762
+# TODO: chf_sufia mixin's or equivalents for:
+# * SearchBuilder::RestrictAdminSearchFields => Makes sure admin notes are only searchable if logged in
+# * SearchBuilder::SyntheticCategoryLimit => something with making our 'topics'/synthetic categories
+#   work as limits, probably just for showing the main page for a 'topic'
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  include BlacklightRangeLimit::RangeLimitBuilder
 
-  include SearchBuilderBehavior
+
+  # Scihist SearchBuilder extensions
+  include SearchBuilder::AccessControlFilter
+  include SearchBuilder::AdminOnlySearchFields
+  include SearchBuilder::CustomSortLogic
+  include SearchBuilder::AllSearchResultIdsBuilder
+
+  ##
+  # @example Adding a new step to the processor chain
+  #   self.default_processor_chain += [:add_custom_data_to_query]
+  #
+  #   def add_custom_data_to_query(solr_parameters)
+  #     solr_parameters[:custom] = blacklight_params[:user_value]
+  #   end
 end
--- app/models/search_builder/within_featured_topic_builder.rb
+++ app/models/search_builder/within_featured_topic_builder.rb
@@ -4,8 +4,15 @@ class SearchBuilder
   # :slug needs to be provided in context.
   #
   # Used on FeaturedTopicController.
-  #
   class WithinFeaturedTopicBuilder < ::SearchBuilder
-    include WithinFeaturedTopicBuilderBehavior
+    extend ActiveSupport::Concern
+
+    self.default_processor_chain += [:within_featured_topic]
+
+    def within_featured_topic(solr_parameters)
+      featured_topic = FeaturedTopic.from_slug(blacklight_params[:slug])
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << featured_topic.solr_fq
+    end
   end
 end
