7a2d05aeac6ffdcf1d4c86daef79ae824d46e550
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-12-12 13:20:15 -0500

    simplify bucket code

--- app/jobs/upload_files_to_google_arts_and_culture_job.rb
+++ app/jobs/upload_files_to_google_arts_and_culture_job.rb
@@ -1,16 +1,14 @@
 
 class UploadFilesToGoogleArtsAndCultureJob  < ApplicationJob
-  def perform(work_ids:,  attribute_keys:, column_counts: )
+  def perform(work_ids:)
     downloaded_files = []
     @work_ids = work_ids
-    @attribute_keys = attribute_keys
-    @column_counts = column_counts
-
     bucket = google_arts_and_storage_bucket
+
     work_ids.each do |id|
       work = Work.find(id)
-      files = GoogleArtsAndCulture::WorkSerializer.new(work, attribute_keys: @attribute_keys, column_counts: column_counts).files.to_h
-      files.each do |filename, uploaded_file_obj|
+      file_hash = GoogleArtsAndCulture::WorkSerializer.file_hash(work)
+      file_hash.each do |filename, uploaded_file_obj|
         file_obj = uploaded_file_obj.download
         downloaded_files << file_obj
         begin
@@ -21,38 +19,37 @@ class UploadFilesToGoogleArtsAndCultureJob  < ApplicationJob
           puts "Done with #{filename}"
         rescue Google::Apis::ClientError, Google::Cloud::PermissionDeniedError => e
           puts "Unable to open the bucket: #{e.message}"
+        ensure
+          (downloaded_files || []).each do |tmp_file|
+            tmp_file.close
+            tmp_file.unlink
+          end
         end
       end
-    end
-  ensure
-    (downloaded_files || []).each do |tmp_file|
-      tmp_file.close
-      tmp_file.unlink
     end
   end
 
   def google_arts_and_storage_bucket
     google_arts_and_storage_bucket ||= begin
+
       credentials_json_string = ScihistDigicoll::Env.lookup(:google_arts_and_culture_credentials)
+      credentials_io =          StringIO.new(credentials_json_string)
       bucket_name =             ScihistDigicoll::Env.lookup(:google_arts_and_culture_bucket_name)
       project_id =              ScihistDigicoll::Env.lookup(:google_arts_and_culture_project_id)
+      auth_scope =              "https://www.googleapis.com/auth/devstorage.read_write"
 
       credentials = ::Google::Auth::ServiceAccountCredentials.make_creds(
-        json_key_io: StringIO.new(credentials_json_string),
-        scope: "https://www.googleapis.com/auth/devstorage.read_write"
+        json_key_io: credentials_io,
+        scope: auth_scope
       )
       storage = Google::Cloud::Storage.new( project_id: project_id, credentials: credentials)
 
       begin
         bucket = storage.bucket(bucket_name)
       rescue Google::Apis::ClientError, Google::Cloud::PermissionDeniedError => e
-        puts "Unable to open the bucket: #{e.message}"
+        puts "Unable to open bucket '#{bucket_name}' : #{e.message}"
       end
       
-      if bucket.nil?
-        puts "Error: Bucket '#{bucket_name}' not found or inaccessible."
-      end
-
       bucket
     end
   end
