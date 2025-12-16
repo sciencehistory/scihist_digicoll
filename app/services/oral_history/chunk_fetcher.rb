a657781073d2e02c80ac32bee08d8a9dd2a956a0
Jonathan Rochkind <jonathan@dnil.net>
2025-12-16 16:51:47 -0500

    add limit_per_document to ChunkFetcher

--- app/services/oral_history/chunk_fetcher.rb
+++ app/services/oral_history/chunk_fetcher.rb
@@ -5,21 +5,75 @@ module OralHistory
   # Can also use fancy SQL to limit to only so many per document, and add
   # other constraints.
   class ChunkFetcher
-    attr_reader :top_k, :question_embedding
+    attr_reader :top_k, :question_embedding, :per_document_limit, :oversample_factor
 
 
-    # @param k [Integer] how many chunks do you want back
-    def initialize(top_k:, question_embedding:)
-      @question_embedding = question_embedding
+    # @param top_k [Integer] how many chunks do you want back
+    #
+    # @param per_document_limit [Integer] if set, only include top per_document_limit
+    #    per oral history.
+    #
+    # @param oversample_factor [Integer] When doing per_document_limit, we need to kind of originally
+    #     fetch more than that, so we can then apply the per_document limit and still have enough.
+    #     It all happens inside a SQL subquery, but we can't actually rank _everything_.
+    def initialize(top_k:, question_embedding:, per_document_limit: nil, oversample_factor: 3)
       @top_k = top_k
+      @question_embedding = question_embedding
+      @per_document_limit = per_document_limit
+      @oversample_factor = oversample_factor
     end
 
     # @return [Array<OralHistoryChunk>]  Where each one also has a `neighbor_distance` attribute
     #     with cosine distance, added by neighbor gem. Set returned has strict_loading to ensure
     #     pre-loading to avoid n+1 fetch problem.
     def fetch_chunks
+      relation = if per_document_limit
+        wrap_for_per_document_limit(base_relation: base_relation, per_document_limit: per_document_limit, inner_limit: oversample_factor * top_k)
+      else
+        base_relation
+      end
+
+      relation.limit(top_k).strict_loading
+    end
+
+    # Without limit count, we'll add that later.
+    def base_relation
+      OralHistoryChunk.neighbors_for_embedding(question_embedding).includes(oral_history_content: :work)
       # Preload work, so we can get title or other metadata we might want.
-      OralHistoryChunk.neighbors_for_embedding(question_embedding).limit(top_k).includes(oral_history_content: :work).strict_loading
+      #OralHistoryChunk.neighbors_for_embedding(question_embedding).includes(oral_history_content: :work)
+    end
+
+    # We need to take base_scope and use it as a Postgres CTE (Common Table Expression)
+    # to select from, but adding on a ROW_NUMBER window function, that let's us limit
+    # to top per_document_limit!
+    #
+    # Kinda tricky. Got from google and talking to LLMs.
+    #
+    # @return [ActiveRecord::Relation] that's been wrapped with a CTE to enforce per_document limits.
+    def wrap_for_per_document_limit(base_relation:, per_document_limit:, inner_limit:)
+      base_relation = base_relation.dup # cause we're gonna mutate it, avoid confusion.
+
+      # add a 'select' using semi-private select_values API
+      # Not sure what neighbor's type erialization is doing we couldn't get right ourselves, but it works.
+      base_relation.select_values += [
+        ActiveRecord::Base.sanitize_sql([
+          "ROW_NUMBER() OVER (PARTITION BY oral_history_content_id ORDER BY oral_history_chunks.embedding <=> ?) as doc_rank",
+          Neighbor::Type::Vector.new.serialize(question_embedding)
+        ])
+      ]
+
+      # In the inner CTE, have to fetch oversampled, so we can wind up with
+      # hopefully enough in outer. Leaving inner unlimited would be peformance,
+      # cause of how indexing works it doesn't need to calculate them all.
+      base_relation.limit(inner_limit)
+
+      # copy the order from inner scope, where neighbor gem set it to be vector distance asc
+      # We leave the real limit for the caller to set
+      OralHistoryChunk.with(ranked_chunks: base_relation).
+        select("*").
+        from("ranked_chunks").
+        where("doc_rank <= ?", per_document_limit).
+        order("neighbor_distance")
     end
   end
 end
--- spec/services/oral_history/chunk_fetcher_spec.rb
+++ spec/services/oral_history/chunk_fetcher_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
+require 'matrix'
 
 describe OralHistory::ChunkFetcher do
+  def cosine_similarity(a, b)
+    a = Vector[*a]
+    b = Vector[*b]
+    dot = a.inner_product(b)
+    dot / (a.norm * b.norm)
+  end
+
+
   # These are all arbitrary, we just took some random sample vectors, having them
   # not be all zero gives us more realistic data, give us a prefix, we'll pad
   # with zeroes to correct length.
@@ -11,19 +20,33 @@ describe OralHistory::ChunkFetcher do
   let(:fake_question_embedding) { fake_vector(0.03263719,-0.021255592,-0.018256947,0.012259656,0.008308401)}
 
 
-  let(:work) { create(:oral_history_work) }
+  let(:work1) { create(:oral_history_work) }
+
 
   let!(:chunk1) { create(:oral_history_chunk,
+                  oral_history_content: work1.oral_history_content,
                   embedding: fake_vector(0.01759516,-0.0453438, -0.029577527, -0.032289326, 0.012045433),
-                  oral_history_content: work.oral_history_content, speakers: ["SMITH"])}
+                  speakers: ["SMITH"])}
 
   let!(:chunk2) { create(:oral_history_chunk,
+                        oral_history_content: work1.oral_history_content,
                         embedding: fake_vector(0.059072047,-0.021131188,-0.013840758,-0.0077753244,-0.02725617),
-                        oral_history_content: work.oral_history_content, speakers: ["SMITH", "JONES"], text: "Chunk 2")}
+                        speakers: ["SMITH", "JONES"], text: "Chunk 2")}
+
+
+  let(:work2) { create(:oral_history_work) }
+
 
   let!(:chunk3) { create(:oral_history_chunk,
+                        oral_history_content: work2.oral_history_content,
                         embedding: fake_vector(0.015151533,-0.01646033,-0.021422518,-0.024602171,0.009659404),
-                        oral_history_content: work.oral_history_content, speakers: ["SMITH", "JONES"], text: "Chunk 3")}
+                        speakers: ["SMITH", "JONES"], text: "Chunk 3")}
+
+  let!(:chunk4) { create(:oral_history_chunk,
+                        oral_history_content: work2.oral_history_content,
+                        embedding: fake_vector(0.013049184,-0.019433592,-0.024848722,-0.010990473,0.024592385),
+                        speakers: ["SMITH", "JONES"], text: "Chunk 3")}
+
 
   it "fetches" do
     results = described_class.new(question_embedding: fake_question_embedding, top_k: 2).fetch_chunks
@@ -35,4 +58,27 @@ describe OralHistory::ChunkFetcher do
     # make sure we can follow associations without triggering strict loading error
     results.collect(&:oral_history_content).flatten.collect(&:work)
   end
+
+
+  it "fetches with per_document_limit" do
+    # We ask for 3, but can only get 2 because of per-doc limit
+    results = described_class.new(question_embedding: fake_question_embedding, top_k: 3, per_document_limit: 1).fetch_chunks
+
+    expect(results.length).to eq 2
+
+    # Two oral_history_content_id's, each with only 1 chunk
+    groups = results.group_by {|c| c.oral_history_content_id }
+    expect(groups.count).to eq 2
+    expect(groups.values).to all satisfy { |v| v.length== 1}
+
+    # included chunks have closer vector distance to question than excluded
+    excluded = [chunk1, chunk2, chunk3, chunk4].find_all { |c| ! c.id.in?(results.collect(&:id)) }
+    excluded_similarity = excluded.collect {|c| cosine_similarity(c.embedding, fake_question_embedding) }
+
+    included_similarity = results.collect {|c| cosine_similarity(c.embedding, fake_question_embedding) }
+
+    # everything included has more similarity than anything excluded!
+    expect(included_similarity.min).to be >= excluded_similarity.max
+  end
+
 end
