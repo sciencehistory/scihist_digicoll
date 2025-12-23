ce588a66568ed270c012f1494518282b242600d4
Eddie Rubeiz <erubeiz@sciencehistory.org>
2025-12-23 15:23:41 -0500

    actually creates a spreadsheet and fills it out

--- app/services/google_arts_and_culture/spreadsheet_creator.rb
+++ app/services/google_arts_and_culture/spreadsheet_creator.rb
@@ -3,32 +3,45 @@ require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'fileutils'
 require 'csv'
-
-# can I do it without this???
-#require 'google/apis/sheets_v4'
+require 'google/apis/sheets_v4'
 
 
 module GoogleArtsAndCulture
   class SpreadsheetCreator
 
-    APPLICATION_NAME = 'Google Arts and Culture' #is this arbitrary?
+    APPLICATION_NAME = 'Science History Institute Digital Collections'
     OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+    CREDENTIALS_PATH = Dir.home
+
+    #csv_data is a Ruby array of arrays
+    def create_google_sheet(csv_data)
+      if csv_data.empty?
+        puts "Empty metadata."
+        return
+      end
+      spreadsheet = blank_spreadsheet
+      authorized_service.update_spreadsheet_value(
+        spreadsheet.spreadsheet_id,
+        'Sheet1!A1:Z' + csv_data.length.to_s,
+        Google::Apis::SheetsV4::ValueRange.new(values: csv_data),
+        value_input_option: 'USER_ENTERED'
+      )
+      puts "Spreadsheet URL: #{spreadsheet.spreadsheet_url}"
+    end
+
+    private
 
     def authorize
       scope =  ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-
-
       FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
 
-      credentials_json_string = ScihistDigicoll::Env.lookup(:google_arts_and_culture_credentials)
-      credentials_io =          StringIO.new(credentials_json_string)
-      client_id = Google::Auth::ClientId.from_file(credentials_io)
+      credentials_json_string = ScihistDigicoll::Env.lookup(:test_google_project_credentials)
+      user_id =                 ScihistDigicoll::Env.lookup(:test_google_project_user_id)
 
-      # create a token scope
+      client_id = Google::Auth::ClientId.from_hash(JSON.parse(credentials_json_string))
       token_store = Google::Auth::Stores::FileTokenStore.new(file: File.join(Dir.home, '.credentials', "sheets.googleapis.com-ruby-csv.yaml"))
       authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
-
-      user_id = 'default'
       credentials = authorizer.get_credentials(user_id)
 
       if credentials.nil?
@@ -37,7 +50,7 @@ module GoogleArtsAndCulture
         puts url
         print 'Enter the authorization code: '
         code = gets.chomp
-        credentials = authorizer.get_and_store_credentials_from_code(user_id, code, base_url: OOB_URI)
+        credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id, code: code, base_url: OOB_URI)
       end
       credentials
     end
@@ -54,32 +67,10 @@ module GoogleArtsAndCulture
 
 
     def blank_spreadsheet
-      spreadsheet_properties = Google::Apis::SheetsV4::SpreadsheetProperties.new(title: 'Metadata')
+      spreadsheet_properties = Google::Apis::SheetsV4::SpreadsheetProperties.new(title: "Metadata #{DateTime.now.strftime("%Y-%m-%d %H:%M:%S")}")
       spreadsheet = Google::Apis::SheetsV4::Spreadsheet.new(properties: spreadsheet_properties)
       authorized_service.create_spreadsheet(spreadsheet, fields: 'spreadsheetId,spreadsheetUrl')
     end
 
-
-    #csv_data is a Ruby array of arrays
-    def update_spreadsheet (csv_data)
-      if csv_data.empty?
-        puts "Empty metadata."
-        return
-      end
-
-      spreadsheet_id = blank_spreadsheet.spreadsheet_id
-      puts "Created new spreadsheet with ID: #{spreadsheet_id}"
-      puts "URL: #{created_spreadsheet.spreadsheet_url}"
-
-      range = 'Sheet1!A1:Z' + csv_data.length.to_s
-      value_range = Google::Apis::SheetsV4::ValueRange.new(values: csv_data)
-
-      authorized_service.update_spreadsheet_value(
-        spreadsheet_id,
-        range,
-        value_range,
-        value_input_option: 'USER_ENTERED'
-      )
-    end
   end
 end
