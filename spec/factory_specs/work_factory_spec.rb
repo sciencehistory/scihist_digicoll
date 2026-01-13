037dbf79438b6a69151602d9e043efc51532d55a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-01-13 16:48:17 -0500

    test for new trait

--- spec/factory_specs/work_factory_spec.rb
+++ spec/factory_specs/work_factory_spec.rb
@@ -84,5 +84,38 @@ describe "work factory" do
         expect(work.representative.file_data['metadata']['mime_type']).to eq("video/mpeg")
       end
     end
+
+    describe "published video work with a poster frame asset" do
+      let(:work) { build(:video_work, :published, :with_poster_frame) }
+
+      it "is valid" do
+        expect(work).to be_valid
+      end
+
+      it "is published" do
+        expect(work).to be_published
+      end
+
+      it "has video representative" do
+        expect(work.representative).to be_present
+        expect(work.representative.content_type).to eq "image/jpeg"
+      end
+
+      it "has thumbnail derivative in video representative" do
+        expect(work.representative.file_derivatives.keys).to include(:thumb_large, :thumb_standard, :thumb_mini)
+      end
+
+      it "has the correct format" do
+        expect(work.format).to eq(["moving_image"])
+      end
+
+      it "has an attached video with the proper mime type" do
+        expect(work.representative.file_data['storage']).to eq("store")
+        expect(work.representative.file_data['metadata']['mime_type']).to eq("image/jpeg")
+      end
+    end
+
   end
+
+
 end
