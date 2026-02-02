ff76ad9e34e92ae214896637088938fb18cb7330
Jonathan Rochkind <jonathan@dnil.net>
2026-02-02 17:25:10 -0500

    Fix specs for refactored AI Conversation with findings list instead of narrative

--- spec/factory_specs/oral_history_ai_conversation_factory_spec.rb
+++ spec/factory_specs/oral_history_ai_conversation_factory_spec.rb
@@ -7,19 +7,21 @@ describe "oral history AI conversation factory" do
     it "has footnotes with consistent data with chunk(s)" do
       expect(ai_conversation.answer_json).to be_present
 
-      expect(ai_conversation.answer_json["narrative"]).to be_present
-      expect(ai_conversation.answer_json["footnotes"]).to be_present
+      expect(ai_conversation.answer_json["introduction"]).to be_present
+      expect(ai_conversation.answer_json["findings"]).to be_present
 
-      ai_conversation.answer_json["footnotes"].each_with_index do |footnote_json, index|
-        expect(footnote_json["number"]).to eq index+1
-        expect(footnote_json["quote"]).to be_present
+      ai_conversation.answer_json["findings"].each_with_index do |finding_json, index|
+        expect(finding_json["answer"]).to be_present
+        expect(finding_json["citations"]).to be_present
 
-        expect(footnote_json["chunk_id"]).to be_present
-        chunk_referenced = OralHistoryChunk.where(id: footnote_json["chunk_id"]).first
-        expect(chunk_referenced).to be_present
+        finding_json["citations"].each do |citation_json|
+          expect(citation_json["chunk_id"]).to be_present
+          chunk_referenced = OralHistoryChunk.where(id: citation_json["chunk_id"]).first
+          expect(chunk_referenced).to be_present
 
-        expect(footnote_json["paragraph_start"].to_i).to be_between(chunk_referenced.start_paragraph_number, chunk_referenced.end_paragraph_number)
-        expect(footnote_json["paragraph_end"].to_i).to be_between(chunk_referenced.start_paragraph_number, chunk_referenced.end_paragraph_number)
+          expect(citation_json["paragraph_start"].to_i).to be_between(chunk_referenced.start_paragraph_number, chunk_referenced.end_paragraph_number)
+          expect(citation_json["paragraph_end"].to_i).to be_between(chunk_referenced.start_paragraph_number, chunk_referenced.end_paragraph_number)
+        end
       end
     end
   end
