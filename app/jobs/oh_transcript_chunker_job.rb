b47cb1b4e8a9a1809915ae00a16a5646b038de2f
Jonathan Rochkind <jonathan@dnil.net>
2026-02-25 21:00:54 -0500

    reset association just in case

--- app/jobs/oh_transcript_chunker_job.rb
+++ app/jobs/oh_transcript_chunker_job.rb
@@ -8,6 +8,7 @@ class OhTranscriptChunkerJob < ApplicationJob
     if oral_history_content.oral_history_chunks.exists?
       if delete_existing
         oral_history_content.oral_history_chunks.delete_all
+        oral_history_content.oral_history_chunks.reset # make sure we don't have cached old association
       else
         raise RuntimeError.new("Can't create chunks when chunks already exist! It would create a mess. Or use delete_existing:true to auto-delete. For OralHistoryContent #{oral_history_content.id}")
       end
