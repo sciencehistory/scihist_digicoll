b93e065970c27d8e0206ba7e84f95bb16a607a10
Jonathan Rochkind <jonathan@dnil.net>
2026-03-31 15:33:17 -0400

    OHMS VTT transcript gets paragraph anchors for paragraph number linking

--- app/components/oral_history/vtt_transcript_component.rb
+++ app/components/oral_history/vtt_transcript_component.rb
@@ -63,9 +63,16 @@ module OralHistory
                                       #
         cue.paragraphs.each do |paragraph|
           paragraph_safe_html = render_footnote_tags(paragraph.scrubbed_ohms_vtt_html)
+          paragraph_speaker_name = (paragraph.speaker_name || paragraph.assumed_speaker_name)
 
-          yield start_sec_f, (paragraph.speaker_name if paragraph.speaker_name != last_speaker), paragraph_safe_html
-          last_speaker = paragraph.speaker_name
+          yield(
+            start_seconds: start_sec_f.to_i,
+            speaker_name: (paragraph_speaker_name if (paragraph_speaker_name != last_speaker)),
+            html_text: paragraph_safe_html,
+            fragment_id: paragraph.fragment_id
+          )
+
+          last_speaker = paragraph_speaker_name
           start_sec_f = nil
         end
       end
