47871d5b283e23b437c4d60cab0b664847662234
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-04-08 14:48:44 -0400

    removing publication date filtering; we trust our rights metadata according to Gabriela

--- app/controllers/admin/google_arts_and_culture_downloads_controller.rb
+++ app/controllers/admin/google_arts_and_culture_downloads_controller.rb
@@ -38,29 +38,19 @@ class Admin::GoogleArtsAndCultureDownloadsController < AdminController
   end
 
   def eligible_scope
-
-    start_date_older_than_95_years_ago = """ exists ( select 1
-      from jsonb_array_elements(kithe_models.json_attributes -> 'date_of_work') as d(elem)
-        where (d.elem ->> 'start') ~ '^\\d{4}'
-        and (LEFT(d.elem ->> 'start', 4))::int <= extract(year from current_date)::int - 95
-        and (LEFT(d.elem ->> 'start', 4))::int >= 1450
-    )"""
-
     @eligible_scope ||= begin
 
-      scope_1 = Work.where(published: true).
+      museum_scope = Work.where(published: true).
       where("json_attributes -> 'department' ?| array[:depts  ]", depts:   ['Museum'] ).
       where("json_attributes -> 'format'     ?| array[:formats]", formats: ['physical_object'] ).
       where("json_attributes -> 'rights'     ?| array[:rights ]", rights:  ['https://creativecommons.org/licenses/by/4.0/'] )
 
-      scope_2 = Work.where(published: true).
+      library_scope = Work.where(published: true).
       where("json_attributes -> 'department' ?| array[:depts  ]", depts:   ['Library'] ).
       where("json_attributes -> 'format'     ?| array[:formats]", formats: ['image'] ).
       where("json_attributes -> 'rights'     ?| array[:rights ]", rights:  ['http://creativecommons.org/publicdomain/mark/1.0/'] ).
-      where(start_date_older_than_95_years_ago)
 
-      scope_1.or(scope_2)
-
+      museum_scope.or(library_scope)
     end
   end
   helper_method :eligible_scope
