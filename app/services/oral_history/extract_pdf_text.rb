db1e15e313bb6bba1e9ecdfc2e47f409fc4011b9
Jonathan Rochkind <jonathan@dnil.net>
2026-04-08 11:23:53 -0400

    extract_pdf_text validates with json schema

--- app/services/oral_history/extract_pdf_text.rb
+++ app/services/oral_history/extract_pdf_text.rb
@@ -8,6 +8,8 @@ module OralHistory
   #    paragraphs out of it (ignoring prefatory material and post-transcript
   #    material)
   class ExtractPdfText
+    class Error < StandardError ; end
+
     class_attribute :extract_pdf_text_command,
       default: ScihistDigicoll::Util.prefix_python_exec_command("./python_script/extract_pdf_text.py")
 
@@ -19,11 +21,99 @@ module OralHistory
 
     # use our extract_pdf_text.py python helper based on PyMyPDF to
     # get a json-compat hash representation that is pretty structural.
-    def extract_pdf_text
-      out, err = TTY::Command.new(printer: :null).run(extract_pdf_text_command, pdf_file_path)
+    #
+    # Will validate against our JSON schema and raise if invalid!
+    def extract_pdf_text(validate: true)
+      out, err = extract_pdf_text_tty_command.run(extract_pdf_text_command, pdf_file_path)
 
-      return JSON.parse(out)
+      parsed = JSON.parse(out)
+      if validate
+        validate_extract_pdf_text_json(parsed)
+      end
+
+      parsed
     end
 
+    private
+
+    # mostly to make it easy to mock in tests
+    def extract_pdf_text_tty_command
+      @extract_pdf_text_tty_command ||= TTY::Command.new(printer: :null)
+    end
+
+    def validate_extract_pdf_text_json(as_json)
+      errors = JSON_SCHEMER.validate(as_json).to_a
+
+      if errors.present?
+        error_msg = errors.collect { |h| h["error"] }.join(", ")
+        raise Error.new("response is not valid for expected schema: #{error_msg}}")
+      end
+    end
+
+    public
+
+    JSON_SCHEMER = JSONSchemer.schema(
+      {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "type": "object",
+        "required": ["pages"],
+        "properties": {
+          "pages": {
+            "type": "array",
+            "items": { "$ref": "#/$defs/page" }
+          }
+        },
+
+        "$defs": {
+          "page": {
+            "type": "object",
+            "required": ["width", "height", "blocks"],
+            "properties": {
+              "width": { "type": "number" },
+              "height": { "type": "number" },
+              "blocks": {
+                "type": "array",
+                "items": { "$ref": "#/$defs/block" }
+              }
+            }
+          },
+
+          "block": {
+            "type": "object",
+            "description": "Layout block heuristically identified by PyMuPDF",
+            "required": ["bbox", "paragraphs"],
+            "properties": {
+              "bbox": { "$ref": "#/$defs/bbox" },
+              "paragraphs": {
+                "type": "array",
+                "items": { "$ref": "#/$defs/paragraph" }
+              }
+            }
+          },
+
+          "paragraph": {
+            "type": "object",
+            "description": "Paragraph created heuristically by extract_pdf_text.py from lines identified by PyMyPDF",
+            "required": ["bbox", "text"],
+            "properties": {
+              "bbox": { "$ref": "#/$defs/bbox" },
+              "text": { "type": "string" }
+            }
+          },
+
+          "bbox": {
+            "type": "object",
+            "description": "Bounding box provided by PyMuPDF, rectangle location on page in PDF 72dpi pixels",
+            "required": ["x0", "y0", "x1", "y1"],
+            "properties": {
+              "x0": { "type": "number" },
+              "y0": { "type": "number" },
+              "x1": { "type": "number" },
+              "y1": { "type": "number" }
+            }
+          }
+        }
+      }
+    )
   end
 end
--- spec/services/oral_history/extract_pdf_text_spec.rb
+++ spec/services/oral_history/extract_pdf_text_spec.rb
@@ -10,6 +10,27 @@ describe OralHistory::ExtractPdfText do
       expect(as_json).to be_kind_of(Hash)
       expect(as_json).to be_present
     end
+
+    describe "schema-invalid JSON from python tool" do
+      let(:extracter) do
+        described_class.new(pdf_file_path: old_oh_sample_pages_pdf_path).tap do |obj|
+          fake_cmd = instance_double(TTY::Command)
+          allow(fake_cmd).to receive(:run).and_return([
+            { "pages": [
+                "bad_key": "I don't even know"
+              ]
+            }.to_json,
+            ""
+          ])
+
+          allow(obj).to receive(:extract_pdf_text_tty_command).and_return(fake_cmd)
+        end
+      end
+
+      it "raises error" do
+        expect { extracter.extract_pdf_text }.to raise_error(OralHistory::ExtractPdfText::Error)
+      end
+    end
   end
 
 end
