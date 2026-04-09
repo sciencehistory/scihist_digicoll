0569e3f4c696d49c0e98cb652bc3cefa85c199cd
Jonathan Rochkind <jonathan@dnil.net>
2026-04-08 20:47:30 -0400

    first pass at ExtractedPdfTextParagraphSplitter

---
+++ app/services/oral_history/extracted_pdf_text_paragraph_splitter.rb
@@ -0,0 +1,154 @@
+module OralHistory
+  # take the structural extracted_pdf_text hash from ExtractPdfText, and try to get
+  # just the actual interview transcript itself (no intro or suffix material), parsed
+  # into our OralHistory::Paragraph objects.
+  #
+  # We do NOT currently parse notes out, they are a lot harder (esp over the years)
+  #
+  # Record any identified page numbers and timecodes in Paragraph, and join paragraphs
+  # across page boundaries.
+  #
+  class ExtractedPdfTextParagraphSplitter
+    class Error < StandardError; end
+
+    # any page beginning with one of these is deemed post-transcript material. Normalized
+    # downcase.
+    POST_TRANSCRIPT_HEADINGS = ['index', 'bibliography', 'publication list', 'notes']
+
+    # Speaker names are VARIOUS in these historical things, we can't do too much more
+    # than so many chars before a colon. Some are not all caps. Some have multiple words.
+    # Some have hyphens!
+    SPEAKER_NAME_RE = /\A([\p{Word}\- '`\.]{1,40}): */
+
+    attr_reader :extracted_pdf_text
+
+    def initialize(extracted_pdf_text:, validate: false)
+      unless extracted_pdf_text.kind_of?(Hash) && extracted_pdf_text["pages"].kind_of?(Array)
+        raise ArgumentError.new("extracted_pdf_text: needs to be a hash matching ExtractPdfText::JSON_SCHEMER")
+      end
+
+      # We don't normally do cause it's already validated, but you can
+      if validate
+        ExtractPdfText.validate_extract_pdf_text_json(extracted_pdf_text)
+      end
+
+      @extracted_pdf_text = extracted_pdf_text
+    end
+
+    def paragraphs
+      @paragraphs ||= create_paragraphs(extracted_pdf_text["pages"])
+    end
+
+    # @return [OralHistory::Paragraph]
+    def create_paragraphs(extracted_pdf_text_pages)
+      paragraphs = []
+
+      first_index = find_page_1_index(extracted_pdf_text_pages)
+      unless first_index
+        raise Error.new("Could not find page 1 index")
+      end
+
+      # just default to last page though if we can't find one
+      last_index = find_last_transcript_page_index(extracted_pdf_text_pages) || extracted_pdf_text_pages.count - 1
+
+      (first_index..last_index).each do |page_index|
+        page_json = extracted_pdf_text_pages[page_index]
+
+        # dup em so we can modify the list to remove page numbers paragraphs. We don't
+        # care about blocks for processing at the moment.
+        page_paragraphs = page_json["blocks"].collect {|h| h["paragraphs"]}.flatten
+
+        next if page_paragraphs.empty?
+
+        logical_page_number = nil
+        if page_paragraphs.last["text"].strip =~ /\A(\d+)\Z/
+          logical_page_number = $1.to_i
+
+          # remove that last paragraph from array, it's a page number
+          page_paragraphs.pop
+        end
+
+        paragraphs.concat(page_paragraphs.collect do |paragraph_json|
+          json_to_paragraph(paragraph_json, logical_page_number: logical_page_number)
+        end)
+      end
+
+      # add in 1-based paragraph indexes
+      paragraphs.each_with_index { |p, i| p.paragraph_index = i + 1 }
+
+      # Make sure everyone has an assumed speaker if they didn't have a speaker,
+      # just keep it going from previous paragraph.
+      paragraphs.each_cons(2) do |p1, p2|
+        if p2.speaker_name.nil?
+          p2.assumed_speaker_name = p1.speaker_name || p1.assumed_speaker_name
+        end
+      end
+
+      return paragraphs
+    end
+
+    # Block with only one paragraph, whose whole text is a integer page number -- we don't worry
+    # about roman numerals for now, cause all of those are prefatory material we don't care about
+    # at the moment.
+    #
+    # @return [False,Integer] if it's a page number block, Integer page number extracted, otherwise false.
+    def block_is_page_number(json_block)
+      return false unless json_block
+
+      if json_block["paragraphs"].count == 1 && (json_block["paragraphs"].first["text"] =~ /\A(\d+)\Z/)
+        return $1.to_i
+      else
+        return false
+      end
+    end
+
+    # index in array of page with numeral "1" as page number
+    def find_page_1_index(pages)
+      pages.find_index do |page_json|
+        block_is_page_number(page_json["blocks"]&.last).to_s == "1"
+      end
+    end
+
+    # Index in array of LAST page of interview transcript itself, before
+    # any post-transcript info, which we recognize from headings.
+    #
+    # @return [Integer,nil] nil if it can't find
+    def find_last_transcript_page_index(pages, start_at_index:0)
+      found_index = pages.each_with_index.find_index do |page_json, index|
+        index >= start_at_index &&
+        page_json["blocks"].first&.dig("paragraphs")&.first&.dig("text")&.downcase&.strip&.in?(POST_TRANSCRIPT_HEADINGS)
+      end
+
+      found_index && found_index - 1
+    end
+
+    def json_to_paragraph(paragraph_json, logical_page_number:)
+      new_style_timecode_re = /\A[[:space:]]*\[(\d+\:\d+:\d+)\]/
+      old_style_timecode_re = /<T: (\d+) min>/
+
+      text = paragraph_json["text"]
+
+      # look for new style timecode as prefix
+      if text.sub!(new_style_timecode_re, '')
+        timestamp = OhmsHelper.parse_ohms_timestamp($1)
+      elsif text =~ old_style_timecode_re
+        # look for timecode in old style <T: \d min> thing, leave
+        # in text in case we want to mark exact timecode location later
+        timestamp = $1.to_i * 60
+      end
+
+      # Do we have a speaker name? Remove it from text but record it as speaker name.
+      if text.sub!(SPEAKER_NAME_RE, '')
+        speaker_name = $1.upcase
+      end
+
+      OralHistoryContent::Paragraph.new(
+        speaker_name: speaker_name,
+        text: text,
+        paragraph_index: nil, # we'll add them in later
+        pdf_logical_page_number: logical_page_number,
+        included_timestamps: [timestamp].compact.presence
+      )
+    end
+  end
+end
---
+++ spec/services/oral_history/extracted_pdf_text_paragraph_splitter_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe OralHistory::ExtractedPdfTextParagraphSplitter do
+  let(:oh_pdf_path) { Rails.root + "spec/test_support/pdf/oh/old_sample_pages_rice_b2l9q.pdf"}
+  let(:extracted_pdf_text) { OralHistory::ExtractPdfText.new(pdf_file_path: oh_pdf_path).extract_pdf_text }
+
+  let(:splitter) { described_class.new(extracted_pdf_text: extracted_pdf_text) }
+
+  it "extracts good paragraphs" do
+    paragraphs = splitter.paragraphs
+
+    expect(paragraphs).to all(be_kind_of(OralHistoryContent::Paragraph))
+    expect(paragraphs).to all(have_attributes(
+      paragraph_index: be_present, pdf_logical_page_number: be_present, text: be_present)
+    )
+    # this one does not have timestamps
+    expect(paragraphs).to all(have_attributes(included_timestamps: be_blank))
+
+    # all page numbers are increasing
+    expect(paragraphs.each_cons(2)).to all(satisfy { |a, b| a.pdf_logical_page_number <= b.pdf_logical_page_number })
+
+    # Some details of the PDF we know and check
+    expect(paragraphs.first.pdf_logical_page_number).to eq 1
+    # we skipped some internal pages in this sample
+    expect(paragraphs.lastfirst.pdf_logical_page_number).to eq "25"
+
+  end
+
+
+end
