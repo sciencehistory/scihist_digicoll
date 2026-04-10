0a9443b1d947cf3838f12b72f4e9328f486a64a2
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-04-10 14:45:51 -0400

    fixing comments

--- app/models/google_arts_and_culture_download.rb
+++ app/models/google_arts_and_culture_download.rb
@@ -1,22 +1,9 @@
-# Represents one of of our large whole-work derivatives (zip of images, or PDF of images),
-# that are created on-demand.
-#
-# Represents the in_progress/success/error state of creation on demand.
-#
-# An OnDemandDerivative record may exist when the actual derivative no longer exists in storage,
-# because we store them in an S3 with lifecycle rules set to remove not recently used copies,
-# as a sort of cache.
-#
-# You can #put_file, #file_exists?, and #file_url to deal with the attached file. We use Shrine::UploadedFile
-# objects directly, without using Shrine Attachment code.
+# Represents a zip file containing files and metadata meant to be downloaded from S3 and then exported into Google Arts and Culture.
+
 class GoogleArtsAndCultureDownload < ApplicationRecord
-
   SHRINE_STORAGE_KEY = :google_arts_and_culture
-
   enum :status, %w{in_progress uploading success error}.collect {|v| [v, v]}.to_h.freeze
-
   belongs_to :user
-
 
   def works_added
     @works_added ||= 0
@@ -48,7 +35,7 @@ class GoogleArtsAndCultureDownload < ApplicationRecord
   def file_url
     uploaded_file.url(
       public: false,
-      response_content_type: uploaded_file.metadata["mime_type"],
+      response_content_type: 'application/zip',
       response_content_disposition: ContentDisposition.attachment(file_key)
     )
   end
@@ -61,15 +48,10 @@ class GoogleArtsAndCultureDownload < ApplicationRecord
       log_error(e)
       raise
     end
-
-
   end
-
 
   def log_error(e)
     Rails.logger.info e.message
     update!({status: 'error', error_info: e.message})
   end
-
-
 end
