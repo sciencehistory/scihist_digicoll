7d55b82398922490b2607f3ca832c456d6d86ab3
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-04-15 13:13:27 -0400

    better testing for download object

--- spec/models/google_arts_and_culture_download_spec.rb
+++ spec/models/google_arts_and_culture_download_spec.rb
@@ -5,8 +5,13 @@ describe GoogleArtsAndCultureDownload do
   let(:file) {  Tempfile.new(["files", ".zip"]) }
   it "can put and access file" do
     download.put_file(file)
-    expect(download.file_exists?).to be true
+    shrine_info = download.uploaded_file.as_json
+    expect(shrine_info['id']).to match /google_arts_and_culture_downloads_\d*.zip/
+    expect(shrine_info['storage']).to eq "google_arts_and_culture"
+    expect(shrine_info['metadata']['mime_type']).to eq "application/zip"
+    expect(shrine_info['metadata'].keys.sort).to eq ["created_at", "created_by", "mime_type"]
     expect(download.file_url).to match /public\/google_arts_and_culture_downloads\/google_arts_and_culture_downloads_[\d]+\.zip/
     download.log_work_added!
+    expect(download.works_added).to eq 1
   end
 end
