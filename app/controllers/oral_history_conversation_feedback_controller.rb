8d7f723ae3a19d4af8125cd96b77cbc0a34c1149
Jonathan Rochkind <jonathan@dnil.net>
2026-04-21 10:41:13 -0400

    AI conversation feedback form, not yet operable

---
+++ app/controllers/oral_history_conversation_feedback_controller.rb
@@ -0,0 +1,18 @@
+class OralHistoryConversationFeedbackController < ApplicationController
+  before_action do
+    # for now let anyone who can engage in conversations rate any conversation,
+    # we don't record who issued which.
+    #authorize! :create, OralHistory::AiConversation
+  end
+
+  # displayed in modal
+  def new
+
+  end
+
+  # response displayed in modal
+  def create
+
+  end
+
+end
