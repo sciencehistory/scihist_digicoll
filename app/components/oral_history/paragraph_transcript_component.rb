1472c4d263829db3d413b18161333f72a4b974f8
Jonathan Rochkind <jonathan@dnil.net>
2026-04-23 15:44:51 -0400

    paragraph transcript component handles inline markers for page break and timestamps,
    Making sure to output the markers at correct inline place.

--- app/components/oral_history/paragraph_transcript_component.rb
+++ app/components/oral_history/paragraph_transcript_component.rb
@@ -38,16 +38,18 @@ module OralHistory
       last_page_number = nil
 
       paragraphs.each do |paragraph|
+        text, changes = replace_internal_markers(paragraph)
+
         yield(
           speaker_label: (paragraph.speaker_name if paragraph.speaker_name != last_speaker),
-          html_text: paragraph.text,
-          start_seconds: paragraph.included_timestamps&.first,
-          page_marker: (paragraph.pdf_logical_page_number if paragraph.pdf_logical_page_number != last_page_number),
+          html_text: text,
+          start_seconds: (paragraph.included_timestamps&.first if !changes[:timestamp]),
+          page_marker: (paragraph.pdf_logical_page_number if (!changes[:page]) && paragraph.pdf_logical_page_number != last_page_number),
           fragment_id: paragraph.fragment_id
         )
 
         last_speaker = paragraph.speaker_name
-        last_page_number = paragraph.pdf_logical_page_number
+        last_page_number = changes[:page] || paragraph.pdf_logical_page_number
       end
     end
 
@@ -64,6 +66,63 @@ module OralHistory
                     # code that searches for it.
                   data: { "ohms_timestamp_s" => "%.3f" % start_seconds.round(3)}
       )
+    end
+
+    # usually a new page or timestamp are just indicated by change in metadata between
+    # paragraphs.
+    #
+    # But sometimes there's an internal marker in the text for EXACTLY where it belongs.
+    # In those cases, we need to actually replace the internal marker with proper html safe
+    # tag, so the marker shows up corresopnding to correct line.
+    #
+    # We also need to return IF it was replaced, so we don't double-place it due to
+    # changed metadata, and if it was a page marker, what it was, so we can keep track
+    # of page changes.
+    #
+    # And we have to be searching for two at once, and HTML-escaping the data on either
+    # side, kind of convoluted.
+    def replace_internal_markers(paragraph)
+      text = paragraph.text
+      orig_html_safe = text.html_safe?
+
+      replaced_page_marker = nil
+      replaced_timestamp = nil
+
+      # can match either, we use named capture group so we can figure out
+      # which we matched.
+      regex = %r{(?:<T: (?<timestamp_m>\d+) min>)|(?:<PAGE-BREAK next=['"](?<break_page>\d+)['"]></PAGE-BREAK>)}
+
+      results = []
+      last = 0
+      text.scan(regex) do |captures|
+        match = Regexp.last_match
+
+        previous = text[last...match.begin(0)]
+        previous.html_safe! if orig_html_safe
+        results << previous
+
+        if match[:break_page]
+          results << render_page_marker(match[:break_page])
+          replaced_page_marker = match[:break_page].to_i
+        elsif match[:timestamp_m]
+          # we don't use the value from <T:>, becuase it hasn't been normalized
+          # for multiple files, we just use the value from the paragraph?
+          if paragraph.included_timestamps&.first
+            results << render_timestamp_marker(paragraph.included_timestamps.first)
+            replaced_timestamp = true
+          end
+        end
+        last = match.end(0)
+      end
+      previous = text[last..]
+      previous.html_safe! if orig_html_safe
+      results << previous
+
+      # safe_join will html_escape components that aren't html_safe before joining
+      return [
+        ActionController::Base.helpers.safe_join(results),
+        { page: replaced_page_marker, timestamp: replaced_timestamp}
+      ]
     end
 
 
