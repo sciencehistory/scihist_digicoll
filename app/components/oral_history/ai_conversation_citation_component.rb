9530b7ca0d46fb8203faf79e2626809635ca40a7
Jonathan Rochkind <jonathan@dnil.net>
2026-05-12 16:20:12 -0400

    include PDF physical page as param from AI link to PDF
    Will result in a redirect, so we need to include the page as a param so the redirect can include it as a fragment anchor

--- app/components/oral_history/ai_conversation_citation_component.rb
+++ app/components/oral_history/ai_conversation_citation_component.rb
@@ -12,19 +12,23 @@ module OralHistory
         # If OHMS, we link directly to work page with anchor to take to specific paragraph
         if citation_item.can_link_to_html_transcript?
           work_path(citation_item.work.friendlier_id, anchor: "p=#{citation_item.paragraph_start}")
-        elsif citation_item.work.oral_history_content.available_by_request_manual_review?
+        elsif citation_item.oral_history_content.available_by_request_manual_review?
           # they will need to request, just go to Work page, and trigger open request form
           work_path(citation_item.work.friendlier_id, anchor: "modal-auto-open=oh-request-trigger")
         else
-          # otherwise, for now wit only staff viewers,  if it's not approval required, we
+          # otherwise, for now with only staff viewers,  if it's not approval required, we
           # we let them see it, and go right , to PDF. May need to rethink if ever for other audience.
           #
           # If we have a page number, we can try linking to page, which some
           # browsers can handle. (mobile usually cannot)
-          #  oops, this doens't work yet, we need to take account offset to do this, although
-          #  we do have offset now and could do that...
-          #anchor = "page=#{citation_item.page_number}" if citation_item.page_number
-          view_transcript_pdf_path(citation_item.work)
+          if citation_item.page_number
+            anchor_page_number = citation_item.page_number.to_i
+            offset = citation_item.oral_history_content&.extracted_pdf_paragraphs&.logical_page_number_offset
+
+            anchor_page_number += offset.to_i if offset
+          end
+
+          view_transcript_pdf_path(citation_item.work, anchor_page: anchor_page_number)
         end
       end
     end
