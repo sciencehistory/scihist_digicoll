a00a9cf89997da4f05564dd7e28e42ae91071ea1
Jonathan Rochkind <jonathan@dnil.net>
2026-08-03 15:13:43 -0400

    move video transcript toggle

--- app/frontend/javascript/transcript_toggle.js
+++ app/frontend/javascript/transcript_toggle.js
@@ -7,16 +7,19 @@ domready(function() {
 
   if (transcriptCollapsible && toggle) {
     transcriptCollapsible.addEventListener('shown.bs.collapse', event => {
-      toggle.textContent = toggle.dataset.hideLabel;
-
       // jump to transcript if needed
       const bounding = transcriptCollapsible.getBoundingClientRect();
       if (bounding && !(bounding.bottom >= 0 && bounding.top <= document.documentElement.clientHeight)) {
         transcriptCollapsible.scrollIntoView();
       }
     });
-    transcriptCollapsible.addEventListener('hidden.bs.collapse', event => {
-      toggle.textContent = toggle.dataset.showLabel;
+
+    transcriptCollapsible.addEventListener('show.bs.collapse', event => {
+      toggle.style.display = "none"; // hide toggle botton
+    });
+
+    transcriptCollapsible.addEventListener('hide.bs.collapse', event => {
+      toggle.style.display = ""; // show toggle botton
     });
   }
 });
