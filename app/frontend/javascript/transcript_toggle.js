a12729b9b503bfbcee05e94c1d2cf315f6ca388f
Jonathan Rochkind <jonathan@dnil.net>
2026-08-03 15:20:36 -0400

    acessiblity improvements for show/hide AV transcript

--- app/frontend/javascript/transcript_toggle.js
+++ app/frontend/javascript/transcript_toggle.js
@@ -4,6 +4,7 @@ import { Collapse } from 'bootstrap';
 domready(function() {
   const toggle = document.getElementById("showVideoTranscriptToggle");
   const transcriptCollapsible = document.getElementById('show-video-transcript-collapse');
+  const transcriptContent = transcriptCollapsible && transcriptCollapsible.querySelector('[data-transcript-content-target]');
 
   if (transcriptCollapsible && toggle) {
     transcriptCollapsible.addEventListener('shown.bs.collapse', event => {
@@ -11,6 +12,11 @@ domready(function() {
       const bounding = transcriptCollapsible.getBoundingClientRect();
       if (bounding && !(bounding.bottom >= 0 && bounding.top <= document.documentElement.clientHeight)) {
         transcriptCollapsible.scrollIntoView();
+      }
+
+      // move focus into the transcript, since the toggle button that had focus is now hidden
+      if (transcriptContent) {
+        transcriptContent.focus();
       }
     });
 
@@ -20,6 +26,11 @@ domready(function() {
 
     transcriptCollapsible.addEventListener('hide.bs.collapse', event => {
       toggle.style.display = ""; // show toggle botton
+    });
+
+    transcriptCollapsible.addEventListener('hidden.bs.collapse', event => {
+      // return focus to toggle, since the close button that had focus is now hidden
+      toggle.focus();
     });
   }
 });
