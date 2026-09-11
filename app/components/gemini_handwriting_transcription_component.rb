a67f58a9e470f79d62a585cfe842384409dd114a
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-09-11 14:08:34 -0400

    transcript request UI now has a component

--- app/components/gemini_handwriting_transcription_component.rb
+++ app/components/gemini_handwriting_transcription_component.rb
@@ -4,4 +4,8 @@ class GeminiHandwritingTranscriptionComponent < ApplicationComponent
   def initialize(work)
     @work = work
   end
+
+  def eligibility_problems
+    GeminiHandwritingTranscriptionService.new(work: work).work_eligibility_problems
+  end
 end
