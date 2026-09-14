38ed030e13dffad4679211a469e929b7f8d57d33
Eddie Rubeiz <erubeiz@sciencehistory.org>
2026-09-14 13:56:44 -0400

    de-hardcoding reference to the htr_transcript attribute

--- app/components/transcription_tabs_component.rb
+++ app/components/transcription_tabs_component.rb
@@ -21,7 +21,7 @@ class TranscriptionTabsComponent < ApplicationComponent
   end
 
   def htr_transcript_texts
-    @htr_transcript_texts ||= Work::TextPage.compile(members, accessor: :htr_transcript)
+    @htr_transcript_texts ||= Work::TextPage.compile(members, accessor: Asset::HTR_TRANSCRIPT_ATTRIBUTE)
   end
 
   def translation_texts
